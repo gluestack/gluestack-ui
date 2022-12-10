e1f2ae591bb6af910d8208f3dd94dbd130873cd7
madhav23bansal <madhavb230100@gmail.com>
2022-12-10 18:24:50 +0530

    Added package-template

---
+++ _template/package-template/example/native/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import Example from "@gluestack/template";
+
+export default function App() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ _template/package-template/src/index.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Example() {
+  return Math.random("Hello World!");
+}
+
+export default Example;
