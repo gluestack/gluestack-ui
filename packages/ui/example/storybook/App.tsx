818d1afe9eb5a631b266e2a5bf28dc9ce3fba4c2
madhav23bansal <madhavb230100@gmail.com>
2022-12-12 18:06:54 +0530

    Added storybook expo

---
+++ packages/ui/example/storybook/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return <View style={styles.container}>Hello</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
