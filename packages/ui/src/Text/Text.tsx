1553e7cec0813893f78c3a34fdaca23b2b88f3fd
mayank-96 <pagarmayank07@gmail.com>
2022-12-12 16:30:56 +0530

    fix: wrap error

--- packages/ui/src/Text/Text.tsx
+++ packages/ui/src/Text/Text.tsx
@@ -1,9 +1,13 @@
 import React, { forwardRef } from 'react';
 import { UIContext } from '../UIProvider';
 
-const Text = ({ children, ...props }: any) => {
+const Text = ({ children, ...props }: any, ref: any) => {
   const { StyledText } = React.useContext(UIContext);
-  return <StyledText {...props}>{children}</StyledText>;
+  return (
+    <StyledText ref={ref} {...props}>
+      {children}
+    </StyledText>
+  );
 };
 
 export default forwardRef(Text);
