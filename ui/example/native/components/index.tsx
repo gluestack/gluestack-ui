445301c7d3eaff7f9509d3ebd7d621c262dcb77c
mayank-96 <pagarmayank07@gmail.com>
2022-12-12 20:31:42 +0530

    feat: added image

--- ui/example/native/components/index.tsx
+++ ui/example/native/components/index.tsx
@@ -1,6 +1,8 @@
 export { default as StyledButton } from './StyledButton';
 export { default as StyledButtonText } from './StyledButtonText';
 export { default as StyledBox } from './StyledBox';
+export { default as StyledImage } from './StyledImage';
+export { default as StyledImageFallbackText } from './StyledImage/StyledImageFallbackText';
 export { default as StyledHeading } from './StyledHeading';
 export { default as StyledText } from './StyledText';
 export { default as StyledCenter } from './StyledCenter';
@@ -8,10 +10,8 @@ export { default as StyledVStack } from './StyledVStack';
 export { default as StyledVStackSpacer } from './StyledVStackSpacer';
 export { default as StyledHStack } from './StyledHStack';
 export { default as StyledHStackSpacer } from './StyledHStackSpacer';
-
 export { default as StyledBoxText } from './StyledBoxText';
 export { default as StyledCheckbox } from './StyledCheckbox';
 export { default as StyledCheckboxIndicator } from './StyledCheckbox/StyledCheckboxIndicator';
 export { default as StyledCheckboxIcon } from './StyledCheckbox/StyledCheckboxIcon';
-
 export { default as StyledCheckboxLabel } from './StyledCheckbox/StyledCheckboxLabel';
