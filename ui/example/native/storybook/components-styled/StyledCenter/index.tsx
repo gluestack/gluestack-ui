db22b8afb2c45dff2077e4ae2970e7bb3fb27aa5
Meenu Makkar <makkarmini007@gmail.com>
2022-12-12 22:00:35 +0530

    feat: moved storybook to natiev folder

---
+++ ui/example/native/storybook/components-styled/StyledCenter/index.tsx
@@ -0,0 +1,16 @@
+import { View } from 'react-native';
+import { styled } from '@gluestack/styled';
+
+export default styled(
+  View,
+  {
+    baseStyle: {
+      style: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    },
+  },
+  {}
+);
