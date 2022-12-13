04754898991a0660e299b232f8bcda53a41179c0
ankit-tailor <tailorankit3345@gmail.com>
2022-12-13 11:36:20 +0530

    feat: switch

---
+++ ui/example/native/components/StyledSwitch/index.tsx
@@ -0,0 +1,14 @@
+import { styled } from '@gluestack/styled';
+import { Switch } from 'react-native';
+
+export default styled(
+  Switch,
+  {
+    baseStyle: {
+      style: {
+        bg: '$primary.500',
+      },
+    },
+  },
+  {}
+);
