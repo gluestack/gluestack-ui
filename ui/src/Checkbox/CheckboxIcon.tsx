ba9f1706fba9f9da84783299ce99f6199cc0e01e
ankit-tailor <tailorankit3345@gmail.com>
2022-12-13 15:12:36 +0530

    feat: checkbox stories

--- ui/src/Checkbox/CheckboxIcon.tsx
+++ ui/src/Checkbox/CheckboxIcon.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { UIContext } from '../UIProvider';
+import { useCheckbox } from './CheckboxProvider';
 
 export function CheckboxIcon({ children, ...props }: any) {
   const { StyledCheckboxIcon } = React.useContext(UIContext);
+  const { isHovered, isChecked, isDisabled, isFocusVisible } =
+    useCheckbox('CheckboxContext');
 
-  return <StyledCheckboxIcon {...props}>{children}</StyledCheckboxIcon>;
+  return (
+    <StyledCheckboxIcon
+      states={{
+        hover: isHovered,
+        checked: isChecked,
+        disabled: isDisabled,
+        focusVisible: isFocusVisible,
+      }}
+      // sx={{
+      //   state: {
+      //     checked: {
+      //       style: {
+      //         bg: '$amber.500',
+      //       },
+      //     },
+      //   },
+      // }}
+      {...props}
+    >
+      {children}
+    </StyledCheckboxIcon>
+  );
 }
