df9402338c84fcb81ec9d543ee53d304e7b97692
ankit-tailor <tailorankit3345@gmail.com>
2022-12-13 14:22:17 +0530

    feat: checkbox

--- packages/ui/src/Checkbox/Checkbox.tsx
+++ packages/ui/src/Checkbox/Checkbox.tsx
@@ -93,8 +93,69 @@
 
 import React from 'react';
 import { UIContext } from '../UIProvider';
+import { CheckboxProvider } from './CheckboxProvider';
+import { VisuallyHidden } from '@react-aria/visually-hidden';
+import { useFocusRing } from '@react-native-aria/focus';
+import { useHover } from '@react-native-aria/interactions';
+import { useToggleState } from '@react-stately/toggle';
+import { useCheckbox } from '@react-native-aria/checkbox';
+import type { ICheckboxProps } from './types';
+import { Platform } from 'react-native';
 
-export function Checkbox({ children, ...props }: any) {
+export function Checkbox({ children, ...props }: ICheckboxProps) {
   const { StyledCheckbox } = React.useContext(UIContext);
-  return <StyledCheckbox {...props}>{children}</StyledCheckbox>;
+
+  const _ref = React.useRef(null);
+  const { isHovered } = useHover({}, _ref);
+
+  // const { checked: isChecked, disabled: isDisabled } = inputProps;
+  const state = useToggleState({
+    ...props,
+    defaultSelected: props.defaultIsChecked,
+    isSelected: props.isChecked,
+  });
+  const { focusProps, isFocusVisible } = useFocusRing();
+  const inputProps = useCheckbox(
+    {
+      'aria-label': props.accessibilityLabel,
+    },
+    state,
+    //@ts-ignore
+    _ref
+  );
+  const {
+    inputProps: { checked: isChecked, disabled: isDisabled },
+  } = inputProps;
+
+  if (Platform.OS === 'web') {
+    return (
+      <StyledCheckbox {...props} accessibilityRole="label" ref={_ref}>
+        <VisuallyHidden>
+          {/* <input {...props.inputProps} {...props.focusProps} ref={props.mergedRef} /> */}
+          <input {...inputProps.inputProps} {...focusProps} ref={_ref} />
+        </VisuallyHidden>
+        <CheckboxProvider
+          isChecked={isChecked}
+          isDisabled={isDisabled}
+          isFocusVisible={isFocusVisible}
+          isHovered={isHovered}
+        >
+          {children}
+        </CheckboxProvider>
+      </StyledCheckbox>
+    );
+  } else {
+    return (
+      <StyledCheckbox {...inputProps.inputProps} {...focusProps}>
+        <CheckboxProvider
+          isChecked={isChecked}
+          isDisabled={isDisabled}
+          isFocusVisible={isFocusVisible}
+          isHovered={isHovered}
+        >
+          {children}
+        </CheckboxProvider>
+      </StyledCheckbox>
+    );
+  }
 }
--- packages/ui/src/Checkbox/CheckboxIndicator.tsx
+++ packages/ui/src/Checkbox/CheckboxIndicator.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { UIContext } from '../UIProvider';
+import { useCheckbox } from './CheckboxProvider';
 
 export function CheckboxIndicator({ children, ...props }: any) {
   const { StyledCheckboxIndicator } = React.useContext(UIContext);
+  const { isChecked, isDisabled, isFocusVisible } =
+    useCheckbox('CheckboxContext');
 
   return (
-    <StyledCheckboxIndicator {...props}>{children}</StyledCheckboxIndicator>
+    <StyledCheckboxIndicator
+      states={{
+        checked: isChecked,
+        focusVisible: isFocusVisible,
+        disabled: isDisabled,
+      }}
+      {...props}
+    >
+      {children}
+    </StyledCheckboxIndicator>
   );
 }
