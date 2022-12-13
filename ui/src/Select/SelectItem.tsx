2359cb74873fe6979d397d1eccffce1e38085987
Viraj-10 <virajjoshi132@gmail.com>
2022-12-13 12:21:03 +0530

    fix: select component fix

--- ui/src/Select/SelectItem.tsx
+++ ui/src/Select/SelectItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import type { ISelectItemProps } from './types';
-import { UIContext } from '../UIProvider';
-// import { useContext } from './SelectContext';
 export function SelectItem({ ...props }: ISelectItemProps) {
   return <></>;
 }
