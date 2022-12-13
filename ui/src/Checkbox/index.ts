08d8cd48f04286f4f8fd22741455d52e9eed4636
ankit-tailor <tailorankit3345@gmail.com>
2022-12-13 16:32:41 +0530

    feat: checkbox group

--- ui/src/Checkbox/index.ts
+++ ui/src/Checkbox/index.ts
@@ -2,8 +2,10 @@ import { Checkbox } from './Checkbox';
 import { CheckboxIcon } from './CheckboxIcon';
 import { CheckboxIndicator } from './CheckboxIndicator';
 import { CheckboxLabel } from './CheckboxLabel';
+import { CheckboxGroup } from './CheckboxGroup';
 
 const CheckboxTemp = Checkbox as any;
+CheckboxTemp.Group = CheckboxGroup;
 CheckboxTemp.Label = CheckboxLabel;
 CheckboxTemp.Icon = CheckboxIcon;
 CheckboxTemp.Indicator = CheckboxIndicator;
