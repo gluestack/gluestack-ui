9312e0aa62610a5060a22e9bd7099acadf9fe9bc
mayank-96 <pagarmayank07@gmail.com>
2022-12-14 20:49:06 +0530

    fix: added button spinner

--- ui/src/Button/index.tsx
+++ ui/src/Button/index.tsx
@@ -1,7 +1,8 @@
 import Button from './Button';
+import ButtonSpinner from './ButtonSpinner';
 import ButtonText from './ButtonText';
 
 const ButtonTemp = Button as any;
 ButtonTemp.Text = ButtonText;
-
+ButtonTemp.Spinner = ButtonSpinner;
 export { ButtonTemp as Button };
