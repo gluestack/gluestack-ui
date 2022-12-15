0b8d7ff97f561150242f61258e65a7f47c3856bc
Viraj-10 <virajjoshi132@gmail.com>
2022-12-15 21:18:53 +0530

    fix: divider and stories

--- packages/ui/src/Divider/index.tsx
+++ packages/ui/src/Divider/index.tsx
@@ -1,3 +1,6 @@
-import { Divider } from './Divider';
+import { Divider as DividerMain } from './Divider';
 
-export const createDivider = ({ StyledDivider }: any) => Divider(StyledDivider);
+export const createDivider = ({ StyledDivider }: any) => {
+  const Divider = DividerMain(StyledDivider);
+  return Divider;
+};
