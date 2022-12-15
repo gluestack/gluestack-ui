30935998a8bceca260841b32d108f33e4ed1360e
Meenu Makkar <makkarmini007@gmail.com>
2022-12-15 16:19:25 +0530

    feat: pushed badge, box, and avatar

--- packages/ui/example/native/src/index.tsx
+++ packages/ui/example/native/src/index.tsx
@@ -1,2 +1,6 @@
 export { Button } from './ui-components/Button';
 export { Actionsheet } from './ui-components/Actionsheet';
+export { Alert } from './ui-components/Alert';
+export { Avatar } from './ui-components/Avatar';
+export { Badge } from './ui-components/Badge';
+export { Box } from './ui-components/Box';
