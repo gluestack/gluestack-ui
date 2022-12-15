e91a35908638985b76f5387c7e5a1bf7415e29a9
Suraj Ahmed <suraj@geekyants.com>
2022-12-15 12:21:37 +0530

    fix: next example updated

--- ui/example/next/pages/_app.jsx
+++ ui/example/next/pages/_app.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
