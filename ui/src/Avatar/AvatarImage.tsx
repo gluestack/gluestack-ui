c457b4e1eb004312862c52bd5e9b3f82114df82e
Viraj-10 <virajjoshi132@gmail.com>
2022-12-15 21:01:25 +0530

    fix: avatar with stories

--- ui/src/Avatar/AvatarImage.tsx
+++ ui/src/Avatar/AvatarImage.tsx
@@ -3,6 +3,7 @@ import React, { forwardRef } from 'react';
 const AvatarImage = (StyledAvatarImage: any, StyledText: any) =>
   forwardRef(({ children, source, ...props }: any, ref: any) => {
     const [error, setError] = React.useState(false);
+
     const getSource = () => {
       if (source) {
         if (source.hasOwnProperty('uri') && source.uri === null) {
