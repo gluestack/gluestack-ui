232a73c17c967817c17fda91574f8f7c97177590
Viraj-10 <virajjoshi132@gmail.com>
2022-12-15 16:02:32 +0530

    feat: text and switch api

--- packages/ui/src/Text/index.tsx
+++ packages/ui/src/Text/index.tsx
@@ -1,9 +1,6 @@
 import { Text } from './Text';
 
-export const createButton = ({ StyledText }: any) => {
+export const createText = ({ StyledText }: any) => {
   const TextTemp = Text(StyledText) as any;
-
   return TextTemp;
 };
-
-// export {Button ,ButtonText}
