9e4d09d864ff487ef17dad4e8906356780be1adc
ankit-tailor <tailorankit3345@gmail.com>
2022-12-16 19:37:29 +0530

    feat: example for utility props resolver

--- styled/example/native/App.tsx
+++ styled/example/native/App.tsx
@@ -2,9 +2,11 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 import { styled } from '@gluestack/styled';
+import { convertUtilityPropsToSX } from '@gluestack/ui-convert-utility-to-sx';
+import React from 'react';
 
 const Box = styled(
-  View,
+  Pressable,
   {
     baseStyle: {
       style: {
@@ -39,22 +41,49 @@ const Box = styled(
 );
 
 export default function App() {
+  const [isPressed, setIsPressed] = React.useState(false);
+  const [isHovered, setisHovered] = React.useState(false);
   return (
     <View style={styles.container}>
       <Box
+        onPressIn={() => setIsPressed(true)}
+        onPressOut={() => setIsPressed(false)}
+        onHoverIn={() => setisHovered(true)}
+        onHoverOut={() => setisHovered(false)}
         variant="greenBox"
-        // states={{
-        //   hover: true,
-        // }}
-        // sx={{
-        //   state: {
-        //     hover: {
-        //       style: {
-        //         bg: "$red.500",
-        //       },
-        //     },
-        //   },
-        // }}
+        bg="$red500"
+        hover-bg="$green600"
+        hover-web-bg="$amber600"
+        active-p="$8"
+        active-bg="$pink600"
+        web-bg="$blue600"
+        web-hover-bg="$gray600"
+        web-hover-p="$8"
+        states={{
+          hover: isHovered,
+          active: isPressed,
+        }}
+        sx={{
+          platform: {
+            web: {
+              state: {
+                hover: {
+                  style: {
+                    bg: '$amber600',
+                    // p: '$8',
+                  },
+                },
+              },
+            },
+          },
+          state: {
+            hover: {
+              style: {
+                bg: '$red500',
+              },
+            },
+          },
+        }}
       >
         Hello Box
       </Box>
