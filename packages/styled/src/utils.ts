6e81595923e495c63d05a33b044661ae5e6a3419
ankit-tailor <tailorankit3345@gmail.com>
2022-12-16 19:29:48 +0530

    fix: update object setter

--- packages/styled/src/utils.ts
+++ packages/styled/src/utils.ts
@@ -1,13 +1,19 @@
-export const setObjectProperty = (object: any, keyPath: any, value: any) => {
-  if (!Array.isArray(keyPath)) {
-    keyPath = [keyPath];
-  }
-  return keyPath.reduceRight((baseObj: any, key: any, index: number) => {
-    if (index === keyPath.length - 1) {
-      return Object.assign({}, baseObj, { [key]: value });
+export const setObjectKeyValue = (obj: any, keys: any, value: any) => {
+  let current = obj;
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (i === keys.length - 1) {
+      // we've reached the desired key, so update its value
+      current[key] = value;
+    } else {
+      // we're still traversing the object, so create the key if it doesn't exist
+      if (!current[key]) {
+        current[key] = {};
+      }
+      current = current[key];
     }
-    return { [key]: baseObj };
-  }, object);
+  }
+  return obj;
 };
 
 export const getObjectProperty = (object: any, keyPath: any) => {
@@ -18,4 +24,17 @@ export const getObjectProperty = (object: any, keyPath: any) => {
     (baseObj: any, key: any) => baseObj && baseObj[key],
     object
   );
+};
+
+export const deepMerge = (target: any, source: any) => {
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (typeof target[key] === 'object' && typeof source[key] === 'object') {
+        deepMerge(target[key], source[key]);
+      } else {
+        target[key] = source[key];
+      }
+    }
+  }
+  return target;
 };
