d6b34903d4c0c52bdd6740218a422153c5d52c69
ankit-tailor <tailorankit3345@gmail.com>
2022-12-16 15:30:51 +0530

    fix: variable changes

--- packages/styled/src/utils.ts
+++ packages/styled/src/utils.ts
@@ -1,12 +1,21 @@
 export const setObjectProperty = (object: any, keyPath: any, value: any) => {
-  return keyPath.reduceRight((acc: any, key: any, index: number) => {
+  if (!Array.isArray(keyPath)) {
+    keyPath = [keyPath];
+  }
+  return keyPath.reduceRight((baseObj: any, key: any, index: number) => {
     if (index === keyPath.length - 1) {
-      return Object.assign({}, acc, { [key]: value });
+      return Object.assign({}, baseObj, { [key]: value });
     }
-    return { [key]: acc };
+    return { [key]: baseObj };
   }, object);
 };
 
 export const getObjectProperty = (object: any, keyPath: any) => {
-  return keyPath.reduce((acc: any, key: any) => acc && acc[key], object);
+  if (!Array.isArray(keyPath)) {
+    keyPath = [keyPath];
+  }
+  return keyPath.reduce(
+    (baseObj: any, key: any) => baseObj && baseObj[key],
+    object
+  );
 };
