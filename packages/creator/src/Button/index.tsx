811b459c88e9fdccf540e7d9acd10fdb1587373c
amars29 <amars@geekyants>
2022-12-19 22:49:26 +0530

    feat: button-group

--- packages/creator/src/Button/index.tsx
+++ packages/creator/src/Button/index.tsx
@@ -1,9 +1,16 @@
 import { Button } from './Button';
+import { ButtonGroup } from './ButtonGroup';
 import { ButtonText } from './ButtonText';
 
-export const createButton = ({ StyledButton, StyledButtonText }: any) => {
+export const createButton = ({
+  StyledButton,
+  StyledButtonText,
+  StyledButtonGroup,
+  StyledButtonGroupSpacer,
+}: any) => {
   const ButtonTemp = Button(StyledButton) as any;
   ButtonTemp.Text = ButtonText(StyledButtonText);
+  ButtonTemp.Group = ButtonGroup(StyledButtonGroup, StyledButtonGroupSpacer);
 
   return ButtonTemp;
 };
--- packages/ui/src/ui-components/Button/index.tsx
+++ packages/ui/src/ui-components/Button/index.tsx
@@ -1,7 +1,14 @@
-import { StyledButton, StyledButtonText } from '../../styled-components';
+import {
+  StyledButton,
+  StyledButtonText,
+  StyledButtonGroup,
+  StyledButtonGroupSpacer,
+} from '../../styled-components';
 import { createButton } from '@gluestack/ui-creator';
 
 export const Button = createButton({
   StyledButton,
   StyledButtonText,
+  StyledButtonGroup,
+  StyledButtonGroupSpacer,
 }) as any;
