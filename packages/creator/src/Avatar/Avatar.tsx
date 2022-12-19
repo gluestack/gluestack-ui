03bff7e2e68889f2ba943caa4cbdc391ace94b35
amars29 <amars@geekyants>
2022-12-19 17:10:40 +0530

    fix: avatar nesting with badge

--- packages/creator/src/Avatar/Avatar.tsx
+++ packages/creator/src/Avatar/Avatar.tsx
@@ -1,10 +1,22 @@
-import React, { forwardRef } from 'react';
+import React, { createContext, forwardRef } from 'react';
+
+export const AvatarContext = createContext<any>({});
 
 const Avatar = (StyledAvatar: any) =>
   forwardRef(({ children, ...props }: any, ref: any) => {
     return (
       <StyledAvatar ref={ref} {...props}>
-        {children}
+        {({ resolveContextChildrenStyle }: any) => {
+          return (
+            <AvatarContext.Provider
+              value={{
+                resolveContextChildrenStyle: resolveContextChildrenStyle,
+              }}
+            >
+              {children}
+            </AvatarContext.Provider>
+          );
+        }}
       </StyledAvatar>
     );
   });
