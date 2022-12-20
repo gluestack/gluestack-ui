e47596329169ae029edfe8da82748305cad4e1bc
amars29 <amars@geekyants>
2022-12-20 15:29:22 +0530

    feat: updated size prop in heading

--- ui/src/styled-components/StyledHeading/index.tsx
+++ ui/src/styled-components/StyledHeading/index.tsx
@@ -6,8 +6,57 @@ export default styled(
   {
     baseStyle: {
       style: {
-        color: '$blue500',
+        color: '$text900',
+        fontWeight: 'bold',
+        lineHeight: 20,
       },
+    },
+    sizes: {
+      '4xl': {
+        style: {
+          fontSize: 60,
+          letterSpacing: 0.8,
+        },
+      },
+      '3xl': {
+        style: {
+          fontSize: 48,
+          letterSpacing: 0.8,
+        },
+      },
+      '2xl': {
+        style: {
+          fontSize: 36,
+        },
+      },
+      'xl': {
+        style: {
+          fontSize: 30,
+        },
+      },
+      'lg': {
+        style: {
+          fontSize: 24,
+        },
+      },
+      'md': {
+        style: {
+          fontSize: 20,
+        },
+      },
+      'sm': {
+        style: {
+          fontSize: 16,
+        },
+      },
+      'xs': {
+        style: {
+          fontSize: 14,
+        },
+      },
+    },
+    defaultProps: {
+      size: 'lg',
     },
   },
   {}
