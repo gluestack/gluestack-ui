cde0b7b1aea503c473468b1fdcc81deaba553e14
amars29 <amars@geekyants>
2022-12-20 13:45:44 +0530

    feat: updated center styles

--- ui/src/styled-components/StyledCenter/index.tsx
+++ ui/src/styled-components/StyledCenter/index.tsx
@@ -11,6 +11,29 @@ export default styled(
         justifyContent: 'center',
       },
     },
+    sizes: {
+      'xs': {
+        style: { height: 10, width: 10 },
+      },
+      'sm': {
+        style: {
+          height: 12,
+          width: 12,
+        },
+      },
+      'md': {
+        style: { height: 16, width: 16 },
+      },
+      'lg': {
+        style: { height: 24, width: 24 },
+      },
+      'xl': {
+        style: { height: 32, width: 32 },
+      },
+      '2xl': {
+        style: { height: 40, width: 40 },
+      },
+    },
   },
   {}
 );
