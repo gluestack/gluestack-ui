be62b65fd32091fce57c4e0e8fb0f4c42e2f2cea
amars29 <amars@geekyants>
2022-12-21 20:20:51 +0530

    fix: heading stories

--- ui/components/Heading/Heading.stories.tsx
+++ ui/components/Heading/Heading.stories.tsx
@@ -7,9 +7,14 @@ const MyHeadingMeta: ComponentMeta<typeof HeadingBasicExample> = {
   component: HeadingBasicExample,
   argTypes: {
     onPress: { action: 'pressed the button' },
+    size: {
+      control: 'select',
+      options: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl'],
+    },
   },
   args: {
-    text: 'Hello world',
+    text: `I'm the heading`,
+    size: '2xl',
   },
 };
 
@@ -17,4 +22,6 @@ export default MyHeadingMeta;
 
 type MyHeadingStory = ComponentStory<typeof HeadingBasicExample>;
 
-export const Basic: MyHeadingStory = (args) => <HeadingBasicExample />;
+export const Basic: MyHeadingStory = (args) => (
+  <HeadingBasicExample {...args} />
+);
