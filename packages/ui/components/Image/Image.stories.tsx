a01e050593a18fabd0d71b9541a780166989665d
mayank-96 <pagarmayank07@gmail.com>
2022-12-22 17:54:35 +0530

    feat: added image knobs

--- packages/ui/components/Image/Image.stories.tsx
+++ packages/ui/components/Image/Image.stories.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react-native';
 import { Example as BasicExample } from './Basic';
-import { Example as FallbackExample } from './Fallback';
 
 const MyImageMeta: ComponentMeta<typeof BasicExample> = {
   title: 'Image',
   component: BasicExample,
   argTypes: {},
-  args: {},
+  args: {
+    uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    fallbackSource: 'https://www.w3schools.com/css/img_lights.jpg',
+  },
 };
 
 export default MyImageMeta;
@@ -15,6 +17,3 @@ export default MyImageMeta;
 type MyCustomImageStory = ComponentStory<typeof BasicExample>;
 
 export const Basic: MyCustomImageStory = (args) => <BasicExample {...args} />;
-export const Fallback: MyCustomImageStory = (args) => (
-  <FallbackExample {...args} />
-);
