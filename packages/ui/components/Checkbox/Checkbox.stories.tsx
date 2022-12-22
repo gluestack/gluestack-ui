7e4cff7182021fbba37fe18e8f15676cdebd70d6
mayank-96 <pagarmayank07@gmail.com>
2022-12-22 17:33:10 +0530

    feat: added icon button knobs

--- packages/ui/components/Checkbox/Checkbox.stories.tsx
+++ packages/ui/components/Checkbox/Checkbox.stories.tsx
@@ -16,9 +16,5 @@ const MyCheckboxMeta: ComponentMeta<typeof CheckboxGroup> = {
 export default MyCheckboxMeta;
 
 type MyCheckboxStory = ComponentStory<typeof CheckboxGroup>;
-// type CheckBoxGroupStory = ComponentStory<typeof CheckboxGroup>;
 
 export const Basic: MyCheckboxStory = (args) => <CheckboxGroup {...args} />;
-// export const CheckboxGroupExample: CheckBoxGroupStory = (args) => (
-//   <CheckboxGroup />
-// );
