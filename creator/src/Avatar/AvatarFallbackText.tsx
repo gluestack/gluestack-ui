5dbd4c7adfe90c7c67d85d969dd48a973500d2a8
Suraj Ahmed <suraj@geekyants.com>
2022-12-22 22:26:20 +0530

    fix: merged developement-new and conflict resolved

--- creator/src/Avatar/AvatarFallbackText.tsx
+++ creator/src/Avatar/AvatarFallbackText.tsx
@@ -1,25 +1,9 @@
-import React, { forwardRef, useContext } from 'react';
-import { AvatarContext } from './Avatar';
+import React, { forwardRef } from 'react';
 
 export const AvatarFallbackText = (StyledAvatarFallbackText: any) =>
   forwardRef(({ children, ...props }: any, ref: any) => {
-    const { resolveContextChildrenStyle } = useContext(AvatarContext);
-
-    const { ancestorStyle } = StyledAvatarFallbackText.config;
-    let styledObject = {};
-
-    ancestorStyle?.forEach((consumer: any) => {
-      if (resolveContextChildrenStyle[consumer]) {
-        styledObject = [styledObject, resolveContextChildrenStyle[consumer]];
-      }
-    });
-
     return (
-      <StyledAvatarFallbackText
-        ref={ref}
-        {...props}
-        ancestorStyle={styledObject}
-      >
+      <StyledAvatarFallbackText ref={ref} {...props}>
         {children}
       </StyledAvatarFallbackText>
     );
