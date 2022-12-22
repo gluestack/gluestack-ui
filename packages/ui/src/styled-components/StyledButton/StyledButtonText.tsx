1dfa5abdbd27f5df03120bbb5a5da6e3dbcfe432
Suraj Ahmed <suraj@geekyants.com>
2022-12-22 12:51:42 +0530

    fix: confict resolved

--- packages/ui/src/styled-components/StyledButton/StyledButtonText.tsx
+++ packages/ui/src/styled-components/StyledButton/StyledButtonText.tsx
@@ -1,56 +1,10 @@
 import { Text } from 'react-native';
 import { styled } from '@gluestack/ui-styled';
-import React from 'react';
 
-const Button = styled(
+export default styled(
   Text,
   {
-    baseStyle: {
-      style: { color: '$text800' },
-      state: {
-        hover: {
-          style: {
-            bg: '$amber100',
-          },
-        },
-      },
-    },
-    sizes: {
-      xs: {
-        style: {
-          h: '$1/3',
-        },
-      },
-    },
-    variants: {
-      primary: {
-        style: {
-          bg: '$red100',
-        },
-      },
-      secondary: {
-        style: {
-          backgroundColor: '$amber100',
-        },
-        colorMode: {
-          light: {
-            style: {
-              fontFamily: '$body',
-            },
-          },
-        },
-      },
-    },
-  } as const,
+    baseStyle: { style: { color: '$text800' } },
+  },
   { ancestorStyle: ['_text'] }
 );
-
-type myType = UtilityProp;
-
-<Button
-  sx={{
-    style: {
-      bg: '$red100',
-    },
-  }}
-/>;
