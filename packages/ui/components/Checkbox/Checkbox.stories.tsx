38ab4a181e0318989862f688d52f3ea31b87a0c9
ankit-tailor <tailorankit3345@gmail.com>
2022-12-23 14:17:46 +0530

    fix: radio stlyed

--- packages/ui/components/Checkbox/Checkbox.stories.tsx
+++ packages/ui/components/Checkbox/Checkbox.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react-native';
-import { Example as CheckboxGroup } from './Checkbox';
+import { CheckboxGroup } from './CheckboxGroup';
 
 const MyCheckboxMeta: ComponentMeta<typeof CheckboxGroup> = {
   title: 'Checkbox',
