8bf9e9b0e24f174235b99e31727748935257386d
Meenu Makkar <makkarmini007@gmail.com>
2022-12-28 13:09:44 +0530

    feat: shadows working

--- packages/styled/example/native/nb.config.ts
+++ packages/styled/example/native/nb.config.ts
@@ -21,6 +21,26 @@ export const config = {
       property: 'borderColor',
       scale: 'colors',
     },
+    shadowColor: {
+      property: 'shadowColor',
+      scale: 'colors',
+    },
+    shadowOffset: {
+      property: 'shadowOffset',
+      scale: 'space',
+    },
+    shadowOpacity: {
+      property: 'shadowOpacity',
+      scale: 'opacity',
+    },
+    shadowRadius: {
+      property: 'shadowRadius',
+      scale: 'radii',
+    },
+    elevation: {
+      property: 'elevation',
+      scale: 'space',
+    },
     // dimension
     h: {
       property: 'height',
@@ -186,18 +206,130 @@ export const config = {
       property: 'fontSize',
       scale: 'fontSizes',
     },
+    shadow: {
+      property: 'shadow',
+      scale: 'shadows',
+    },
     // Media Query
     condition: {
       property: 'condition',
       scale: 'mediaQueries',
     },
-    //shadow
-    // shadow: {
-    //   property: 'letterSpacing',
-    //   scale: 'shadows',
-    // },
   } as const,
+
   tokens: {
+    shadows: {
+      'none': {
+        shadowColor: 'transparent',
+        shadowOffset: {
+          width: 0,
+          height: 0,
+        },
+        shadowOpacity: 0,
+        shadowRadius: 0,
+        elevation: 0,
+      },
+      '0': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.18,
+        shadowRadius: 1.0,
+        elevation: 1,
+      },
+      '1': {
+        shadowColor: '$red500',
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+      },
+      '2': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+      },
+      '3': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+      },
+      '4': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+      },
+      '5': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+        elevation: 6,
+      },
+      '6': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+      },
+      '7': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4.65,
+        elevation: 8,
+      },
+      '8': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.32,
+        shadowRadius: 5.46,
+        elevation: 9,
+      },
+      '9': {
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 0,
+          height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+      },
+    },
     colors: {
       rose50: '#fff1f2',
       rose100: '#ffe4e6',
