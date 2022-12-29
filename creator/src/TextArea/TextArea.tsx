f79978658cecf6df254910e9164760886f9b70e5
Viraj-10 <virajjoshi132@gmail.com>
2022-12-29 17:12:37 +0530

    fix: textarea focus and styling

--- creator/src/TextArea/TextArea.tsx
+++ creator/src/TextArea/TextArea.tsx
@@ -3,36 +3,54 @@ import { useFormControl } from '../FormControl/useFormControl';
 import { useTextArea } from './TextAreaContext';
 
 export const TextArea = (StyledTextArea: any) =>
-  forwardRef(({ children, multiline = true, ...props }: any, ref: any) => {
-    const {
-      isDisabled,
-      // isReadOnly,
-      // handleFocus,
-      isFocused,
-    } = useTextArea('TextAreaContext');
+  forwardRef(
+    ({ children, multiline = true, onKeyPress, ...props }: any, ref: any) => {
+      const {
+        isDisabled,
+        // isReadOnly,
+        handleFocus,
+        isFocused,
+      } = useTextArea('TextAreaContext');
 
-    const inputProps = useFormControl({
-      isDisabled: props.isDisabled,
-      isInvalid: props.isInvalid,
-      isReadOnly: props.isReadOnly,
-      isRequired: props.isRequired,
-      nativeID: props.nativeID,
-    });
+      const inputProps = useFormControl({
+        isDisabled: props.isDisabled,
+        isInvalid: props.isInvalid,
+        isReadOnly: props.isReadOnly,
+        isRequired: props.isRequired,
+        nativeID: props.nativeID,
+      });
 
-    // console.log(ancestorStyle, 'ancestorStyle');
-    // console.log(styledObject, 'styledObject');
+      // console.log(ancestorStyle, 'ancestorStyle');
+      // console.log(styledObject, 'styledObject');
 
-    return (
-      <StyledTextArea
-        states={{
-          focus: isFocused,
-        }}
-        ref={ref}
-        {...props}
-        multiline={multiline}
-        disabled={isDisabled || inputProps.disabled}
-      >
-        {children}
-      </StyledTextArea>
-    );
-  });
+      return (
+        <StyledTextArea
+          states={{
+            focus: isFocused,
+          }}
+          ref={ref}
+          {...props}
+          multiline={multiline}
+          disabled={isDisabled || inputProps.disabled}
+          onKeyPress={(e: any) => {
+            e.persist();
+            onKeyPress && onKeyPress(e);
+          }}
+          onFocus={(e: any) => {
+            handleFocus(
+              true,
+              props?.onFocus ? () => props?.onFocus(e) : () => {}
+            );
+          }}
+          onBlur={(e: any) => {
+            handleFocus(
+              false,
+              props?.onBlur ? () => props?.onBlur(e) : () => {}
+            );
+          }}
+        >
+          {children}
+        </StyledTextArea>
+      );
+    }
+  );
