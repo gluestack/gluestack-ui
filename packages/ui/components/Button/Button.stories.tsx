6697284c47bcf0eed916a2deabf9e6edbfb27973
amars29 <amars@geekyants>
2023-01-02 15:47:56 +0530

    button story example

--- packages/ui/components/Button/Button.stories.tsx
+++ packages/ui/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react-native';
-import { Button, Center, AddIcon, MinusIcon } from '@gluestack/ui';
+import { Button } from '@gluestack/ui';
 
 const MyButtonMeta: ComponentMeta<typeof Button> = {
   title: 'FORMS/Button',
@@ -17,19 +17,42 @@ const MyButtonMeta: ComponentMeta<typeof Button> = {
     variant: {
       control: 'select',
       options: ['solid', 'subtle', 'outline', 'ghost', 'link'],
+      description: 'The variant of the button style to use.',
+      table: {
+        defaultValue: { summary: 'solid' },
+      },
     },
     size: {
       control: 'select',
       options: ['xs', 'sm', 'md', 'lg'],
+      description: 'The size of the button.',
+      table: {
+        defaultValue: { summary: 'md' },
+      },
     },
     isLoading: {
       control: 'boolean',
+      description: 'If true, the button will show a spinner.',
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: false },
+      },
     },
     leftIcon: {
       control: 'boolean',
+      description: 'The left icon element to use in the button.',
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: false },
+      },
     },
     rightIcon: {
       control: 'boolean',
+      description: 'The right icon element to use in the button.',
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: false },
+      },
     },
   },
 };
@@ -37,81 +60,15 @@ const MyButtonMeta: ComponentMeta<typeof Button> = {
 export default MyButtonMeta;
 
 type MyButtonStory = ComponentStory<typeof Button>;
-// type MyButtonGroupStory = ComponentStory<typeof Button>;
 
-export const Basic: MyButtonStory = ({
-  leftIcon,
-  isLoading,
-  rightIcon,
-  text,
-  ...props
-}) => {
-  return (
-    <Center>
-      <Button {...props}>
-        {isLoading && <Button.Spinner sx={{ style: { mr: 8 } }} />}
-        {leftIcon && <MinusIcon sx={{ style: { mr: 8 } }} />}
-        <Button.Text>{text}</Button.Text>
-        {rightIcon && <AddIcon sx={{ style: { ml: 8 } }} />}
-      </Button>
-    </Center>
-  );
-};
+export { Basic } from './Basic';
 
-// export const GroupedExample: MyButtonGroupStory = ({
-//   variant,
-//   text,
-//   size,
-//   isLoading,
-//   leftIcon,
-//   rightIcon,
-//   direction,
-//   ...props
-// }) => {
-//   return (
-//     <Center>
-//       <Button.Group direction={direction}>
-//         <Button variant={variant} size={size} {...props}>
-//           {isLoading && <Button.Spinner />}
-//           {leftIcon && <AddIcon />}
-//           <Button.Text>{text}</Button.Text>
-//           {rightIcon && <AddIcon />}
-//         </Button>
-//         <Button
-//           variant={variant}
-//           size={size}
-//           {...props}
-//           sx={{ style: { bg: '$blue500' } }}
-//         >
-//           {isLoading && <Button.Spinner />}
-//           {leftIcon && <AddIcon />}
-//           <Button.Text>{text}</Button.Text>
-//           {rightIcon && <AddIcon />}
-//         </Button>
-//         <Button variant={variant} size={size} {...props}>
-//           {isLoading && <Button.Spinner />}
-//           {leftIcon && <AddIcon />}
-//           <Button.Text>{text}</Button.Text>
-//           {rightIcon && <AddIcon />}
-//         </Button>
-//       </Button.Group>
-//     </Center>
-//   );
-// };
+export { ButtonWithIcons } from './ButtonWithIcon';
 
-// GroupedExample.args = {
-//   text: 'Button Text',
-//   variant: 'solid',
-//   size: 'md',
-//   isLoading: false,
-//   leftIcon: false,
-//   rightIcon: false,
-//   direction: 'row',
-// };
+// export { ButtonGroup } from './ButtonGroup';
 
-// GroupedExample.argTypes = {
-//   direction: {
-//     control: 'radio',
-//     options: ['row', 'column'],
-//   },
-// };
+export { Variants } from './Variants';
+
+export { Sizes } from './Sizes';
+
+export { Loading } from './Loading';
