53d790336e2dda6a4a93f2208b9dd5f8b6cf9ce8
amars29 <amars@geekyants>
2023-01-02 18:36:38 +0530

    added box, center and stack stories

--- packages/ui/storybookDocsComponents/DynamicTyping/index.tsx
+++ packages/ui/storybookDocsComponents/DynamicTyping/index.tsx
@@ -4,13 +4,13 @@ const DynamicTyping = ({ component }: any) => {
   return (
     <>
       <h1>{component} Props</h1>
-      <h3>colorScheme</h3>
+      {/* <h3>colorScheme</h3>
       <p>
         The color of the radio when it's checked. This should be one of the
         color keys in the theme (e.g."green", "red").
       </p>
       <p>Type: ColorSchemeType</p>
-      <p>Default: 'primary'</p>
+      <p>Default: 'primary'</p> */}
     </>
   );
 };
