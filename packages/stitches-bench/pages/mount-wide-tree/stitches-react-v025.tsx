1c3ec2c8d319ea83fd14690d4eb22ce14c2e2852
Meenu Makkar <makkarmini007@gmail.com>
2023-01-05 16:18:11 +0530

    feat: basic styled stitches test working

--- packages/stitches-bench/pages/mount-wide-tree/stitches-react-v025.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const SierpinskiTriangle = () => {
-  const StitchesTest = dynamic(() => import('../../bench/mount-wide-tree/stitches-react-v025'), { ssr: false });
-
-  return <StitchesTest />;
-};
-
-export default SierpinskiTriangle;
