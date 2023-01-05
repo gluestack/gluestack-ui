054668871da7c39526fbdc15cbe32525eae21b68
Meenu Makkar <makkarmini007@gmail.com>
2023-01-05 16:04:12 +0530

    feat: basic styled stitches test working

---
+++ performance-test/bench/create-and-mount-button/nativebase-v3.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TestComponentProps, TestRunner } from '../TestRunner';
+// import { css } from '../utils/stitches-core-v025.config';
+// import { buttonStyles } from '../utils/buttonStyles';
+import { Button, NativeBaseProvider } from 'native-base';
+
+const Test = ({}: // testIndex
+TestComponentProps) => {
+  // This purposefully creates the styled component inside the Test component
+  // so that we can measure the time it takes using the React profiler
+  // const button = css({
+  //   '--test-index': testIndex,
+  // });
+
+  return (
+    // <div className={button()}>
+    <NativeBaseProvider>
+      <Button>Hello nbv3 button</Button>
+    </NativeBaseProvider>
+    // </div>
+  );
+};
+
+const StitchesTest = () => {
+  return <TestRunner numberOfRuns={3} iterationN={1000} TestComponent={Test} />;
+};
+
+export default StitchesTest;
