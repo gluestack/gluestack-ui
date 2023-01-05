8de0de558fde0c4d7e734dfa1101312cca2c3715
Suraj Ahmed <suraj@geekyants.com>
2023-01-05 19:27:25 +0530

    fix: storybook example for react native

--- storybook/components/Avatar/Avatar.stories.tsx
+++ storybook/components/Avatar/Avatar.stories.tsx
@@ -14,7 +14,7 @@ const AvatarMeta: ComponentMeta<typeof Avatar> = {
     size: 'md',
     uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     fallbackText: 'AB',
-    badge: false,
+    badge: true,
   },
   argTypes: {
     size: {
