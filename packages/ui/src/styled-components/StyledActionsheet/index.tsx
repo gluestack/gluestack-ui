1c4a06833d93177aa1346d89afae97da941f3cd6
mayank-96 <pagarmayank07@gmail.com>
2023-01-05 14:37:18 +0530

    fix: actionsheet height

--- packages/ui/src/styled-components/StyledActionsheet/index.tsx
+++ packages/ui/src/styled-components/StyledActionsheet/index.tsx
@@ -6,8 +6,8 @@ export default styled(
   {
     baseStyle: {
       style: {
-        // width: '100%',
-        // height: '100%',
+        width: '100%',
+        height: '100%',
         justifyContent: 'flex-end',
         alignItems: 'center',
       },
