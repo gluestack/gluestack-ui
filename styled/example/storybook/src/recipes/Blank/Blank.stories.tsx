9f35ebcae79fa7c7836ae3bcd31997f6bab95661
vidhi499 <katariavidhi99@gmail.com>
2023-01-06 18:55:46 +0530

    feat: variant and sizes added

--- styled/example/storybook/src/recipes/Blank/Blank.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { Blank } from './Blank';
-const MyBlankMeta: ComponentMeta<typeof Blank> = {
-  title: 'Recipes/Blank',
-  component: Blank,
-};
-
-export { Blank } from './Blank';
-export default MyBlankMeta;
