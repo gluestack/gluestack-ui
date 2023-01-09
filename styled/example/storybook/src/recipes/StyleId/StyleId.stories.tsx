6af017ae86e3a279e97633f905a4a45bd0a52831
Suraj Ahmed <suraj@geekyants.com>
2023-01-09 14:00:13 +0530

    fix: styleid refactor

---
+++ styled/example/storybook/src/recipes/StyleId/StyleId.stories.tsx
@@ -0,0 +1,9 @@
+import type { ComponentMeta } from '@storybook/react-native';
+import { StyleId } from './StyleId';
+const MyStyleIdMeta: ComponentMeta<typeof StyleId> = {
+  title: 'recipes/StyleId',
+  component: StyleId,
+};
+
+export { StyleId } from './StyleId';
+export default MyStyleIdMeta;
