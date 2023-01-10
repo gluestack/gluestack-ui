ecb489e44129e40b794a1750211fb465f48e556f
mayank-96 <pagarmayank07@gmail.com>
2023-01-10 13:42:23 +0530

    feat: theme tokens

--- packages/styled/example/storybook/src/recipes/ThemeTokens/ThemeTokens.tsx
+++ packages/styled/example/storybook/src/recipes/ThemeTokens/ThemeTokens.tsx
@@ -10,8 +10,10 @@ const StyledThemeTokens = styled(
       style: {
         height: 100,
         width: 100,
-        bg: '$primary400',
+        bg: '$primary300',
         margin: 10,
+        borderWidth: '$1',
+        borderColor: '$lightBlue500',
       },
     },
   },
@@ -27,23 +29,34 @@ const customConfig = {
 
   tokens: {
     colors: {
-      primary50: '#ecfdf5',
-      primary100: '#d1fae5',
-      primary200: '#a7f3d0',
-      primary300: '#6ee7b7',
-      primary400: '#34d399',
-      primary500: '#10b981',
-      primary600: '#059669',
+      primary50: '#fffbeb',
+      primary100: '#fef3c7',
+      primary200: '#fde68a',
+      primary300: '#fcd34d',
+      primary400: '#fbbf24',
+      primary500: '#f59e0b',
+      primary600: '#d97706',
+      lightBlue300: '#7dd3fc',
+      lightBlue400: '#38bdf8',
+      lightBlue500: '#0ea5e9',
+      lightBlue600: '#0284c7',
+    },
+    borderWidths: {
+      '0': 0,
+      '1': '4px',
+      '2': '8px',
+      '4': '12px',
+      '8': '16px',
     },
   } as const,
-};
+} as const;
 
 export function ThemeTokens() {
   return (
     <StyledProvider config={customConfig}>
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         <StyledThemeTokens />
-        <StyledThemeTokens bg="$primary600" />
+        <StyledThemeTokens bg="$primary500" />
       </View>
     </StyledProvider>
   );
