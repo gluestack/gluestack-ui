a8223c1a42cef0ed9205b4f74df146ca593f37fd
madhav23bansal <madhavb230100@gmail.com>
2023-01-11 11:46:57 +0530

    fix: config file path

--- packages/gluestack-design-system/src/core/AppProvider/index.tsx
+++ packages/gluestack-design-system/src/core/AppProvider/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GluestackUIProvider } from '@gluestack/ui';
-import { config } from '../../../gluestack.config';
+import { config } from './gluestack.config';
+
 export const AppProvider = ({ children, ...props }: any) => {
   return (
     <GluestackUIProvider config={config} {...props}>
