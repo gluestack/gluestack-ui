6b9d918ee51e048025218aafd60c9750a090c89e
Viraj-10 <virajjoshi132@gmail.com>
2023-01-12 17:41:52 +0530

    feat: added variant and sizes docs

---
+++ example/storybook/src/api/VariantsAndSizes/BaseStyleVariantSizes.stories.tsx
@@ -0,0 +1,11 @@
+import type { ComponentMeta } from '@storybook/react-native';
+import { BaseStyleVariantSizes } from './BaseStyleVariantSizes';
+const MyBaseStyleVariantSizesMeta: ComponentMeta<typeof BaseStyleVariantSizes> =
+  {
+    title: 'api/stories/BaseStyleVariantSizes',
+    component: BaseStyleVariantSizes,
+  };
+
+export { BaseStyleVariantSizes } from './BaseStyleVariantSizes';
+
+export default MyBaseStyleVariantSizesMeta;
