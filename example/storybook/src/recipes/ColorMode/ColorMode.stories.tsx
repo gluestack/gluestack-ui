19cf7d01b1dcca7199258b7cbe2d0c84a204b95f
Viraj-10 <virajjoshi132@gmail.com>
2023-01-12 19:06:56 +0530

    refactor: deleted stories

--- example/storybook/src/recipes/ColorMode/ColorMode.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { ColorMode } from './ColorMode';
-const MyColorModeMeta: ComponentMeta<typeof ColorMode> = {
-  title: 'recipes/ColorMode',
-  component: ColorMode,
-};
-
-export { ColorMode } from './ColorMode';
-export default MyColorModeMeta;
--- example/storybook/src/recipes/Descendants/Descendants.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { Descendants } from './Descendants';
-const MyDescendantsMeta: ComponentMeta<typeof Descendants> = {
-  title: 'recipes/Descendants',
-  component: Descendants,
-};
-
-export { Descendants } from './Descendants';
-export default MyDescendantsMeta;
