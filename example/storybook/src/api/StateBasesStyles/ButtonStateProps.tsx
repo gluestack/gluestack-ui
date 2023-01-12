5ea7670fbd5afc08c51e3b315e9176eb9b8a4663
Viraj-10 <virajjoshi132@gmail.com>
2023-01-12 17:40:50 +0530

    feat: added state base style docs

---
+++ example/storybook/src/api/StateBasesStyles/ButtonStateProps.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Pressable, Text } from 'react-native';
+import { styled } from '@dank-style/react';
+import { Wrapper } from '../../components/Wrapper';
+import { useState } from 'react';
+const StyledButtonStateProps = styled(
+  Pressable,
+  {
+    baseStyle: {
+      style: {
+        bg: '$primary600',
+        p: '$3',
+      },
+      descendants: {
+        _text: {
+          style: {
+            color: '$white',
+          },
+        },
+      },
+      state: {
+        hover: {
+          style: {
+            bg: '$primary700',
+          },
+        },
+      },
+    },
+  },
+  { descendantStyle: ['_text'] }
+);
+
+const StyledButtonText = styled(
+  Text,
+  {},
+  {
+    ancestorStyle: ['_text'],
+  }
+);
+
+export function ButtonStateProps({ ...args }: any) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Wrapper>
+      <StyledButtonStateProps
+        onHoverIn={() => setIsHovered(true)}
+        onHoverOut={() => setIsHovered(false)}
+        states={{
+          hover: isHovered,
+        }}
+        {...args}
+      >
+        <StyledButtonText>Md Button</StyledButtonText>
+      </StyledButtonStateProps>
+    </Wrapper>
+  );
+}
