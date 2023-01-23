5b2171ee06818f1b84c1adfa973bb8caea62ccc1
Meenu Makkar <makkarmini007@gmail.com>
2023-01-23 16:25:24 +0530

    feat: deleted access packages from universa11y

--- box/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import type React from 'react';
-import { Box as BoxMain } from './Box';
-
-export function createBox<T>({ Root }: { Root: React.ComponentType<T> }) {
-  const Box = BoxMain(Root);
-  Box.displayName = 'Box';
-  return Box;
-}
--- box/src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface InterfaceBoxProps {
-  /**
-   * Renders components as Box children. Accepts a JSX.Element or an array of JSX.Element. */
-  children?: string | JSX.Element | JSX.Element[];
-  // gap?: ResponsiveValue<number | string>;
-}
-
-export type IBoxProps = InterfaceBoxProps;
