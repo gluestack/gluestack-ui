8ca45ca1b3f3d119e01d9a86246f7bfeb3d6c0d5
Viraj-10 <virajjoshi132@gmail.com>
2023-01-25 18:06:03 +0530

    feat: removing unwanted stories

--- example/storybook/src/recipes/StateColorMode/StateColorMode.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { StateColorMode } from './StateColorMode';
-const MyStateColorModeMeta: ComponentMeta<typeof StateColorMode> = {
-  title: 'recipes/StateColorMode',
-  component: StateColorMode,
-};
-
-export { StateColorMode } from './StateColorMode';
-export default MyStateColorModeMeta;
--- example/storybook/src/recipes/StateMediaQuery/StateMediaQuery.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { StateMediaQuery } from './StateMediaQuery';
-const MyStateMediaQueryMeta: ComponentMeta<typeof StateMediaQuery> = {
-  title: 'recipes/StateMediaQuery',
-  component: StateMediaQuery,
-};
-
-export { StateMediaQuery } from './StateMediaQuery';
-export default MyStateMediaQueryMeta;
