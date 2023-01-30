791520dc063050e4bde711f65a0b1a409ff0b738
Meenu Makkar <makkarmini007@gmail.com>
2023-01-30 16:00:39 +0530

    feat: example fixes

--- example/storybook/src/components/AlertDialog/AlertDialog.tsx
+++ example/storybook/src/components/AlertDialog/AlertDialog.tsx
@@ -51,11 +51,11 @@ export const AlertDialog = () => {
           </AlertDialogTemp.Header>
           <AlertDialogTemp.Body>
             <Text>
-              Create a 'Return Request' under “My Orders” section of
+              {`Create a 'Return Request' under “My Orders” section of
               App/Website. Follow the screens that come up after tapping on the
               'Return’ button. Please make a note of the Return ID that we
               generate at the end of the process. Keep the item ready for pick
-              up or ship it to us basis on the return mode.
+              up or ship it to us basis on the return mode.`}
             </Text>
           </AlertDialogTemp.Body>
           <AlertDialogTemp.Footer>
