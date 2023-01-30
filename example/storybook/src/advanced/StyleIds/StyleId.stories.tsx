2111fc5a7e7a4946518128413427cd0651ce93b5
Rohit Singh <rohit99.ind@gmail.com>
2023-01-30 13:17:03 +0530

    fix: update docs pages

--- example/storybook/src/advanced/StyleIds/StyleId.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react-native';
-import { StyleId } from './StyleId';
-const MyStyleIdMeta: ComponentMeta<typeof StyleId> = {
-  title: 'advance/stories/StyleId',
-  component: StyleId,
-};
-
-export { StyleId } from './StyleId';
-export default MyStyleIdMeta;
