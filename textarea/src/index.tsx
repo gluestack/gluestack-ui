840468813901eedc64631941f77ef41cc3abfee1
Suraj Ahmed <suraj@geekyants.com>
2023-01-31 11:01:24 +0530

    fix: switch and textarea typing

--- textarea/src/index.tsx
+++ textarea/src/index.tsx
@@ -1,15 +1,22 @@
 import { TextArea } from './TextArea';
 import { TextAreaRoot } from './TextAreaRoot';
+import type { ITextAreaComponentType } from './types';
 
 // const TextAreaTemp = TextArea as any;
 // TextAreaTemp.Root = TextAreaRoot;
 
-export const createTextArea = ({ Root, Input }: any) => {
+export function createTextArea<TextAreaProps, InputProps>({
+  Root,
+  Input,
+}: {
+  Root: React.ComponentType<TextAreaProps>;
+  Input: React.ComponentType<InputProps>;
+}) {
   const TextAreaTemp = TextAreaRoot(Input) as any;
   TextAreaTemp.Input = TextArea(Root) as any;
 
   TextAreaTemp.displayName = 'TextArea';
   TextAreaTemp.Input.displayName = 'TextArea.Input';
 
-  return TextAreaTemp;
-};
+  return TextAreaTemp as ITextAreaComponentType<TextAreaProps, InputProps>;
+}
