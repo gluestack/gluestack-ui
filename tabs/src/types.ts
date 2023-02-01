ff47ff3105eefec91c0b3c769e3f0ee74a6e0db5
Suraj Ahmed <suraj@geekyants.com>
2023-02-01 12:04:25 +0530

    fix: tabs typing

--- tabs/src/types.ts
+++ tabs/src/types.ts
@@ -1,12 +1,21 @@
+interface ITabProps {
+  value?: string;
+}
+interface ITabsProps {
+  value?: string;
+}
+interface ITabPanelProps {
+  value?: string;
+}
 export type ITabsComponentType<
   TabsProps,
   TabProps,
   TabPanelsProps,
   TabListProps,
   TabPanelProps
-> = ((props: TabsProps) => JSX.Element) & {
-  Tab: (props: TabProps) => JSX.Element;
+> = ((props: TabsProps & ITabsProps) => JSX.Element) & {
+  Tab: (props: TabProps & ITabProps) => JSX.Element;
   TabPanels: (props: TabPanelsProps) => JSX.Element;
   TabList: (props: TabListProps) => JSX.Element;
-  TabPanel: (props: TabPanelProps) => JSX.Element;
+  TabPanel: (props: TabPanelProps & ITabPanelProps) => JSX.Element;
 };
