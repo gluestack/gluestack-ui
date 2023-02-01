834f52df02934255795f7c52abbfcf02e3ffab3c
Suraj Ahmed <suraj@geekyants.com>
2023-02-01 11:47:19 +0530

    fix: tabs typing

--- tabs/src/index.tsx
+++ tabs/src/index.tsx
@@ -1,34 +1,35 @@
 import type React from 'react';
 import { Tab as TabHOC } from './Tab';
-import { TabList } from './TabList';
-import { TabPanel } from './TabPanel';
-import { TabPanels } from './TabPanels';
+import { TabList as TabListMain } from './TabList';
+import { TabPanel as TabPanelMain } from './TabPanel';
+import { TabPanels as TabPanelsMain } from './TabPanels';
 import { Tabs } from './Tabs';
+import type { ITabsComponentType } from './types';
 
 export const createTabs = <
-  StyledTabsProps,
-  StyledTabProps,
-  StyledTabPanelsProps,
-  StyledTabListProps,
-  StyledTabPanelProps
+  TabsProps,
+  TabProps,
+  TabPanelsProps,
+  TabListProps,
+  TabPanelProps
 >({
   Root,
   Tab,
-  Panels,
-  List,
-  Panel,
+  TabPanels,
+  TabList,
+  TabPanel,
 }: {
-  Root: React.ComponentType<StyledTabsProps>;
-  Tab: React.ComponentType<StyledTabProps>;
-  Panels: React.ComponentType<StyledTabPanelsProps>;
-  List: React.ComponentType<StyledTabListProps>;
-  Panel: React.ComponentType<StyledTabPanelProps>;
+  Root: React.ComponentType<TabsProps>;
+  Tab: React.ComponentType<TabProps>;
+  TabPanels: React.ComponentType<TabPanelsProps>;
+  TabList: React.ComponentType<TabListProps>;
+  TabPanel: React.ComponentType<TabPanelProps>;
 }) => {
   const TabsMain = Tabs(Root) as any;
   TabsMain.Tab = TabHOC(Tab);
-  TabsMain.TabPanels = TabPanels(Panels);
-  TabsMain.TabList = TabList(List);
-  TabsMain.TabPanel = TabPanel(Panel);
+  TabsMain.TabPanels = TabPanelsMain(TabPanels);
+  TabsMain.TabList = TabListMain(TabList);
+  TabsMain.TabPanel = TabPanelMain(TabPanel);
 
   TabsMain.displayName = 'Tabs';
   TabsMain.Tab.displayName = 'Tab';
@@ -36,5 +37,11 @@ export const createTabs = <
   TabsMain.TabList.displayName = 'List';
   TabsMain.TabPanel.displayName = 'Panel';
 
-  return TabsMain;
+  return TabsMain as ITabsComponentType<
+    TabsProps,
+    TabProps,
+    TabPanelsProps,
+    TabListProps,
+    TabPanelProps
+  >;
 };
