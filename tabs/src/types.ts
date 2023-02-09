37c831da3232d674883e186981d46665be826af8
Viraj-10 <virajjoshi132@gmail.com>
2023-02-09 16:14:04 +0530

    feat: tab title icon

--- tabs/src/types.ts
+++ tabs/src/types.ts
@@ -12,10 +12,14 @@ export type ITabsComponentType<
   TabProps,
   TabPanelsProps,
   TabListProps,
-  TabPanelProps
+  TabPanelProps,
+  TabTitleProps,
+  TabIconProps
 > = ((props: TabsProps & ITabsProps) => JSX.Element) & {
   Tab: (props: TabProps & ITabProps) => JSX.Element;
   TabPanels: (props: TabPanelsProps) => JSX.Element;
   TabList: (props: TabListProps) => JSX.Element;
   TabPanel: (props: TabPanelProps & ITabPanelProps) => JSX.Element;
+  TabTitle: (props: TabTitleProps) => JSX.Element;
+  TabIcon: (props: TabIconProps) => JSX.Element;
 };
