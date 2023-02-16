dfdd1ac69a8b3005e338ce7a0cc9db74dbdad8f7
Suraj Ahmed <suraj@geekyants.com>
2023-02-16 18:02:39 +0530

    fix: repo structure

--- example/storybook/src/components/Wrapper.tsx
+++ example/storybook/src/components/Wrapper.tsx
@@ -1,83 +1,66 @@
 import React from 'react';
-import {
-  AppProvider,
-  IconButton,
-  Box,
-  // Button,
-  Center,
-  MoonIcon,
-  SunIcon,
-} from '@gluestack/design-system';
-// import { StyledProvider } from '@gluestack/ui-styled';
-// import { set, get } from '@dank-style/color-mode';
-import { Platform } from 'react-native';
-import { useDarkMode } from '../hooks/useDarkMode';
-// import { Button } from './Button/Button.stories';
+import { View } from 'react-native';
+
+// import { Platform } from 'react-native';
+// import { useDarkMode } from '../hooks/useDarkMode';
 
 // window['setTheme'] = set;
 // window['getTheme'] = get;
-const Wrapper = ({ children, ...props }: any) => {
-  let value = false;
-  // if (Platform.OS === 'web') {
-  value = useDarkMode();
-  // }
-  // set(value ? 'dark' : 'light');
-  // useEffect(() => {
-  //   set('light');
-  //   onChange((colorMode) => {
-  //     setIsDark(colorMode == 'dark' ? true : false);
-  //   });
-  // }, []);
-  const [isDark, setIsDark] = React.useState(false);
+// const Wrapper = ({ children, ...props }: any) => {
+// let value = false;
+// // if (Platform.OS === 'web') {
+// value = useDarkMode();
+// // }
+// // set(value ? 'dark' : 'light');
+// // useEffect(() => {
+// //   set('light');
+// //   onChange((colorMode) => {
+// //     setIsDark(colorMode == 'dark' ? true : false);
+// //   });
+// // }, []);
+// const [isDark, setIsDark] = React.useState(false);
 
-  function getColorMode() {
-    if (Platform.OS === 'web') {
-      return value ? 'dark' : 'light';
-    } else {
-      return isDark ? 'dark' : 'light';
-    }
-  }
+// function getColorMode() {
+//   if (Platform.OS === 'web') {
+//     return value ? 'dark' : 'light';
+//   } else {
+//     return isDark ? 'dark' : 'light';
+//   }
+// }
 
+// return <>hello</>;
+// return (
+//   <AppProvider {...props} colorMode={getColorMode()}>
+//     {/* <Center>{children}</Center> */}
+//   </AppProvider>
+// );
+// };
+
+// Wrapper.displayName = 'GluestackUIProvider';
+
+import { config } from '../gluestack.config';
+import { StyledProvider } from '@dank-style/react';
+import { createProvider } from '@universa11y/provider';
+
+const Provider = createProvider({ StyledProvider }) as any;
+Provider.displayName = 'Provider';
+
+const Wrapper = ({ children }: any) => {
   return (
-    <AppProvider {...props} colorMode={getColorMode()}>
-      <Box
-        h="100%"
-        sx={{
-          _dark: {
-            bg: '$backgroundDark950',
-          },
+    <Provider config={config} colorMode="light">
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+          height: '100%',
+          width: '100%',
         }}
       >
-        {Platform.OS !== 'web' ? (
-          <Box zIndex={1} position="absolute" top="$2" right="$2">
-            {isDark ? (
-              <IconButton
-                p="$4"
-                // sx={{ backgroundColor: 'transparent' }}
-                onPress={() => setIsDark(!isDark)}
-              >
-                <SunIcon color="$white" />
-              </IconButton>
-            ) : (
-              <IconButton
-                onPress={() => setIsDark(!isDark)}
-                p="$4"
-                // sx={{ backgroundColor: 'transparent' }}
-              >
-                <MoonIcon color="$black" />
-              </IconButton>
-            )}
-          </Box>
-        ) : (
-          <></>
-        )}
-        <Center h="100%">{children}</Center>
-      </Box>
-      {/* <Center>{children}</Center> */}
-    </AppProvider>
+        {children}
+      </View>
+    </Provider>
   );
 };
-
-Wrapper.displayName = 'GluestackUIProvider';
 
 export default Wrapper;
