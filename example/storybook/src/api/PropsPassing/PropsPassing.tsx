87f2b107e4c064b17f0d757736a05c8cdad1e3ba
ankit-tailor <tailorankit3345@gmail.com>
2023-02-17 17:55:15 +0530

    feat: passing props example

--- example/storybook/src/api/PropsPassing/PropsPassing.tsx
+++ example/storybook/src/api/PropsPassing/PropsPassing.tsx
@@ -1,58 +1,103 @@
-import React, { memo, useState } from 'react';
-import { View, Pressable, Text } from 'react-native';
+//@ts-nocheck
+import React, { memo, useEffect, useState } from 'react';
 import { styled } from '@dank-style/react';
 import { Wrapper } from '../../components/Wrapper';
-import { get, set } from '@dank-style/color-mode';
 import { Motion } from '@legendapp/motion';
 
 const StyledMotionView = memo(
   styled(
-    Motion.Pressable,
+    Motion.View,
     {
-      'w': 200,
+      'w': 100,
       'h': 200,
-      'bg': '$blue800',
+      'bg': '$blue500',
       'props': {
-        style: {
-          backgroundColor: 'red',
-        },
+        initial: { y: -150 },
         animate: {
           opacity: 0.5,
+          y: 50,
+          scale: 1,
         },
       },
       ':hover': {
-        w: 400,
-        //@ts-ignore
         props: {
           animate: {
             opacity: 1,
           },
         },
       },
+      '_text': {
+        'props': {
+          animate: {
+            scale: 1,
+          },
+        },
+        ':hover': {
+          props: {
+            animate: {
+              scale: 1.01,
+            },
+          },
+        },
+      },
+      'variants': {
+        variant: {
+          solid: {
+            bg: '$green500',
+          },
+          subtle: {
+            bg: '$green200',
+          },
+        },
+      },
+      'defaultProps': {
+        variant: 'solid',
+      },
     },
-    {}
+    {
+      descendantStyle: ['_text'],
+    }
   )
+);
+
+const StlyedText = styled(
+  Motion.View,
+  {},
+  {
+    ancestorStyle: ['_text'],
+  }
 );
 
 export function PropsPassing() {
   const [hover, setHover] = useState(false);
 
+  const ref = React.useRef(null);
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.addEventListener('mouseover', () => {
+        setHover(true);
+      });
+      ref.current.addEventListener('mouseout', () => {
+        setHover(false);
+      });
+    }
+  }, []);
+
   return (
     <Wrapper>
       <StyledMotionView
-        onHoverIn={() => {
-          setHover(true);
-        }}
-        onHoverOut={() => {
-          setHover(false);
-        }}
+        ref={ref}
+        variant="subtle"
         /*        animate={{
           x: value * 100,
           opacity: value ? 1 : 0.2,
           scale: value ? 1 : 0.5,
         }}*/
         states={{ hover: hover }}
-      />
+      >
+        <StlyedText>Hello World</StlyedText>
+      </StyledMotionView>
     </Wrapper>
   );
 }
