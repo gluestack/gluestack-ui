90c076c8ad38d001f413b8f1f637bce6ec47f77d
ankit-tailor <tailorankit3345@gmail.com>
2023-02-23 15:13:18 +0530

    feat: fonts plugin example

---
+++ example/storybook/src/plugins/FontsPlugin/FontsPlugin.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStyled, FontResolver } from '@dank-style/react';
+import { Wrapper } from '../../components/Wrapper';
+import { Text } from 'react-native';
+
+const styledFonts = createStyled([new FontResolver()]);
+
+const StyledText = styledFonts(Text, {
+  fontFamily: 'Nunito Sans',
+  fontWeight: 800,
+  fontStyle: 'normal',
+  fontSize: '$xl',
+});
+
+export function FontsPlugin() {
+  return (
+    <Wrapper>
+      <StyledText>Hello world</StyledText>
+    </Wrapper>
+  );
+}
+
+export default FontsPlugin;
