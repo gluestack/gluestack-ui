d86c966522f35f642897bed92f32922c7f7d46c0
Suraj Ahmed <suraj@geekyants.com>
2023-02-23 17:45:36 +0530

    fix: default props and variant issue

--- packages/react/src/StyledProvider.tsx
+++ packages/react/src/StyledProvider.tsx
@@ -66,6 +66,7 @@ export const StyledProvider: React.FC<{
 
   let contextValue;
   if (Platform.OS === 'web') {
+    set(currentColorMode === 'dark' ? 'dark' : 'light');
     // This if statement technically breaks the rules of hooks, but is safe
     // because the condition never changes after mounting.
     // eslint-disable-next-line react-hooks/rules-of-hooks
