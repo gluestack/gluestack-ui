2dc86285a88095d78a82d9c30fae19acffd7603e
ankit-tailor <tailorankit3345@gmail.com>
2023-02-23 16:45:09 +0530

    fix: font resolution expo google fonts

--- packages/react/src/plugins/font-resolver.tsx
+++ packages/react/src/plugins/font-resolver.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { Platform } from 'react-native';
 import type { IStyled, IStyledPlugin } from '../createStyled';
 
+const fontWeights: any = {
+  '100': 'Thin',
+  '200': 'ExtraLight',
+  '300': 'Light',
+  '400': 'Regular',
+  '500': 'Medium',
+  '600': 'SemiBold',
+  '700': 'Bold',
+  '800': 'ExtraBold',
+  '900': 'Black',
+  '950': 'ExtraBlack',
+};
+
 interface FontPlugin {
   mapFonts?(style: any): any;
 }
@@ -18,10 +31,15 @@ export class FontResolver implements IStyledPlugin, FontPlugin {
         (typeof style.fontWeight === 'string' ||
           typeof style.fontWeight === 'number')
       ) {
-        fontFamilyValue = `${fontFamilyValue}_${style.fontWeight}`;
+        fontFamilyValue = `${fontFamilyValue}_${style.fontWeight}${
+          fontWeights[style.fontWeight]
+        }`;
       }
       if (style.fontStyle && typeof style.fontStyle === 'string') {
-        fontFamilyValue = `${fontFamilyValue}_${style.fontStyle}`;
+        const fontStyle = style.fontStyle.replace(/^\w/, (c: any) =>
+          c.toUpperCase()
+        );
+        fontFamilyValue = `${fontFamilyValue}_${fontStyle}`;
       }
 
       style.fontFamily = fontFamilyValue;
