f53dd41942b4ae9dd614bc3bc7385b4f41a985f6
madhav23bansal <madhavb230100@gmail.com>
2023-03-06 13:12:16 +0530

    fix: components

---
+++ example/storybook/src/components/DataDisplay/Badge/Badge.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Center, VStack, Badge } from '../../../ui-components';
+import Wrapper from '../../Wrapper';
+
+export const MyBadgeExample = ({
+  // variant = 'subtle',
+  text = 'Badge',
+  ...props
+}: any) => {
+  return (
+    <Wrapper>
+      <Center>
+        <Badge {...props}>
+          <Badge.Text>{text}</Badge.Text>
+          {/* <Badge.Icon sx={{ pl: 4 }}></Badge.Icon> */}
+        </Badge>
+      </Center>
+    </Wrapper>
+  );
+};
+
+export { Center, VStack, Badge };
---
+++ example/storybook/src/components/Feedback/Progress/Progress.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Wrapper from '../../Wrapper';
+import { Center, Progress } from '../../../ui-components';
+
+export const ProgressBasicStory = ({ value = 50, ...props }: any) => {
+  return (
+    <Wrapper>
+      <Center w="100%" h="100%" alignItems="center" justifyContent="center">
+        <Progress w="70%" value={value} {...props}>
+          <Progress.FilledTrack />
+        </Progress>
+      </Center>
+    </Wrapper>
+  );
+};
+
+export { Progress };
