836e078b1e6ccb42609d0d536999f66ff5d94a11
Meenu Makkar <makkarmini007@gmail.com>
2023-03-06 11:59:44 +0530

    feat: resolved

--- transitions/babel.config.js
+++ transitions/babel.config.js
@@ -10,15 +10,18 @@ module.exports = function (api) {
             'module-resolver',
             {
               alias: {
-                // ['@gluestack-ui/react-native-aria']: path.resolve(
-                //   __dirname,
-                //   '../react-native-aria/src'
-                // ),
-                // ['@gluestack-ui/overlay']: path.resolve(
-                //   __dirname,
-                //   '../overlay/src'
-                // ),
-                // ['@gluestack-ui/utils']: path.resolve(__dirname, '../utils/src'),
+                ['@gluestack-ui/react-native-aria']: path.resolve(
+                  __dirname,
+                  '../react-native-aria/src'
+                ),
+                ['@gluestack-ui/overlay']: path.resolve(
+                  __dirname,
+                  '../overlay/src'
+                ),
+                ['@gluestack-ui/utils']: path.resolve(
+                  __dirname,
+                  '../utils/src'
+                ),
                 // For development, we want to alias the library to the source
               },
             },
