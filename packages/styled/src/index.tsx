da59232f17f0e3c5562f3340d02221a7f4acd50e
Meenu Makkar <makkarmini007@gmail.com>
2023-03-20 15:41:47 +0530

    feat: styled packeg

---
+++ packages/styled/src/index.tsx
@@ -0,0 +1,12 @@
+import { createStyled, FontResolver } from '@dank-style/react';
+
+export * from '@dank-style/react';
+
+// const fontMapper = (style: any) => {
+// };
+
+export const styled: any = createStyled([
+  new FontResolver({
+    // mapFonts: fontMapper,
+  }),
+]);
