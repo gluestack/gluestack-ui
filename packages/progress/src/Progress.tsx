cd5e97e0c9a234f577def7e7f3d4f715a4e1b508
Rohit Singh <rohit99.ind@gmail.com>
2023-03-22 13:33:57 +0530

    fix: uselink and progress aria

--- packages/progress/src/Progress.tsx
+++ packages/progress/src/Progress.tsx
@@ -2,6 +2,38 @@ import React, { forwardRef } from 'react';
 import { ProgressProvider } from './ProgressContext';
 import type { IProgressProps } from './types';
 
+const useProgress = ({
+  min,
+  max,
+  value,
+}: {
+  min: number;
+  max: number;
+  value: number;
+}) => {
+  const valueWidth =
+    value < max && value > min
+      ? ((value - min) / (max - min)) * 100
+      : value > min
+      ? 100
+      : 0;
+
+  return {
+    'accessible': true,
+    'role': 'progressbar',
+    'aria-valuemin': min,
+    'aria-valuemax': max,
+    'aria-valuenow': valueWidth,
+    'aria-valuetext': `${valueWidth}%`,
+    valueWidth,
+    'accessibilityRole': 'progressbar',
+    'accessibilityValueNow': valueWidth,
+    'accessibilityValueMin': min,
+    'accessibilityValueMax': max,
+    'accessibilityValueText': `${valueWidth}%`,
+  };
+};
+
 export function Progress<StyledProgressProps>(
   StyledProgress: React.ComponentType<StyledProgressProps>
 ) {
@@ -10,26 +42,19 @@ export function Progress<StyledProgressProps>(
       { children, min = 0, max = 100, value = 0, ...props }: IProgressProps,
       ref: any
     ) => {
-      const valueWidth =
-        value < max && value > min
-          ? ((value - min) / (max - min)) * 100
-          : value > min
-          ? 100
-          : 0;
+      const progressProps = useProgress({ min, max, value });
 
       return (
         <StyledProgress
           ref={ref}
-          accessible
-          accessibilityRole="progressbar"
-          accessibilityValue={{
-            min: min,
-            max: max,
-            now: valueWidth,
-          }}
+          {...progressProps}
           {...(props as StyledProgressProps)}
         >
-          <ProgressProvider min={min} max={max} valueWidth={valueWidth}>
+          <ProgressProvider
+            min={min}
+            max={max}
+            valueWidth={progressProps.valueWidth}
+          >
             {children}
           </ProgressProvider>
         </StyledProgress>
