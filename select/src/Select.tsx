ee71f66a8fc97098eb690b8ddb149cdb7beb8fc6
mayank-96 <pagarmayank07@gmail.com>
2023-03-22 18:53:44 +0530

    fix: state prop

--- select/src/Select.tsx
+++ select/src/Select.tsx
@@ -11,6 +11,8 @@ export const Select = (StyledSelect: any) =>
         isDisabled,
         isInvalid,
         isFocusVisible: isFocusVisibleProp,
+        isFocused: isFocusedProp,
+        isHovered: isHoveredProp,
         ...props
       }: any,
       ref: any
@@ -23,8 +25,8 @@ export const Select = (StyledSelect: any) =>
       return (
         <SelectContext.Provider
           value={{
-            isHovered: isHovered,
-            isFocused: isFocused,
+            isHovered: isHovered || isHoveredProp,
+            isFocused: isFocused || isFocusedProp,
             isDisabled: isDisabled,
             isReadOnly: isReadOnly,
             isInvalid: isInvalid,
@@ -39,8 +41,8 @@ export const Select = (StyledSelect: any) =>
             ref={ref}
             accessibilityRole="button"
             states={{
-              hover: isHovered,
-              active: isFocused,
+              hover: isHovered || isHoveredProp,
+              active: isFocused || isFocusedProp,
               disabled: isDisabled,
               invalid: isInvalid,
               readonly: isReadOnly,
