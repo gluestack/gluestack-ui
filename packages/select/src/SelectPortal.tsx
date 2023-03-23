7af7d965606fa068eca76777860cc631636e21a9
mayank-96 <pagarmayank07@gmail.com>
2023-03-23 19:46:57 +0530

    fix: web fixes

--- packages/select/src/SelectPortal.tsx
+++ packages/select/src/SelectPortal.tsx
@@ -1,24 +1,92 @@
 import React, { forwardRef } from 'react';
 import { SelectContext, SelectPortalContext } from './SelectContext';
+import { StyleSheet, Platform } from 'react-native';
+import { mergeRefs } from '@gluestack-ui/utils';
 
 export const SelectPortal = (Actionsheet: any) =>
   forwardRef(({ children, ...props }: any, ref: any) => {
-    const { isOpen, handleClose, closeOnOverlayClick, ...portalProps } =
-      React.useContext(SelectContext);
+    const {
+      isOpen,
+      handleClose,
+      closeOnOverlayClick,
+      isDisabled,
+      hoverRef,
+      hoverProps,
+      isReadOnly,
+      focusProps,
+      setValue,
+      value,
+      setFocused,
+      ...portalProps
+    } = React.useContext(SelectContext);
 
+    const tempFix = '__GluestackPlaceholder__';
+
+    if (Platform.OS !== 'web') {
+      return (
+        <Actionsheet
+          isOpen={isOpen}
+          onClose={handleClose}
+          closeOnOverlayClick={closeOnOverlayClick}
+          {...props}
+          ref={ref}
+        >
+          <SelectPortalContext.Provider
+            value={{
+              isOpen,
+              handleClose,
+              closeOnOverlayClick,
+              isDisabled,
+              hoverRef,
+              hoverProps,
+              isReadOnly,
+              focusProps,
+              setValue,
+              value,
+              setFocused,
+              ...portalProps,
+            }}
+          >
+            {children}
+          </SelectPortalContext.Provider>
+        </Actionsheet>
+      );
+    }
     return (
-      <Actionsheet
-        isOpen={isOpen}
-        onClose={handleClose}
-        closeOnOverlayClick={closeOnOverlayClick}
-        {...props}
-        ref={ref}
-      >
-        <SelectPortalContext.Provider
-          value={{ handleClose: handleClose, ...portalProps }}
+      <>
+        <select
+          aria-readonly={isReadOnly}
+          disabled={isDisabled}
+          {...focusProps}
+          {...hoverProps}
+          //@ts-ignore
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          ref={mergeRefs([ref, hoverRef])}
+          value={value === null ? tempFix : value}
+          aria-label="placeholder"
+          style={StyleSheet.flatten([
+            {
+              appearance: 'none',
+              WebkitAppearance: 'none',
+              MozAppearance: 'none',
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              opacity: 0,
+              zIndex: 1,
+            },
+          ])}
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onBlur={() => {
+            setFocused(false);
+          }}
         >
           {children}
-        </SelectPortalContext.Provider>
-      </Actionsheet>
+        </select>
+      </>
     );
   });
--- packages/select/src/index.ts
+++ packages/select/src/index.ts
@@ -13,12 +13,21 @@ export const createSelect = (
 ) => {
   const Select = SelectMain(Root) as any;
   Select.Trigger = SelectTrigger(Trigger, Input);
-
   Select.Portal = SelectPortal(Actionsheet);
   Select.Backdrop = SelectBackdrop(Actionsheet);
   Select.Content = SelectContent(Actionsheet);
   Select.DragIndicator = SelectDragIndicator(Actionsheet);
   Select.DragIndicatorWrapper = SelectDragIndicatorWrapper(Actionsheet);
   Select.Item = SelectItem(Actionsheet);
+
+  Select.displayName = 'Select';
+  Select.Trigger.displayName = 'Select.Trigger';
+  Select.Portal.displayName = 'Select.Portal';
+  Select.Backdrop.displayName = 'Select.Backdrop';
+  Select.Content.displayName = 'Select.Content';
+  Select.DragIndicator.displayName = 'Select.DragIndicator';
+  Select.DragIndicatorWrapper.displayName = 'Select.DragIndicatorWrapper';
+  Select.Item.displayName = 'Select.Item';
+
   return Select as any;
 };
