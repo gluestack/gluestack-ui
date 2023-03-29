0978c7b5dba0d06677d8eb0c117fec0899eeb374
Viraj-10 <virajjoshi132@gmail.com>
2023-03-29 13:11:23 +0530

    fix: popover iskeyboarddismis

--- packages/popover/src/Popover.tsx
+++ packages/popover/src/Popover.tsx
@@ -24,6 +24,7 @@ export const Popover = (StyledPopover: any) =>
         shouldOverlapWithTrigger = false,
         crossOffset,
         offset,
+        isKeyboardDismissable = true,
         ...props
       }: any,
       ref: any
@@ -87,7 +88,7 @@ export const Popover = (StyledPopover: any) =>
           <Overlay
             isOpen={isOpen}
             onRequestClose={handleClose}
-            isKeyboardDismissable
+            isKeyboardDismissable={isKeyboardDismissable}
             // useRNModalOnAndroid
             useRNModal={useRNModal}
             unmountOnExit
