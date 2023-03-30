3fa568b610c1ed0db99b4adfb0a97be93ad89f36
mayank-96 <pagarmayank07@gmail.com>
2023-03-30 13:11:44 +0530

    feat: actionsheet content accessibility

--- packages/actionsheet/src/ActionsheetContent.tsx
+++ packages/actionsheet/src/ActionsheetContent.tsx
@@ -1,16 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { forwardRef } from 'react';
-import { Animated } from 'react-native';
+import { Animated, findNodeHandle, AccessibilityInfo } from 'react-native';
 import { ActionsheetContext } from './context';
 import { ActionsheetContentProvider } from './ActionsheetContentContext';
 import { FocusScope } from '@react-native-aria/focus';
+import { mergeRefs } from '@gluestack-ui/utils';
 
 function ActionsheetContent<T>(
   StyledActionsheetContent: React.ComponentType<T>
 ) {
   return forwardRef(
     ({ children, ...props }: T & { children?: any }, ref?: any) => {
-      const { handleClose, trapFocus } = React.useContext(ActionsheetContext);
+      const { handleClose, trapFocus, visible } =
+        React.useContext(ActionsheetContext);
       const pan = React.useRef(new Animated.ValueXY()).current;
       const sheetHeight = React.useRef(0);
 
@@ -18,6 +20,18 @@ function ActionsheetContent<T>(
         ActionsheetContext,
         handleClose,
       ]);
+
+      const contentRef = React.useRef(null);
+      React.useEffect(() => {
+        if (contentRef) {
+          const reactTag = findNodeHandle(contentRef.current);
+          if (reactTag) {
+            AccessibilityInfo.setAccessibilityFocus(reactTag);
+          }
+        }
+      }, [visible, contentRef]);
+
+      const mergedRef = mergeRefs([ref, contentRef]);
 
       return (
         <Animated.View
@@ -34,7 +48,7 @@ function ActionsheetContent<T>(
           pointerEvents="box-none"
         >
           <FocusScope contain={trapFocus} restoreFocus autoFocus>
-            <StyledActionsheetContent ref={ref} {...(props as T)}>
+            <StyledActionsheetContent ref={mergedRef} {...(props as T)}>
               <ActionsheetContentProvider
                 sheetHeight={sheetHeight}
                 pan={pan}
