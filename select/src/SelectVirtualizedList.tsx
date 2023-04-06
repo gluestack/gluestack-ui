af88e1aaf4a54e825e017bef5a9d7c328f9da9a5
mayank-96 <pagarmayank07@gmail.com>
2023-04-06 15:05:44 +0530

    fix: added virtualized list support

--- select/src/SelectVirtualizedList.tsx
+++ select/src/SelectVirtualizedList.tsx
@@ -2,22 +2,20 @@ import React, { forwardRef } from 'react';
 import { Platform } from 'react-native';
 
 export const SelectVirtualizedList = (StyledSelectVirtualizedList: any) =>
-  forwardRef(
-    ({ children, data, renderItem, getItem, ...props }: any, ref: any) => {
-      if (Platform.OS === 'web') {
-        return (
-          <>
-            {data.map((_: any, index: any) => {
-              const item = getItem(data, index);
-              return renderItem({ item });
-            })}
-          </>
-        );
-      }
+  forwardRef(({ children, ...props }: any, ref: any) => {
+    if (Platform.OS === 'web') {
       return (
-        <StyledSelectVirtualizedList {...props} ref={ref}>
-          {children}
-        </StyledSelectVirtualizedList>
+        <>
+          {props.data.map((_: any, index: any) => {
+            const item = props.getItem(props.data, index);
+            return props.renderItem({ item });
+          })}
+        </>
       );
     }
-  );
+    return (
+      <StyledSelectVirtualizedList {...props} ref={ref}>
+        {children}
+      </StyledSelectVirtualizedList>
+    );
+  });
