b22d0d11328f943f4f0fdef056855d892662ce24
ankit-tailor <tailorankit3345@gmail.com>
2023-04-11 18:00:47 +0530

    fix: checkbox native ids

--- spinner/src/Spinner.tsx
+++ spinner/src/Spinner.tsx
@@ -1,16 +1,22 @@
 import React, { forwardRef } from 'react';
+import type { ActivityIndicatorProps } from 'react-native';
 
 export function Spinner<StyledSpinnerProps>(
   StyledSpinner: React.ComponentType<StyledSpinnerProps>
 ) {
-  return forwardRef(({ ...props }: StyledSpinnerProps, ref: any) => {
-    return (
-      <StyledSpinner
-        {...(props as StyledSpinnerProps)}
-        ref={ref}
-        accessible
-        accessibilityLabel="loading"
-      />
-    );
-  });
+  return forwardRef(
+    (
+      { ...props }: Omit<StyledSpinnerProps, 'size'> & ActivityIndicatorProps,
+      ref: any
+    ) => {
+      return (
+        <StyledSpinner
+          {...(props as StyledSpinnerProps)}
+          ref={ref}
+          accessible
+          accessibilityLabel="loading"
+        />
+      );
+    }
+  );
 }
