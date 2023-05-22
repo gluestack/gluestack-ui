a8ea249edc07b77f9ab203e2ad679b4ec536de54
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-05-22 14:59:34 +0530

    fix:  "added link text"

--- example/storybook/src/components/Forms/Link/Link.tsx
+++ example/storybook/src/components/Forms/Link/Link.tsx
@@ -1,44 +1,14 @@
 import React from 'react';
 import Wrapper from '../../Wrapper';
-import { Link as LinkTemp, Text } from '../../../ui-components';
-
+import { Link as LinkTemp } from '../../../ui-components';
 export const Link = () => {
   return (
     <Wrapper>
-      <LinkTemp href="https://gluestack.io/" isExternal>
-        <Text
-        // sx={{
-        //   'fontWeight': '$normal',
-        //   'color': '$info600',
-        //   ':hover': {
-        //     textDecorationLine: 'underline',
-        //   },
-        //   ':active': {
-        //     textDecorationLine: 'underline',
-        //     color: '$info700',
-        //   },
-        //   ':pressed': {
-        //     textDecorationLine: 'underline',
-        //     color: '$info700',
-        //   },
-        //   '_dark': {
-        //     'color': '$info300',
-        //     ':hover': {
-        //       textDecorationLine: 'underline',
-        //       color: '$info300',
-        //     },
-        //     ':active': {
-        //       textDecorationLine: 'underline',
-        //       color: '$info200',
-        //     },
-        //   },
-        // }}
-        >
-          gluestack
-        </Text>
+      <LinkTemp href="https://google.com" isExternal>
+        <LinkTemp.Text>Gluestack</LinkTemp.Text>
       </LinkTemp>
     </Wrapper>
   );
 };
 
-export { Text, LinkTemp };
+export { LinkTemp };
