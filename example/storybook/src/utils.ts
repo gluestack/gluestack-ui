9aa2b2d4fc046185813f21333b7c212dba6620bb
Akash Gautam <akashgautam@geekyants.com>
2023-05-22 16:46:30 +0530

    fix: updated code

--- example/storybook/src/utils.ts
+++ example/storybook/src/utils.ts
@@ -1,9 +1,13 @@
 // Transform code To Remove Wrapper And Function Name
-export function transformedCode(code: string) {
-  if (code.includes('function')) {
+export function transformedCode(
+  code: string,
+  type: 'jsx' | 'function' = 'jsx',
+  componentName?: string
+) {
+  if (type === 'function') {
     return `function App() {
       ${code}
-    return <Wrapper><Example /></Wrapper>;
+    return <Wrapper><${componentName} /></Wrapper>;
   };`;
   }
   return `function App() {
