d5bf6c6684b82e0b4f31a5131d8bf147ced4b9d9
Viraj-10 <virajjoshi132@gmail.com>
2023-05-24 12:41:07 +0530

    feat: typing for global variant

--- packages/react/src/createConfig.ts
+++ packages/react/src/createConfig.ts
@@ -2,17 +2,71 @@ type myType<Aliases, Tokens> = {
   [key in keyof Aliases]?: keyof Tokens;
 };
 
+import type { ViewProps } from 'react-native';
 import type {
   StringifyToken,
   PropertyTokenType,
   RemoveNever,
   ExtendRNStyle,
   RNStyledProps,
+  COLORMODES,
+  IState,
+  PLATFORMS,
 } from './types';
-export const createConfig = <Aliases, Tokens>(
+
+export type SxPropsNew<Aliases, Tokens, Variants, PLATFORM = ''> = Partial<
+  RNStyledProps & AliasesProps<Aliases, Tokens>
+> & {
+  [Key in `_${COLORMODES}`]?: SxPropsNew<Aliases, Tokens, Variants, PLATFORM>;
+} & {
+  [Key in `:${IState}`]?: SxPropsNew<Aliases, Tokens, Variants, PLATFORM>;
+} & {
+  [Key in `_${PLATFORMS}`]?: SxPropsNew<Aliases, Tokens, Variants, Key>;
+} & {
+  [Key in `_${string & {}}`]?:
+    | SxPropsNew<Aliases, Tokens, Variants, PLATFORM>
+    | {
+        [key in string]?: any;
+      };
+};
+
+type CompoundVariant<Variants, Aliases, Tokens> = Partial<
+  {
+    [Key in keyof Variants]?: keyof Variants[Key];
+  } & {
+    value?: SxPropsNew<Aliases, Tokens, Variants>;
+  }
+>;
+
+export type StyledThemePropsNew<Variants, Aliases, Tokens> = AliasesProps<
+  Aliases,
+  Tokens
+> &
+  RNStyledProps &
+  AliasesProps<Aliases, Tokens> & {
+    variants?: VariantTypeNew<Variants, Aliases, Tokens>;
+    compoundVariants?: Array<CompoundVariant<Variants, Aliases, Tokens>>;
+  };
+
+export type VariantTypeNew<Variants, Aliases, Tokens> =
+  | {
+      [Key1 in keyof Variants]: {
+        [Key in keyof Variants[Key1] | (string & {})]?: Partial<
+          SxPropsNew<Aliases, Tokens, Variants> & {
+            [K in `@${keyof Tokens['mediaQueries']}`]?: SxPropsNew<
+              Aliases,
+              Tokens,
+              Variants
+            >;
+          }
+        >;
+      };
+    };
+
+export const createConfig = <Aliases, Tokens, Variants>(
   aliases: Aliases,
   tokens: Tokens,
-  globalStyle: AliasesProps<Aliases, Tokens>
+  globalStyle: Partial<StyledThemePropsNew<Variants, Aliases, Tokens>> // AliasesProps<Aliases, Tokens>
 ) => {
   return {
     aliases,
@@ -23,11 +77,12 @@ export const createConfig = <Aliases, Tokens>(
 
 export type AliasesProps<Aliases, Token> = RemoveNever<{
   [key in keyof Aliases]?: Aliases[key] extends keyof RNStyledProps
-    ?
-        | StringifyToken<keyof Token[PropertyTokenType[Aliases[key]]]>
+    ? //@ts-ignore
+      | StringifyToken<keyof Token[PropertyTokenType[Aliases[key]]]>
         | ExtendRNStyle<RNStyledProps, Aliases[key]>
     : never;
 }>;
+
 const config = createConfig(
   {
     bg: 'backgroundColor',
@@ -35,6 +90,7 @@ const config = createConfig(
     bgColor: 'backgroundColor',
     color: 'color',
     borderColor: 'borderColor',
+    borderWidth: 'borderWidth',
     shadowColor: 'shadowColor',
     shadowOffset: 'shadowOffset',
     shadowOpacity: 'shadowOpacity',
@@ -216,7 +272,26 @@ const config = createConfig(
     },
   } as const,
   {
-    bgColor: '$pink400',
-    w: '$1',
-  } as const
+    variants: {
+      variant: {
+        primary: {
+          '@sm': {
+            bg: '$pink100',
+          },
+        },
+      },
+      size: {
+        sm: {},
+      },
+    },
+    compoundVariants: [
+      {
+        variant: 'primary',
+        value: {
+          bg: '$pink100',
+          bgColor: '$pink100',
+        },
+      },
+    ],
+  }
 );
