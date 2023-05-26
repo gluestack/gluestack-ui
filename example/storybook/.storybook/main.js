04d765b68ae26a05236d9f84cac81910521ce3b3
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-05-26 13:11:12 +0530

    fix: updated config

--- example/storybook/.storybook/main.js
+++ example/storybook/.storybook/main.js
@@ -50,13 +50,19 @@ module.exports = {
         // path.resolve('../../', 'node_modules/@universa11y'),
         path.resolve('../../', 'node_modules/@gluestack-ui'),
         path.resolve('../../', 'node_modules/@gluestack/design-system'),
+        path.resolve('../../', 'node_modules/@gluestack-style/react'),
         path.resolve('./', 'node_modules/@gluestack-style/react'),
-        path.resolve('./', 'node_modules/@dank-style'),
+        path.resolve('../../', 'node_modules/@dank-style'),
         // path.resolve('./', 'node_modules/@gluestack-ui'),
       ],
       use: 'babel-loader',
     });
 
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
     // Return the altered config
     return config;
   },
--- example/storybook/babel.config.js
+++ example/storybook/babel.config.js
@@ -15,34 +15,19 @@ module.exports = function (api) {
                   __dirname,
                   '../../packages/react/src'
                 ),
-                ['@dank-style/react']: path.join(
-                  __dirname,
-                  '../../packages/react/src'
-                ),
-                ['@gluestack-style/convert-utility-to-sx']: path.join(
-                  __dirname,
-                  '../../packages/convert-utility-to-sx/src'
-                ),
-                ['@gluestack-style/css-injector']: path.join(
-                  __dirname,
-                  '../../packages/css-injector/src'
-                ),
-                ['@gluestack-style/cssify']: path.join(
-                  __dirname,
-                  '../../packages/cssify/src'
-                ),
-                ['@gluestack-style/color-mode']: path.join(
-                  __dirname,
-                  '../../packages/color-mode/src'
-                ),
-                ['@gluestack-style/animation-plugin']: path.join(
-                  __dirname,
-                  '../../packages/animation-plugin/src'
-                ),
-                ['@dank-style/animation-plugin']: path.join(
-                  __dirname,
-                  '../../packages/animation-plugin/src'
-                ),
+
+                // ['@dank-style/react']: path.join(
+                //   __dirname,
+                //   '../../packages/react/src'
+                // ),
+                // ['@gluestack-style/animation-plugin']: path.join(
+                //   __dirname,
+                //   '../../packages/animation-plugin/src'
+                // ),
+                // ['@dank-style/animation-plugin']: path.join(
+                //   __dirname,
+                //   '../../packages/animation-plugin/src'
+                // ),
               },
             },
           ]
