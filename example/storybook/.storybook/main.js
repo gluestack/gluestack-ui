1f5827085b19855a8efff4af403aa0b6d9f2b46a
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-05-26 16:44:58 +0530

    fix: updated design system version'

--- example/storybook/.storybook/main.js
+++ example/storybook/.storybook/main.js
@@ -50,9 +50,14 @@ module.exports = {
         // path.resolve('../../', 'node_modules/@universa11y'),
         path.resolve('../../', 'node_modules/@gluestack-ui'),
         path.resolve('../../', 'node_modules/@gluestack/design-system'),
+        path.resolve(
+          '../../',
+          'node_modules/@gluestack-style/animation-plugin'
+        ),
+        path.resolve('./', 'node_modules/@gluestack-style/animation-plugin'),
+        path.resolve('./', 'node_modules/@gluestack/design-system'),
         path.resolve('../../', 'node_modules/@gluestack-style/react'),
         path.resolve('./', 'node_modules/@gluestack-style/react'),
-        path.resolve('../../', 'node_modules/@dank-style'),
         // path.resolve('./', 'node_modules/@gluestack-ui'),
       ],
       use: 'babel-loader',
