b707ad6d73fca56bbc394bd1ec2ca550732e6b40
Mayank Pagar <pagarmayank07@gmail.com>
2023-05-31 20:21:10 +0530

    feat: styling

--- example/storybook/src/components/Forms/Input/Input.tsx
+++ example/storybook/src/components/Forms/Input/Input.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wrapper from '../../Wrapper';
-import { Input, Center } from '../../../ui-components';
+import { Input, Center, Icon, SearchIcon } from '../../../ui-components';
 
 const InputStory = ({ ...props }: any) => {
   const [value, setValue] = React.useState('');
@@ -15,6 +15,9 @@ const InputStory = ({ ...props }: any) => {
           value={value}
           placeholder="Enter Text here"
         />
+        <Input.Icon pr="$4">
+          <Icon as={SearchIcon} />
+        </Input.Icon>
       </Input>
     </Wrapper>
   );
