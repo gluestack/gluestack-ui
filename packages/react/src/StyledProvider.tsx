82546e024a468626b3c05f69b6ccfcbb697ab2cb
Ankit Tailor <44310861+ankit-tailor@users.noreply.github.com>
2023-06-08 17:27:23 +0530

    Revert "Fix/context"

--- packages/react/src/StyledProvider.tsx
+++ packages/react/src/StyledProvider.tsx
@@ -29,7 +29,6 @@ export const StyledProvider: React.FC<{
   children?: React.ReactNode;
   globalStyles?: any;
 }> = ({ config, colorMode, children, globalStyles }) => {
-  const id = React.useId();
   const currentConfig = React.useMemo(() => {
     //TODO: Add this later
     return platformSpecificSpaceUnits(config, Platform.OS);
@@ -75,8 +74,8 @@ export const StyledProvider: React.FC<{
   }
 
   let contextValue = React.useMemo(() => {
-    return { config: currentConfig, id };
-  }, [currentConfig, id]);
+    return { config: currentConfig };
+  }, [currentConfig]);
 
   return (
     <StyledContext.Provider value={contextValue}>
