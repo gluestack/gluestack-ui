db6a56ce4fe7a99a7dc7a7718c552c320e1d7cee
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-06-09 13:08:39 +0530

    fix: remove unwanted size

--- example/storybook/src/ui-components/Spinner/styled-components/Root.tsx
+++ example/storybook/src/ui-components/Spinner/styled-components/Root.tsx
@@ -4,32 +4,14 @@ import { ActivityIndicator } from 'react-native';
 export default styled(
   ActivityIndicator,
   {
-    variants: {
-      size: {
-        xs: {
-          h: '$3',
-          w: '$3',
-        },
-        sm: {
-          h: '$4',
-          w: '$4',
-        },
-        md: {
-          h: '$5',
-          w: '$5',
-        },
-        lg: {
-          h: '$6',
-          w: '$6',
-        },
-        xl: {
-          h: '$10',
-          w: '$10',
-        },
-      },
-    },
+    variants: {},
   },
   {
     resolveProps: ['color'],
+  },
+  {
+    propertyTokenMap: {
+      size: 'size',
+    },
   }
 );
