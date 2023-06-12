bfb0eb4c6fe5f97ee2442119465805aa7f569860
Akash Gautam <akashgautam@geekyants.com>
2023-06-12 16:00:29 +0530

    feat: textarea dev review and examples

--- textarea/src/TextArea.tsx
+++ textarea/src/TextArea.tsx
@@ -1,18 +1,39 @@
 import React, { forwardRef } from 'react';
+import { useFormControl } from '@gluestack-ui/form-control';
 import { useTextarea } from './TextareaContext';
 
 export const Textarea = (StyledTextarea: any) =>
   forwardRef(
-    ({ children, multiline = true, onKeyPress, ...props }: any, ref?: any) => {
+    (
+      {
+        children,
+        multiline = true,
+        onKeyPress,
+        accessibilityLabel = 'Input Field',
+        accessibilityHint,
+        accessibilityRole = 'text',
+        ...props
+      }: any,
+      ref?: any
+    ) => {
       const {
         isDisabled,
         isReadOnly,
         isFocused,
         isInvalid,
-        isRequired,
         isHovered,
+        isFocusVisible,
+        isRequired,
         handleFocus,
       } = useTextarea('TextareaContext');
+
+      const textareaProps = useFormControl({
+        isDisabled: props.isDisabled,
+        isInvalid: props.isInvalid,
+        isReadOnly: props.isReadOnly,
+        isRequired: props.isRequired,
+        nativeID: props.nativeID,
+      });
 
       return (
         <StyledTextarea
@@ -20,13 +41,27 @@ export const Textarea = (StyledTextarea: any) =>
           {...props}
           states={{
             focus: isFocused,
-            disabled: isDisabled,
             invalid: isInvalid,
             readonly: isReadOnly,
             required: isRequired,
             hover: isHovered,
+            focusVisible: isFocusVisible,
+            disabled: isDisabled || textareaProps.isDisabled,
           }}
+          accessible
+          accessibilityLabel={accessibilityLabel}
+          accessibilityHint={accessibilityHint}
+          accessibilityRole={accessibilityRole}
+          accessibilityRequired={isRequired || textareaProps.isRequired}
+          accessibilityInvalid={isInvalid || textareaProps.isInvalid}
+          accessibilityState={{
+            invalid: isInvalid || textareaProps.isInvalid,
+            disabled: isDisabled || textareaProps.isDisabled,
+            selected: isFocused,
+          }}
+          accessibilityElementsHidden={isDisabled}
           editable={isDisabled || isReadOnly ? false : true}
+          disabled={isDisabled || textareaProps.isDisabled}
           multiline={multiline}
           onKeyPress={(e: any) => {
             e.persist();
