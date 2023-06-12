06162699e4eae599904f9e8ff0f2da1167cbf03f
Viraj-10 <virajjoshi132@gmail.com>
2023-06-12 13:05:54 +0530

    fix: making hover ref optional with checks

--- packages/interactions/src/useHover.web.ts
+++ packages/interactions/src/useHover.web.ts
@@ -2,10 +2,10 @@ import { useHover as useHoverWeb, HoverProps } from '@react-aria/interactions';
 import { useEffect } from 'react';
 import { attachEventHandlersOnRef } from '@react-native-aria/utils';
 
-export const useHover = (props: HoverProps, ref: any) => {
+export const useHover = (props: HoverProps, ref?: any) => {
   let params = useHoverWeb(props);
   useEffect(() => {
-    attachEventHandlersOnRef(params.hoverProps, ref);
+    ref && ref.current && attachEventHandlersOnRef(params.hoverProps, ref);
   }, []);
 
   return params;
