b3085f0c504b61ba4c669980de3a79a6cdf7e0f2
Viraj-10 <virajjoshi132@gmail.com>
2023-06-13 17:17:02 +0530

    feat: checkbox migration

--- packages/checkbox/babel.config.js
+++ packages/checkbox/babel.config.js
@@ -10,10 +10,6 @@ module.exports = function (api) {
             'module-resolver',
             {
               alias: {
-                // ['@gluestack-ui/react-native-aria']: path.resolve(
-                //   __dirname,
-                //   '../react-native-aria/src'
-                // ),
                 ['@gluestack-ui/utils']: path.resolve(
                   __dirname,
                   '../utils/src'
