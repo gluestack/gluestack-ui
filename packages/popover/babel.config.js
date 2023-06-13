4283e4a370ae42226313f7d6b53878bf438df005
Viraj-10 <virajjoshi132@gmail.com>
2023-06-13 18:15:12 +0530

    feat: popover migration

--- packages/popover/babel.config.js
+++ packages/popover/babel.config.js
@@ -10,11 +10,6 @@ module.exports = function (api) {
             'module-resolver',
             {
               alias: {
-                // ['@gluestack-ui/react-native-aria']: path.resolve(
-                //   __dirname,
-                //   '../react-native-aria/src'
-                // ),
-
                 ['@gluestack-ui/overlay']: path.resolve(
                   __dirname,
                   '../overlay/src'
