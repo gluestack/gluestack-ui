062d756a4516c04484338f4f5263f76d60b17354
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-06-13 13:08:00 +0530

    fix: icon updated

--- icon/Icons/Add.tsx
+++
@@ -1,7 +0,0 @@
-import { createIcon } from '../src/createIcon';
-
-export const AddIcon = createIcon({
-  StyledIcon,
-  viewBox: '0 0 24 24',
-  d: 'M13.25 10.75V2H10.75V10.75H2V13.25H10.75V22H13.25V13.25H22V10.75H13.25Z',
-});
