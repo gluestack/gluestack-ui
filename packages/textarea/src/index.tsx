f87ee1808c3e9ad3b01af98a9ace6169e1b161e6
Suraj <surajahmedchoudhury@Surajs-MacBook-Air.local>
2023-06-13 12:02:51 +0530

    fix: textarea rename

--- packages/textarea/src/index.tsx
+++ packages/textarea/src/index.tsx
@@ -2,7 +2,24 @@ import { Textarea } from './Textarea';
 import { TextareaRoot } from './TextareaRoot';
 import type { ITextareaComponentType } from './types';
 
+//TODO: deprecate later
 export function createTextArea<Root, Input>({
+  Root,
+  Input,
+}: {
+  Root: React.ComponentType<Root>;
+  Input: React.ComponentType<Input>;
+}) {
+  const TextareaTemp = TextareaRoot(Root) as any;
+  TextareaTemp.Input = Textarea(Input);
+
+  TextareaTemp.displayName = 'Textarea';
+  TextareaTemp.Input.displayName = 'Textarea.Input';
+
+  return TextareaTemp as ITextareaComponentType<Root, Input>;
+}
+
+export function createTextarea<Root, Input>({
   Root,
   Input,
 }: {
