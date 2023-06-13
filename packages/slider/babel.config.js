29a83596f51391af1a049ce3db8f0a3e1ac55cac
Viraj-10 <virajjoshi132@gmail.com>
2023-06-13 18:36:36 +0530

    fix: slider migration

--- packages/slider/babel.config.js
+++ packages/slider/babel.config.js
@@ -10,10 +10,6 @@ module.exports = function (api) {
             'module-resolver',
             {
               alias: {
-                // ['@gluestack-ui/react-native-aria']: path.resolve(
-                //   __dirname,
-                //   '../react-native-aria/src'
-                // ),
                 // ['@gluestack-ui/utils']: path.resolve(__dirname, '../utils/src'),
                 // For development, we want to alias the library to the source
               },
