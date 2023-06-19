8d93f63ebd75606f963178fbe0dc64672a4c910b
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-06-19 18:26:40 +0530

    fix: styling

--- example/storybook/src/ui-components/Slider/styled-components/FilledTrack.tsx
+++ example/storybook/src/ui-components/Slider/styled-components/FilledTrack.tsx
@@ -6,21 +6,24 @@ export default styled(
   {
     'bg': '$primary600',
     'h': '100%',
-    ':focus': {
-      _web: {
-        outlineWidth: 6,
-        outlineColor: '$primary700',
-        outlineStyle: 'solid',
-        _dark: {
-          outlineColor: '$primary300',
-        },
-      },
-    },
+    // ':focus': {
+    //   _web: {
+    //     outlineWidth: 6,
+    //     outlineColor: '$primary700',
+    //     outlineStyle: 'solid',
+    //     _dark: {
+    //       outlineColor: '$primary300',
+    //     },
+    //   },
+    // },
     ':disabled': {
       bg: '$primary600_alpha60',
     },
     '_dark': {
-      bg: '$primary500',
+      'bg': '$primary300',
+      ':disabled': {
+        bg: '$primary600_alpha60',
+      },
     },
   },
   {}
