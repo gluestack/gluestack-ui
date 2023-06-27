3fd2d3698a6c34ff26c847c9b4198d714919c37d
Akash Gautam <akashgautam@geekyants.com>
2023-06-27 15:30:22 +0530

    fix: styling

--- example/storybook/src/ui-components/Popover/styled-components/Root.tsx
+++ example/storybook/src/ui-components/Popover/styled-components/Root.tsx
@@ -10,25 +10,17 @@ export default styled(
     alignItems: 'center',
     variants: {
       size: {
-        xs: {
-          _content: {
-            width: '60%',
-            maxWidth: 360,
-          },
-        },
+        xs: { _content: { width: '60%', maxWidth: 360 } },
         sm: { _content: { width: '70%', maxWidth: 420 } },
         md: { _content: { width: '80%', maxWidth: 510 } },
         lg: { _content: { width: '90%', maxWidth: 640 } },
-        full: {
-          _content: { width: '100%' },
-        },
+        full: { _content: { width: '100%' } },
       },
     },
 
     defaultProps: { size: 'md' },
 
     _web: {
-      //@ts-ignore
       pointerEvents: 'box-none',
     },
   },
