51c12ad4a2e90f66e4142d7cefa21544848d74e1
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-07-04 23:29:16 +0530

    fix: styling

--- example/storybook/src/ui-components/Slider/styled-components/FilledTrack.tsx
+++ example/storybook/src/ui-components/Slider/styled-components/FilledTrack.tsx
@@ -4,23 +4,42 @@ import { View } from 'react-native';
 export default styled(
   View,
   {
-    'bg': '$primary600',
+    'bg': '$primary500',
     'h': '100%',
     // ':focus': {
     //   _web: {
-    //     outlineWidth: 6,
-    //     outlineColor: '$primary700',
-    //     outlineStyle: 'solid',
-    //     _dark: {
-    //       outlineColor: '$primary300',
-    //     },
+    //     bg: '$primary600',
+    //     // outlineWidth: 6,
+    //     // outlineColor: '$primary700',
+    //     // outlineStyle: 'solid',
+    //     // _dark: {
+    //     //   outlineColor: '$primary300',
+    //     // },
     //   },
     // },
+    ':active': {
+      // outlineWidth: 4,
+      // // outlineColor: '$primary700',
+      // outlineStyle: 'solid',
+      bg: '$red400',
+      // ':hover': {
+      //   outlineColor: '$primary700',
+      // },
+
+      _dark: {
+        //outlineColor: '$primary400',
+        bg: '$primary400',
+      },
+    },
+
+    ':hover': {
+      bg: '$primary600',
+    },
     ':disabled': {
       bg: '$primary600_alpha60',
     },
     '_dark': {
-      'bg': '$primary300',
+      'bg': '$primary400',
       ':disabled': {
         bg: '$primary600_alpha60',
       },
