e6db0ec0b13804f60982899d6f2a66b86478b9e5
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-07-10 15:13:17 +0530

    fix: styling

--- example/storybook/src/ui-components/Menu/styled-components/ItemLabel.tsx
+++ example/storybook/src/ui-components/Menu/styled-components/ItemLabel.tsx
@@ -8,7 +8,7 @@ export const Label = styled(
     fontWeight: '$normal',
     fontFamily: '$body',
     fontStyle: 'normal',
-    fontSize: '$md',
+    fontSize: '$sm',
     letterSpacing: '$md',
     lineHeight: '$md',
 
