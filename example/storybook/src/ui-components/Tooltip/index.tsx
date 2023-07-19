c79ebb1578a3d8e4a3d74ca94e8d0b46c10ae7ec
Sandeep Kumar Srivastava <sandeepkumar@geekyants.com>
2023-07-19 15:54:36 +0530

    fix: added text

--- example/storybook/src/ui-components/Tooltip/index.tsx
+++ example/storybook/src/ui-components/Tooltip/index.tsx
@@ -1,10 +1,11 @@
-import { Root, Content } from './styled-components';
+import { Root, Content, Text } from './styled-components';
 import { createTooltip } from '@gluestack-ui/tooltip';
 import { styled } from '../styled';
 
 export const Tooltip = createTooltip({
   Root,
   Content,
+  Text,
   //@ts-ignore
   AnimatePresence: styled.Component,
 });
