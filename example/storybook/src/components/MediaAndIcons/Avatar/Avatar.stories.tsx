ea0c2346764ca0a818105934300c20586c84b9fd
mayank-96 <pagarmayank07@gmail.com>
2023-07-26 17:35:51 +0530

    fix: avatr changes

--- example/storybook/src/components/MediaAndIcons/Avatar/Avatar.stories.tsx
+++ example/storybook/src/components/MediaAndIcons/Avatar/Avatar.stories.tsx
@@ -19,6 +19,14 @@ const AvatarMeta: ComponentMeta<any> = {
     },
     badge: {
       control: 'boolean',
+      options: [true, false],
+    },
+    uri: {
+      control: 'select',
+      options: [
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        'https://broken.link',
+      ],
     },
   },
 };
--- example/storybook/src/components/MediaAndIcons/Avatar/Avatar.tsx
+++ example/storybook/src/components/MediaAndIcons/Avatar/Avatar.tsx
@@ -18,28 +18,18 @@ const AvatarStory: CustomAvatarStory = ({
   uri = 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
   badge = true,
   fallbackText = 'John Doe',
+  ...props
 }: any) => {
   return (
-    <HStack space="md" h="100%" justifyContent="center" alignItems="center">
-      <Avatar size={size}>
-        <Avatar.FallbackText>{fallbackText}</Avatar.FallbackText>
-        <Avatar.Image
-          source={{
-            uri: uri,
-          }}
-        />
-        {badge && <Avatar.Badge />}
-      </Avatar>
-      <Avatar size={size}>
-        <Avatar.FallbackText>{fallbackText}</Avatar.FallbackText>
-        <Avatar.Image
-          source={{
-            uri: 'https://broken.link',
-          }}
-        />
-        {badge && <Avatar.Badge />}
-      </Avatar>
-    </HStack>
+    <Avatar size={size} {...props}>
+      <Avatar.FallbackText>{fallbackText}</Avatar.FallbackText>
+      <Avatar.Image
+        source={{
+          uri: uri,
+        }}
+      />
+      {badge && <Avatar.Badge />}
+    </Avatar>
   );
 };
 
