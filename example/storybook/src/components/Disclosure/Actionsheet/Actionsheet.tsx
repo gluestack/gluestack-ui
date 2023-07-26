005e16eb15f601b1d131f30eee9703b4eb3cf04f
mayank-96 <pagarmayank07@gmail.com>
2023-07-26 19:10:07 +0530

    fix: actionsheet changes

--- example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
+++ example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
@@ -11,50 +11,81 @@ import {
   AddIcon,
   Image,
   Text,
+  Center,
 } from '../../../ui-components';
 
-function ActionsheetStory({
-  showActionsheet: showActionsheetProp = true,
-  ...props
-}: any) {
+function ActionsheetStory({ ...props }: any) {
   const [showActionsheet, setShowActionsheet] = React.useState(false);
 
   const handleClose = () => setShowActionsheet(false);
 
   return (
-    <Actionsheet
-      isOpen={showActionsheet || showActionsheetProp}
-      onClose={handleClose}
-      {...props}
-    >
-      <Actionsheet.Backdrop />
-      <Actionsheet.Content>
-        <Actionsheet.DragIndicatorWrapper>
-          <Actionsheet.DragIndicator />
-        </Actionsheet.DragIndicatorWrapper>
-        <Actionsheet.Item onPress={handleClose} isDisabled>
-          <Actionsheet.ItemText>Delete</Actionsheet.ItemText>
-        </Actionsheet.Item>
-        <Actionsheet.Item onPress={handleClose}>
-          <Actionsheet.ItemText>Share</Actionsheet.ItemText>
-        </Actionsheet.Item>
-        <Actionsheet.Item onPress={handleClose}>
-          <Actionsheet.ItemText>Play</Actionsheet.ItemText>
-        </Actionsheet.Item>
-        <Actionsheet.Item onPress={handleClose}>
-          <Actionsheet.ItemText>Favourite</Actionsheet.ItemText>
-        </Actionsheet.Item>
-        <Actionsheet.Item onPress={handleClose}>
-          <Actionsheet.ItemText>Cancel</Actionsheet.ItemText>
-        </Actionsheet.Item>
-      </Actionsheet.Content>
-    </Actionsheet>
+    <Center>
+      <Button
+        onPress={() => {
+          setShowActionsheet((prev) => !prev);
+        }}
+      >
+        <Button.Text>Open</Button.Text>
+      </Button>
+      <Actionsheet isOpen={showActionsheet} onClose={handleClose} {...props}>
+        <Actionsheet.Backdrop />
+        <Actionsheet.Content>
+          <Actionsheet.DragIndicatorWrapper>
+            <Actionsheet.DragIndicator />
+          </Actionsheet.DragIndicatorWrapper>
+          <Actionsheet.Item onPress={handleClose} isDisabled>
+            <Actionsheet.ItemText>Delete</Actionsheet.ItemText>
+          </Actionsheet.Item>
+          <Actionsheet.Item onPress={handleClose}>
+            <Actionsheet.ItemText>Share</Actionsheet.ItemText>
+          </Actionsheet.Item>
+          <Actionsheet.Item onPress={handleClose}>
+            <Actionsheet.ItemText>Play</Actionsheet.ItemText>
+          </Actionsheet.Item>
+          <Actionsheet.Item onPress={handleClose}>
+            <Actionsheet.ItemText>Favourite</Actionsheet.ItemText>
+          </Actionsheet.Item>
+          <Actionsheet.Item onPress={handleClose}>
+            <Actionsheet.ItemText>Cancel</Actionsheet.ItemText>
+          </Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </Center>
   );
 }
 
-export default ActionsheetStory;
+function FigmaActionsheetStory({ ...props }: any) {
+  return (
+    <Center w={900} h={400}>
+      <Actionsheet.Content {...props}>
+        <Actionsheet.DragIndicatorWrapper>
+          <Actionsheet.DragIndicator />
+        </Actionsheet.DragIndicatorWrapper>
+        <Actionsheet.Item>
+          <Actionsheet.ItemText>Delete</Actionsheet.ItemText>
+        </Actionsheet.Item>
+        <Actionsheet.Item>
+          <Actionsheet.ItemText>Share</Actionsheet.ItemText>
+        </Actionsheet.Item>
+        <Actionsheet.Item>
+          <Actionsheet.ItemText>Play</Actionsheet.ItemText>
+        </Actionsheet.Item>
+        <Actionsheet.Item>
+          <Actionsheet.ItemText>Favourite</Actionsheet.ItemText>
+        </Actionsheet.Item>
+        <Actionsheet.Item>
+          <Actionsheet.ItemText>Cancel</Actionsheet.ItemText>
+        </Actionsheet.Item>
+      </Actionsheet.Content>
+    </Center>
+  );
+}
+
+export default FigmaActionsheetStory;
 
 export {
+  ActionsheetStory,
   Actionsheet,
   Button,
   VStack,
