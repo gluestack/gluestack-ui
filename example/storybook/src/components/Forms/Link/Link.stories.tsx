a2cb9a0f2b6b34ba1184efba3abc6ba8eb8b4724
mayank-96 <pagarmayank07@gmail.com>
2023-07-27 16:16:23 +0530

    fix: story changes

--- example/storybook/src/components/Forms/Link/Link.stories.tsx
+++ example/storybook/src/components/Forms/Link/Link.stories.tsx
@@ -2,7 +2,7 @@ import type { ComponentMeta } from '@storybook/react-native';
 import React from 'react';
 import Link from './Link';
 
-export const LinkStory = ({ ...props }: any) => {
+const LinkStory = ({ ...props }: any) => {
   return <Link {...props} />;
 };
 const LinkMeta: ComponentMeta<typeof LinkStory> = {
