ecab2ae7c4baf3fd3e3c50eb547acd4d2e7adcb4
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Air.local>
2023-08-09 19:01:33 +0530

    fix: package json

--- src/components/core/GluestackUIProvider/index.tsx
+++ src/components/core/GluestackUIProvider/index.tsx
@@ -6,13 +6,12 @@ import {
 import { StyledProvider } from "@gluestack-style/react";
 import { OverlayProvider } from "@gluestack-ui/overlay";
 import { ToastProvider } from "@gluestack-ui/toast";
-import { config } from "../../../gluestack-ui.config";
 
 const GluestackUIStyledProvider = createProvider({ StyledProvider });
 
 const GluestackUIProvider = ({ children, ...props }: any) => {
   return (
-    <GluestackUIStyledProvider {...props} config={config.theme}>
+    <GluestackUIStyledProvider {...props}>
       <OverlayProvider>
         <ToastProvider>{children}</ToastProvider>
       </OverlayProvider>
