833f415cfc521fc17a9b9db80eeadbfae299ff16
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-08-21 16:15:58 +0530

    fix: working examples

--- example/storybook/src/unstyled/ui-components/Button/index.tsx
+++ example/storybook/src/unstyled/ui-components/Button/index.tsx
@@ -1,22 +1,22 @@
 import { createButton } from '@gluestack-ui/button';
 import { Root, Text, Group, Spinner, Icon } from './styled-components';
 
-export const AccessibleButton = createButton({
+export const Button = createButton({
   Root,
   Text,
   Group,
   Spinner,
   Icon,
 });
-// export const ButtonText = Button.Text;
-// export const ButtonGroup = Button.Group;
-// export const ButtonSpinner = Button.Spinner;
-// export const ButtonIcon = Button.Icon;
+export const ButtonText = Button.Text;
+export const ButtonGroup = Button.Group;
+export const ButtonSpinner = Button.Spinner;
+export const ButtonIcon = Button.Icon;
 
-const ButtonTemp = ({ children, ...props }) => {
-  return <AccessibleButton>{children}</AccessibleButton>;
-};
+// const ButtonTemp = ({ children, ...props }) => {
+//   return <AccessibleButton>{children}</AccessibleButton>;
+// };
 
-const Button = ButtonTemp;
-Button.Text = AccessibleButton.Text;
-export const Button = ButtonTemp;
+// const Button = ButtonTemp;
+// Button.Text = AccessibleButton.Text;
+// export const Button = ButtonTemp;
