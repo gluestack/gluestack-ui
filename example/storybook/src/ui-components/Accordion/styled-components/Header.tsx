1d2fa0640cd7c2d24c98006802cc0a1a794c06f1
Damini <daminipandey1310@gmail.com>
2023-08-22 16:22:18 +0530

    fix: icon button example in docs

---
+++ example/storybook/src/ui-components/Accordion/styled-components/Header.tsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+import { styled } from '../../styled';
+import { View } from 'react-native';
+
+export default styled(
+  View,
+  {
+    py: '$3',
+  },
+  {
+    descendantStyle: [],
+    ancestorStyle: [],
+  }
+);
