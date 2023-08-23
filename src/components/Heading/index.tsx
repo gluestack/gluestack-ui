524c5f23dfd2214b8c7f92eac6e65819a7618ae6
Akash Gautam <akashgautam@geekyants.com>
2023-08-23 18:09:10 +0530

    fix: heading convert_spelling

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Root } from './styled-components';
 import { usePropResolution } from '../../hooks/usePropResolution';
 
-export const Text = ({ children, ...props }: any) => {
+export const Heading = ({ children, ...props }: any) => {
   const resolvedPropForGluestack = usePropResolution(props);
   return <Root {...resolvedPropForGluestack}>{children}</Root>;
 };
