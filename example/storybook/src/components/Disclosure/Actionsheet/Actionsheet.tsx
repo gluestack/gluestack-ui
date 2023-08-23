a3da34494173555cd97c19bbf61f63b70d490584
mayank-96 <pagarmayank07@gmail.com>
2023-08-23 15:29:29 +0530

    fix: fixed actionsheet

--- example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
+++ example/storybook/src/components/Disclosure/Actionsheet/Actionsheet.tsx
@@ -27,39 +27,44 @@ import {
   Center,
 } from '../../../ui-components';
 
-function ActionsheetStory({ ...props }: any) {
+function ActionsheetStory({ showActionsheetProp, ...props }: any) {
   const [showActionsheet, setShowActionsheet] = React.useState(false);
 
   const handleClose = () => setShowActionsheet(false);
 
   return (
-    <Actionsheet
-      isOpen={showActionsheet || showActionsheetProp}
-      onClose={handleClose}
-      {...props}
-    >
-      <ActionsheetBackdrop />
-      <ActionsheetContent>
-        <ActionsheetDragIndicatorWrapper>
-          <ActionsheetDragIndicator />
-        </ActionsheetDragIndicatorWrapper>
-        <ActionsheetItem onPress={handleClose} isDisabled>
-          <ActionsheetItemText>Delete</ActionsheetItemText>
-        </ActionsheetItem>
-        <ActionsheetItem onPress={handleClose}>
-          <ActionsheetItemText>Share</ActionsheetItemText>
-        </ActionsheetItem>
-        <ActionsheetItem onPress={handleClose}>
-          <ActionsheetItemText>Play</ActionsheetItemText>
-        </ActionsheetItem>
-        <ActionsheetItem onPress={handleClose}>
-          <ActionsheetItemText>Favourite</ActionsheetItemText>
-        </ActionsheetItem>
-        <ActionsheetItem onPress={handleClose}>
-          <ActionsheetItemText>Cancel</ActionsheetItemText>
-        </ActionsheetItem>
-      </ActionsheetContent>
-    </Actionsheet>
+    <Center>
+      <Button onPress={() => setShowActionsheet(true)}>
+        <ButtonText>Open</ButtonText>
+      </Button>
+      <Actionsheet
+        isOpen={showActionsheet || showActionsheetProp}
+        onClose={handleClose}
+        {...props}
+      >
+        <ActionsheetBackdrop />
+        <ActionsheetContent>
+          <ActionsheetDragIndicatorWrapper>
+            <ActionsheetDragIndicator />
+          </ActionsheetDragIndicatorWrapper>
+          <ActionsheetItem onPress={handleClose} isDisabled>
+            <ActionsheetItemText>Delete</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleClose}>
+            <ActionsheetItemText>Share</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleClose}>
+            <ActionsheetItemText>Play</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleClose}>
+            <ActionsheetItemText>Favourite</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleClose}>
+            <ActionsheetItemText>Cancel</ActionsheetItemText>
+          </ActionsheetItem>
+        </ActionsheetContent>
+      </Actionsheet>
+    </Center>
   );
 }
 
