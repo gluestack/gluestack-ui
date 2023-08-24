f99f39534ceb21807667e7236e632c156df0706b
Akash Gautam <akashgautam@geekyants.com>
2023-08-24 17:07:53 +0530

    feat: native base shadow variants

--- src/components/gluestack-ui.config.ts
+++ src/components/gluestack-ui.config.ts
@@ -550,9 +550,121 @@ export const config = {
     } as const,
     globalStyle: {
       variants: {
+        shadow: {
+          'none': {
+            shadowColor: 'transparent',
+            shadowOffset: {
+              width: 0,
+              height: 0,
+            },
+            shadowOpacity: 0,
+            shadowRadius: 0,
+            elevation: 0,
+          },
+          '0': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.18,
+            shadowRadius: 1.0,
+            elevation: 1,
+          },
+          '1': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.2,
+            shadowRadius: 1.41,
+            elevation: 2,
+          },
+          '2': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.22,
+            shadowRadius: 2.22,
+            elevation: 3,
+          },
+          '3': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.23,
+            shadowRadius: 2.62,
+            elevation: 4,
+          },
+          '4': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          },
+          '5': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.27,
+            shadowRadius: 4.65,
+            elevation: 6,
+          },
+          '6': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.29,
+            shadowRadius: 4.65,
+            elevation: 7,
+          },
+          '7': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.3,
+            shadowRadius: 4.65,
+            elevation: 8,
+          },
+          '8': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 4,
+            },
+            shadowOpacity: 0.32,
+            shadowRadius: 5.46,
+            elevation: 9,
+          },
+          '9': {
+            shadowColor: '$light.900',
+            shadowOffset: {
+              width: 0,
+              height: 5,
+            },
+            shadowOpacity: 0.34,
+            shadowRadius: 6.27,
+            elevation: 10,
+          },
+        },
         hardShadow: {
           '1': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: -2,
               height: 2,
@@ -562,7 +674,7 @@ export const config = {
             elevation: 10,
           },
           '2': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 3,
@@ -572,7 +684,7 @@ export const config = {
             elevation: 10,
           },
           '3': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 2,
               height: 2,
@@ -582,7 +694,7 @@ export const config = {
             elevation: 10,
           },
           '4': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: -3,
@@ -594,7 +706,7 @@ export const config = {
           // this 5th version is only for toast shadow
           // temporary
           '5': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 3,
@@ -606,7 +718,7 @@ export const config = {
         },
         softShadow: {
           '1': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 0,
@@ -614,13 +726,13 @@ export const config = {
             shadowRadius: 10,
             shadowOpacity: 0.1,
             _android: {
-              shadowColor: '$backgroundLight500',
+              shadowColor: '$light.500',
               elevation: 5,
               shadowOpacity: 0.05,
             },
           },
           '2': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 0,
@@ -635,7 +747,7 @@ export const config = {
             },
           },
           '3': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 0,
@@ -650,7 +762,7 @@ export const config = {
             },
           },
           '4': {
-            shadowColor: '$backgroundLight900',
+            shadowColor: '$light.900',
             shadowOffset: {
               width: 0,
               height: 0,
