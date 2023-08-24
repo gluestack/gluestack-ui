d45033ee8a56a84106d2f1c62ece97ac13d2904e
Meenu Makkar <makkarmini007@gmail.com>
2023-08-24 15:11:51 +0530

    feat: just to test docs

--- example/storybook/src/components/Feedback/Spinner/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Spinner, HStack, Text, VStack } from '@gluestack-ui/themed';
-
-const SpinnerStory = ({ ...props }) => <Spinner {...props} />;
-
-export default SpinnerStory;
-
-export { Spinner, HStack, Text, VStack };
