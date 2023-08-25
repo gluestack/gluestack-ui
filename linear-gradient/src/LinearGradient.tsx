0ccf9d4a573fc78aacfc8000c4f5d08383b274cb
Damini <daminipandey1310@gmail.com>
2023-08-25 19:08:41 +0530

    feat: add linear-gradient component

--- linear-gradient/src/LinearGradient.tsx
+++ linear-gradient/src/LinearGradient.tsx
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react';
 
 export const LinearGradient = (StyledLinearGradient: any) =>
   forwardRef(({ children, ...props }: any, ref?: any) => {
+    // console.log(props);
     return (
       <StyledLinearGradient ref={ref} {...props}>
         {children}
