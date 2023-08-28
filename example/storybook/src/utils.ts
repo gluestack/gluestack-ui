6866d44b5e0039efad4a712196910d44ec120511
Vidhi Kataria <katariavidhi99@gmail.com>
2023-08-28 18:21:25 +0530

    feat: styling docs added

--- example/storybook/src/utils.ts
+++ example/storybook/src/utils.ts
@@ -30,3 +30,19 @@ export function transformedThemedCode(
     return <GluestackUIProvider>${code}</GluestackUIProvider>;
   };`;
 }
+
+export function transformedCodeWithoutWrapper(
+  code: string,
+  type: 'jsx' | 'function' = 'jsx',
+  componentName?: string
+) {
+  if (type === 'function') {
+    return `function App() {
+      ${code}
+    return <${componentName} />;
+  };`;
+  }
+  return `function App() {
+    return ${code};
+  };`;
+}
