ad90f67426d9d3adffea8a90e764c5df13e935ce
Vidhi Kataria <katariavidhi99@gmail.com>
2023-08-31 16:14:01 +0530

    fix: responsive

--- example/storybook/src/resources/dashboard-app/Responsive.tsx
+++ example/storybook/src/resources/dashboard-app/Responsive.tsx
@@ -1,9 +1,6 @@
-import {
-  Box,
-  //  Pressable, Text
-} from '@gluestack/design-system';
+import { Box, Text } from '@gluestack/design-system';
 import React, { useRef, useState } from 'react';
-// import NextImage from "next/image";
+import NextImage from 'next/image';
 import { Expand } from './Expand';
 import { Resizable } from 're-resizable';
 // import { GluestackUIProvider } from "@gluestack-ui/themed"
@@ -11,11 +8,10 @@ import { Resizable } from 're-resizable';
 
 function Responsiveness() {
   const resizableRef = useRef<any>(null);
-  const [setIsMobile] = useState(true);
+  const [isMobile, setIsMobile] = useState(true);
   return (
-    <>
+    <Box minWidth={'70vw'}>
       <Resizable
-        style={{ zIndex: 1 }}
         onResize={() =>
           setIsMobile(
             resizableRef.current?.resizable?.offsetWidth &&
@@ -55,6 +51,7 @@ function Responsiveness() {
           ),
         }}
         minWidth="300px"
+        maxWidth="100%"
         enable={{
           top: false,
           right: true,
@@ -66,14 +63,87 @@ function Responsiveness() {
           topLeft: false,
         }}
       >
-        <Box h="100vh" w="100%" overflow="hidden">
+        <Box
+          flexDirection="row"
+          alignItems="center"
+          bg="$backgroundDark950"
+          py="$1.5"
+          borderTopLeftRadius="$2xl"
+          borderTopRightRadius="$2xl"
+          display="none"
+          sx={{
+            '@sm': {
+              display: isMobile ? 'none' : 'flex',
+            },
+          }}
+        >
+          <NextImage
+            src="/images/menu.svg"
+            alt="menu-options"
+            width={40}
+            height={10}
+            style={{
+              marginRight: 20,
+              marginLeft: 27,
+            }}
+          />
+          <Box flexDirection="row">
+            <Text mr="$5" fontSize="$xs" fontWeight="$normal">
+              Homestay
+            </Text>
+
+            <Text mr="$5" fontSize="$xs" fontWeight="$normal">
+              File
+            </Text>
+
+            <Text mr="$5" fontSize="$xs" fontWeight="$normal">
+              Edit
+            </Text>
+
+            <Text mr="$5" fontSize="$xs" fontWeight="$normal">
+              View
+            </Text>
+          </Box>
+        </Box>
+
+        <Box
+          display="flex"
+          sx={{
+            '@sm': {
+              display: isMobile ? 'flex' : 'none',
+            },
+          }}
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          borderTopLeftRadius="$2xl"
+          borderTopRightRadius="$2xl"
+          bg="$backgroundDark950"
+          py="$1.5"
+        >
+          <Text ml="$5" fontSize="$xs" fontWeight="$bold" color="$trueGray400">
+            8:15
+          </Text>
+
+          <NextImage
+            src="/images/statusicons.svg"
+            alt="menu-options"
+            width={56}
+            height={15}
+            style={{
+              marginRight: 24,
+            }}
+          />
+        </Box>
+
+        <Box h="100%" w="100%" overflow="hidden">
           <iframe
             src="https://ui-kitchensink.gluestack.io/"
             title="NativeBase v3 Dashboard Example"
             style={{
               transformOrigin: '0px 0px',
               transform: 'scale(0.8)',
-              width: '125vw',
+              width: '125%',
               height: '125%',
               border: 'none',
               borderBottomLeftRadius: 16,
@@ -82,7 +152,7 @@ function Responsiveness() {
           />
         </Box>
       </Resizable>
-    </>
+    </Box>
   );
 }
 
