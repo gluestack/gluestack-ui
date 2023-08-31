c36e5da515e83db67775ca1a1d8ab5ddb11cc715
Akash Gautam <akashgautam@geekyants.com>
2023-09-01 00:29:16 +0530

    feat: new icons for gui same as NB

---
+++ src/components/Icons/Icons/Hamburger.tsx
@@ -0,0 +1,10 @@
+import { createIcon } from '../styled-components';
+
+const HamburgerIcon = createIcon({
+  viewBox: '0 0 24 24',
+  d: 'M22 5H2V7.47961H22V5ZM22 10.4795H2V12.9591H22V10.4795ZM2 15.959H22V18.4386H2V15.959Z',
+});
+
+HamburgerIcon.displayName = 'HamburgerIcon';
+
+export { HamburgerIcon };
---
+++ src/components/Icons/Icons/Minus.tsx
@@ -0,0 +1,10 @@
+import { createIcon } from '../styled-components';
+
+const MinusIcon = createIcon({
+  viewBox: '0 0 24 24',
+  d: 'M22 10.5H2V13H22V10.5Z',
+});
+
+MinusIcon.displayName = 'MinusIcon';
+
+export { MinusIcon };
