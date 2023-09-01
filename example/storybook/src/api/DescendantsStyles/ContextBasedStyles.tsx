b3d7f2d515f1c2061d1c914952c95476df74b045
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-09-01 19:26:27 +0530

    fix: config

--- example/storybook/src/api/DescendantsStyles/ContextBasedStyles.tsx
+++ example/storybook/src/api/DescendantsStyles/ContextBasedStyles.tsx
@@ -13,87 +13,25 @@ import {
   StyleSheet,
   View,
 } from 'react-native';
-import { AsForwarder, styled } from '@gluestack-style/react';
+import { AsForwarder, styled, Theme } from '@gluestack-style/react';
 import { Wrapper } from '../../components/Wrapper';
 // import { AddIcon } from '@gluestack/design-system';
-import { createIcon } from '@gluestack-ui/icon';
-import { Svg } from 'react-native-svg';
-import { FlatList } from 'react-native';
-import { StyledText, StyledView } from '../AsForwarder/AsForwarder';
-
-// const Box = styled(View, {
-//   bg: '$backgroundDark300',
-//   width: '200px',
-//   height: '100px',
-// });
-
-const Text = styled(
-  RNText,
-  {
-    // bg: '$amber300',
-    // color: '$red500',
-    // props: {
-    //   color: '$pink400',
-    // },
-    // variants: {
-    //   variant: {
-    //     solid: {
-    //       color: '$green500',
-    //     },
-    //   },
-    // },
-  },
-  {
-    componentName: 'TEXT',
-  }
-);
-const MyPressable = styled(
-  RNPressable,
-  {
-    bg: '$red500',
-    // height: 40,
-    // p: 2,
-    // // props: {
-    // //   color: '$purple500',
-    // // },
-    // variants: {
-    //   variant: {
-    //     solid: {
-    //       bg: '$red400',
-    //     },
-    //   },
-    // },
-    // defaultProps: {
-    //   variant: 'solid',
-    // },
-  },
-  {
-    componentName: 'BOX',
-  }
-);
 
 const Box = styled(
   View,
   {
-    variants: {
-      variant: {},
+    // 'bg': '$red500',
+    // 'bg': '$red600',
+    'w': 100,
+    'h': 100,
+    '_light': {
+      bg: '$red600',
     },
-    // bg: '$red500',
-    // height: 40,
-    // p: 2,
-    // // props: {
-    // //   color: '$purple500',
-    // // },
-    // variants: {
-    //   variant: {
-    //     solid: {
-    //       bg: '$red400',
-    //     },
-    //   },
-    // },
-    // defaultProps: {
-    //   variant: 'solid',
-    // },
+    '@base': {
+      // _light: {
+      bg: '$green500',
+      // },
+    },
   },
   {
     componentName: 'BOX2',
@@ -109,84 +47,13 @@ export function ContextBasedStyles() {
   );
 }
 
-const MyFlatList = styled(
-  FlatList,
-  {},
-  {
-    componentName: 'MyFlatList',
-  }
-);
-const styleshet = StyleSheet.create({
-  style: {
-    backgroundColor: 'blue',
-    padding: 2,
-    height: 40,
-  },
-});
 export function ContextBasedStylesContent() {
-  const timeTaken = useRef(Date.now());
-  // useEffect(() => {
-  //   console.log(Date.now() - timeTaken.current, 'hello');
-  // }, []);
-
-  const [state, setState] = useState(true);
-
-  const handlePress = useCallback(() => {
-    timeTaken.current = Date.now();
-    setState(!state);
-  }, [state]);
-
-  // const layoutChange = () => {};
-
-  // useEffect(() => {
-  //   console.log(Date.now() - timeTaken.current, 'hello');
-  // });
-
-  // useEffect(() => {
-  //   console.log(Date.now() - timeTaken.current, 'hello');
-  // }, [state]);
-
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
-
-  return (
-    <MyFlatList
-      data={DATA}
-      bounces={false}
-      // horizontal
-      showsHorizontalScrollIndicator={false}
-      // numColumns={2}
-      sx={{
-        _web: {
-          props: { bg: '$red500', numColumns: 2 },
-        },
-      }}
-      renderItem={({ item }: { item: any }) => {
-        return (
-          <Box mr={'$2'} sx={{ props: {} }}>
-            <StyledText>Hello</StyledText>
-          </Box>
-        );
-      }}
-      keyExtractor={(item, index) => 'key' + index}
-      ItemSeparatorComponent={() => <Box w="4" />}
-    />
-  );
-
   // return <MyFlatList></MyFlatList>;
-  return <Text>hello</Text>;
+  return (
+    <Theme name={'modern'}>
+      <Box></Box>
+    </Theme>
+  );
 
   return (
     <>
