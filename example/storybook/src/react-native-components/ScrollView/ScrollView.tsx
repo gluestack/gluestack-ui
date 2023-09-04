c0b6c6e70d34b3ce21d83514f3edd4353ad315ce
Vidhi Kataria <katariavidhi99@gmail.com>
2023-09-04 18:25:42 +0530

    feat: react native components added

---
+++ example/storybook/src/react-native-components/ScrollView/ScrollView.tsx
@@ -0,0 +1,1 @@
+export { ScrollView } from '@gluestack-ui/themed';
---
+++ packages/themed/src/components/KeyboardAvoidingView/styled-components/Root.tsx
@@ -0,0 +1,6 @@
+import { KeyboardAvoidingView } from 'react-native';
+import { styled } from '../../styled';
+
+export default styled(KeyboardAvoidingView, {}, {
+  componentName: 'KeyboardAvoidingView',
+} as const);
