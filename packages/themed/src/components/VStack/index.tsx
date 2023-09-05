f0b3ab9e006c5e1b3c1f66c40b75dbf26e6bd975
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-09-05 16:02:00 +0530

    fix: hstack vstack api changes

--- packages/themed/src/components/VStack/index.tsx
+++ packages/themed/src/components/VStack/index.tsx
@@ -1,7 +1,6 @@
 import { createVStack } from '@gluestack-ui/vstack';
-import { Root, Spacer } from './styled-components';
+import { Root } from './styled-components';
 
 export const VStack = createVStack({
   Root,
-  Spacer,
 });
