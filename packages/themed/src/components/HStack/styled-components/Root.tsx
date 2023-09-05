626e8c3e858fea069e246359d8925ebed66dec1a
Rohit Singh <rohit99.ind@gmail.com>
2023-09-05 15:34:43 +0530

    feat: hstack and vstack new version

--- packages/themed/src/components/HStack/styled-components/Root.tsx
+++ packages/themed/src/components/HStack/styled-components/Root.tsx
@@ -5,8 +5,40 @@ export default styled(
   View,
   {
     flexDirection: 'row',
-
-    defaultProps: {
+    variants: {
+      space: {
+        'xs': {
+          gap: `$1`,
+        },
+        'sm': {
+          gap: `$2`,
+        },
+        'md': {
+          gap: `$3`,
+        },
+        'lg': {
+          gap: `$4`,
+        },
+        'xl': {
+          gap: `$5`,
+        },
+        '2xl': {
+          gap: `$6`,
+        },
+        '3xl': {
+          gap: `$7`,
+        },
+        '4xl': {
+          gap: `$8`,
+        },
+      },
+      reversed: {
+        true: {
+          flexDirection: 'row-reverse',
+        },
+      },
+    },
+    props: {
       space: 'md',
     },
   },
