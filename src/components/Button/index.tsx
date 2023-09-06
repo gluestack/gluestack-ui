17540f0f4a4debbe4d67c65f14d8056d4190767a
Akash Gautam <akashgautam@geekyants.com>
2023-09-06 16:49:32 +0530

    fix: added types for props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -11,7 +11,7 @@ import {
   Icon,
 } from './styled-components';
 // import { useStyled } from '@gluestack-style/react';
-// import { usePropResolution } from '../../hooks/usePropResolution';
+import { usePropResolution } from '../../hooks/usePropResolution';
 
 export const AccessibleButton = createButton({
   Root,
@@ -26,10 +26,14 @@ export const AccessibleButton = createButton({
 let But = AccessibleButton;
 
 const ButtonNew = forwardRef(({ children, ...props }: any, ref?: any) => {
-  // const resolvedPropForGluestack = usePropResolution(props);
+  const resolvedPropForGluestack = usePropResolution(props);
+
+  // if (props.colorScheme) {
+  //   const colorSchemeSx = getColorSchemeSX();
+  // }
 
   return (
-    <AccessibleButton {...props} ref={ref}>
+    <AccessibleButton {...resolvedPropForGluestack} ref={ref}>
       {typeof children === 'string' ? (
         <AccessibleButton.Text>{children}</AccessibleButton.Text>
       ) : (
