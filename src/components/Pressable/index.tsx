2e5c04e6cd1a42754c917b3895edd64dc26e0a44
Akash Gautam <akashgautam@geekyants.com>
2023-09-08 12:53:57 +0530

    feat: pressable done

--- src/components/Pressable/index.tsx
+++ src/components/Pressable/index.tsx
@@ -1,2 +1,11 @@
-import { Pressable } from './styled-components';
-export { Pressable };
+import React from 'react';
+import { Pressable as AccessiblePressable } from './styled-components';
+import { usePropResolution } from '../../hooks/usePropResolution';
+
+export const Pressable = ({
+  children,
+  ...props
+}: Parameters<typeof AccessiblePressable>[0]) => {
+  const resolvedProps = usePropResolution(props);
+  return <AccessiblePressable {...resolvedProps} children={children} />;
+};
--- src/components/Pressable/styled-components/Root.tsx
+++ src/components/Pressable/styled-components/Root.tsx
@@ -4,13 +4,17 @@ import { Pressable } from 'react-native';
 export default styled(
   Pressable,
   {
-    _web: {
-      ':focusVisible': {
-        outlineWidth: '2px',
-        outlineColor: '$primary700',
-        outlineStyle: 'solid',
-        _dark: {
-          outlineColor: '$primary300',
+    ':focus': {
+      _web: {
+        outlineWidth: 0,
+        boxShadow: `$cyan.400 0px 0px 0px 2px`,
+      },
+    },
+    '_dark': {
+      ':focus': {
+        _web: {
+          outlineWidth: 0,
+          boxShadow: `$cyan.500 0px 0px 0px 2px`,
         },
       },
     },
