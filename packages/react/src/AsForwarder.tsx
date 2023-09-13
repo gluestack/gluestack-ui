1c58fd12dae56fcfe57522216c6d8b7a61a39a34
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-09-13 18:14:26 +0530

    fix: as forwarder issue

--- packages/react/src/AsForwarder.tsx
+++ packages/react/src/AsForwarder.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Svg from 'react-native-svg';
+import type { ViewProps } from 'react-native';
 
-const AsForwarder = ({ as, children, ...props }: any) => {
+export interface InterfaceAsForwardedProps extends ViewProps {
+  as?: any;
+}
+
+const AsForwarderTemp = ({
+  as,
+  children,
+  ...props
+}: any): React.ReactElement => {
   const As: any = as;
   return as ? <As {...props}>{children}</As> : <Svg {...props}>{children}</Svg>;
 };
-AsForwarder.displayName = '__AsForwarder__';
-export { AsForwarder };
+AsForwarderTemp.displayName = '__AsForwarder__';
+
+export const AsForwarder =
+  AsForwarderTemp as React.ComponentType<InterfaceAsForwardedProps>;
