f916d9262d328fe7df88bb2f00e4f8f921ad0dbe
Akash Gautam <akashgautam@geekyants.com>
2023-09-13 18:01:01 +0530

    feat: createIcon docs update

--- packages/themed/src/components/Icons/index.tsx
+++ packages/themed/src/components/Icons/index.tsx
@@ -1,8 +1,14 @@
 import { createIcon } from '@gluestack-ui/icon';
 import Root from './styled-components/Root';
+import { Root as IconRoot } from './styled-components';
 
 export const Icon = createIcon({
   Root,
 });
 
 export * from './Icons';
+type ParameterTypes = Omit<Parameters<typeof createIcon>[0], 'Root'>;
+const createIconUI = ({ ...props }: ParameterTypes) =>
+  createIcon({ Root: IconRoot, ...props });
+
+export { createIconUI as createIcon };
