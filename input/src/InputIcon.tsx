f307e8b45835cb24c30475887190a45060017016
Meenu Makkar <makkarmini007@gmail.com>
2023-09-14 15:56:40 +0530

    feat: changed inputicon to inputslot and added inputicon

--- input/src/InputIcon.tsx
+++ input/src/InputIcon.tsx
@@ -1,33 +1,9 @@
 import React, { forwardRef } from 'react';
-import { useInput } from './InputContext';
-import { useFormControl } from '@gluestack-ui/form-control';
 
 export const InputIcon = (StyledInputIcon: any) =>
-  forwardRef(({ children, onPress, ...props }: any, ref?: any) => {
-    const { inputFieldRef, isDisabled } = useInput('InputContext');
-
-    const handleFocus = () => {
-      inputFieldRef?.current?.focus();
-    };
-
-    const inputProps = useFormControl({
-      isDisabled: props.isDisabled,
-    });
-
+  forwardRef(({ children, ...props }: any, ref?: any) => {
     return (
-      <StyledInputIcon
-        states={{
-          disabled: isDisabled || inputProps.isDisabled,
-        }}
-        onPress={() => {
-          handleFocus();
-          if (onPress) onPress();
-        }}
-        accessibilityElementsHidden={true}
-        focusable={false}
-        ref={ref}
-        {...props}
-      >
+      <StyledInputIcon focusable={false} ref={ref} {...props}>
         {children}
       </StyledInputIcon>
     );
