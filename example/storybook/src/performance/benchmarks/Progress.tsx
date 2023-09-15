d642553dbd58fafe39d4bc326d7f210de1a8fc1e
Suraj Ahmed Choudhury <suraj@geekyants.com>
2023-09-15 20:12:19 +0530

    Update Progress.tsx

--- example/storybook/src/performance/benchmarks/Progress.tsx
+++ example/storybook/src/performance/benchmarks/Progress.tsx
@@ -23,7 +23,7 @@ export function Graph({ data }: { data: any }) {
               <Text
                 sx={{
                   _web: { whiteSpace: 'nowrap' },
-                  w: 120,
+                  w: 130,
                   fontSize: '$sm',
                   textAlign: 'right',
                   fontWeight: key === 'gluestack-ui' ? '$semibold' : '$normal',
