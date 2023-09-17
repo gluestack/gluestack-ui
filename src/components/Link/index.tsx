6a443484349e343dce00c2cb0f144f0c7946105d
Akash Gautam <akashgautam@geekyants.com>
2023-09-18 03:44:38 +0530

    feat: link done

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,8 +1,29 @@
-import { Root, Text } from './styled-components';
+import { Root } from './styled-components';
+import { Text } from '../Text';
 import { createLink } from '@gluestack-ui/link';
+import { forwardRef } from 'react';
+import React from 'react';
+import { usePropResolution } from '../../hooks/usePropResolution';
 
-export const Link = createLink({
+const AccessibleLink = createLink({
   Root,
   Text,
 });
-export const LinkText = Link.Text;
+
+export const Link = forwardRef(
+  (
+    { children, ...props }: React.ComponentProps<typeof AccessibleLink>,
+    ref?: any
+  ) => {
+    const resolvedProps = usePropResolution(props);
+    return (
+      <AccessibleLink {...resolvedProps} ref={ref}>
+        {typeof children === 'string' ? (
+          <AccessibleLink.Text>{children}</AccessibleLink.Text>
+        ) : (
+          children
+        )}
+      </AccessibleLink>
+    );
+  }
+);
