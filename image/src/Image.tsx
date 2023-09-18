1d930b29dbc5c5e8fb7f29e8b823741a5f7040bd
Meenu Makkar <makkarmini007@gmail.com>
2023-09-18 16:35:50 +0530

    feat: image released

--- image/src/Image.tsx
+++ image/src/Image.tsx
@@ -2,10 +2,17 @@ import React, { forwardRef } from 'react';
 
 export const Image = (StyledImage: any) =>
   forwardRef(({ ...props }: any, ref?: any) => {
+    // Assuming props is an object with a 'source' property
     let source = props.source;
-    if (!props.source.uri) {
+    if (typeof source === 'number') {
+      // Handle case where source is a number
+    } else if (!props.source.uri) {
+      // Check if source.uri is not defined or falsy
       source = {
         uri: props.source.default ? props.source.default.src : props.source,
+        // If so, set source to an object with a 'uri' property
+        // Use a ternary operator to check if props.source.default exists,
+        // and if so, use its 'src' property, otherwise, use the original source
       };
     }
     const { alt, ...resolvedProps } = props;
