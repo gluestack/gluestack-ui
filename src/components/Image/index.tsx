dac04c53a83ac8ffa534c59ca0eefd8caac23199
Akash Gautam <akashgautam@geekyants.com>
2023-09-20 18:48:15 +0530

    feat: image finally resolved

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,3 +1,10 @@
+import React, { forwardRef } from 'react';
 import { Root } from './styled-components';
+import { usePropResolution } from '../../hooks/usePropResolution';
 
-export const Image = Root;
+export const Image = forwardRef(
+  ({ source, alt, ...props }: React.ComponentProps<typeof Root>, ref?: any) => {
+    const resolvedProps = usePropResolution(props);
+    return <Root {...resolvedProps} source={source} alt={alt} ref={ref} />;
+  }
+);
