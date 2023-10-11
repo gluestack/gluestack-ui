17910862905333d36d69b23b1320d8b1d8321e3b
ankit-tailor <tailorankit3345@gmail.com>
2023-10-11 16:44:42 +0530

    fix: experimental provider for color mode change

--- packages/react/src/StyledProvider.tsx
+++ packages/react/src/StyledProvider.tsx
@@ -93,7 +93,7 @@ export const StyledProvider: React.FC<{
 
     onChange((currentColor: string) => {
       // only for web
-      if (!_experimentalNestedProviderRef) {
+      if (!_experimentalNestedProvider) {
         const documentElement = document.documentElement;
 
         if (Platform.OS === 'web') {
@@ -162,6 +162,7 @@ export const StyledProvider: React.FC<{
       setAnimationDriverData,
       inlineStyleMap: inlineStyleMap.current,
     };
+
     if (_experimentalNestedProvider) {
       //@ts-ignore
       styledData._experimentalNestedProvider = _experimentalNestedProvider;
