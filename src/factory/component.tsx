db9ea70e5689d2bdc10e693047e4baed4821febc
Akash Gautam <akashgautam@geekyants.com>
2023-10-12 11:44:56 +0530

    fix: aliases

--- src/factory/component.tsx
+++ src/factory/component.tsx
@@ -1,57 +1,68 @@
 import React from 'react';
 import { styled, useStyled } from '@gluestack-style/react';
 import { usePropResolution } from '../hooks/usePropResolution';
-import { addDollarSignsToProps, convertToSXForStateColorModeMediaQuery } from '../utils';
+import {
+  addDollarSignsToProps,
+  convertToSXForStateColorModeMediaQuery,
+} from '../utils';
 
 const transformTheme = (componentTheme: any) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const styledContext = useStyled();
 
   const propsWithDollarSigns = addDollarSignsToProps(
     {
-      ...componentTheme.baseStyle, variants: {
+      ...componentTheme.baseStyle,
+      variants: {
         variant: {
-          ...componentTheme.variants
-        }, size: {
-          ...componentTheme.sizes
-        }
+          ...componentTheme.variants,
+        },
+        size: {
+          ...componentTheme.sizes,
+        },
       },
       props: {
-        ...componentTheme.defaultProps
-      }
+        ...componentTheme.defaultProps,
+      },
     },
     styledContext.config
   );
-
 
   const sxProps = convertToSXForStateColorModeMediaQuery(
     propsWithDollarSigns,
     styledContext.config
   );
 
-  return sxProps
-}
+  return sxProps;
+};
 export default function Factory<P>(
   Component: React.ComponentType<P>,
   componentTheme?: {
-    variants?: Object,
-    sizes?: Object,
-    baseStyle?: Object,
-    defaultProps?: Object
+    variants?: Object;
+    sizes?: Object;
+    baseStyle?: Object;
+    defaultProps?: Object;
   }
 ) {
-  const StyledComponent: any = styled(Component, transformTheme(componentTheme));
-  return React.forwardRef(
-    ({ children, _state, ...props }: any, ref: any) => {
-      if (componentTheme) {
-        let inlineProps: any = usePropResolution(props);
-        // const resolvedPropForGluestack = usePropResolution(props);
-
-        return <StyledComponent ref={ref} {...inlineProps}>{children}</StyledComponent>;
-      } else {
-        // FIX: Typings
-        // @ts-ignore
-        return <Component>{children}</Component>;
-      }
-    }
+  const StyledComponent: any = styled(
+    Component,
+    transformTheme(componentTheme)
   );
+  return React.forwardRef(({ children, _state, ...props }: any, ref: any) => {
+    if (componentTheme) {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const inlineProps: any = usePropResolution(props);
+      // const resolvedPropForGluestack = usePropResolution(props);
+
+      return (
+        <StyledComponent ref={ref} {...inlineProps}>
+          {children}
+        </StyledComponent>
+      );
+    } else {
+      // FIX: Typings
+      // @ts-ignore
+      return <Component>{children}</Component>;
+    }
+  });
 }
