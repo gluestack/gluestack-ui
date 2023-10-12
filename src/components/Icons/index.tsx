3d6623d6e21d45019449f8d2f2e2bbf785476907
Akash Gautam <akashgautam@geekyants.com>
2023-10-12 12:54:06 +0530

    fix: icons prop resolve

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -2,6 +2,7 @@ import { createIcon as createIconUI } from '@gluestack-ui/icon';
 import React, { forwardRef } from 'react';
 import { createIcon, Root } from './styled-components';
 import { GenericComponentType } from '../../types';
+import { usePropResolution } from '../../hooks/usePropResolution';
 
 const AccessibleIcon = createIconUI({
   Root: Root,
@@ -17,7 +18,12 @@ const IconTemp = forwardRef(
     }: React.ComponentProps<typeof AccessibleIcon> & { viewBox?: string },
     ref?: any
   ) => {
+    const resolvedProps = usePropResolution(props);
     let IconForward;
+    let sizeProp = {};
+    if (typeof props.size === 'number') {
+      sizeProp = { h: props.size, w: props.size };
+    }
     if (as) {
       IconForward = as;
     } else if (typeof viewBox === 'string') {
@@ -29,7 +35,14 @@ const IconTemp = forwardRef(
     } else if (children) {
       IconForward = children;
     }
-    return <AccessibleIcon as={IconForward} {...props} ref={ref} />;
+    return (
+      <AccessibleIcon
+        as={IconForward}
+        {...resolvedProps}
+        {...sizeProp}
+        ref={ref}
+      />
+    );
   }
 );
 
