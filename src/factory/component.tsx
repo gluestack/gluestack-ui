c22a5d9b16ac7f69c551afd07a46e577f2cee7e1
Vidhi Kataria <katariavidhi99@gmail.com>
2023-10-25 11:57:21 +0530

    fix: fixes

--- src/factory/component.tsx
+++ src/factory/component.tsx
@@ -3,17 +3,28 @@ import { styled, useStyled } from '@gluestack-style/react';
 import {
   addDollarSignsToProps,
   convertToSXForStateColorModeMediaQuery,
-  transformTheme,
+  transformTheme
 } from '../utils';
 import { config } from '../components/gluestack-ui.config';
 import { extendTheme } from '../utils/extendTheme';
 
+// const transformTheme = (componentTheme: any) => {
+//   // FIX: Remove Hook from here
+//   const styledContext = useStyled();
+//   let transformedTheme: any = {
+//     variants: {
+//       variant: {
+//       }, size: {
+//       }
+//     },
+//     defaultProps: {}
+//   };
+// const { baseStyle, variants, sizes, defaultProps } = componentTheme;
 
 export default function Factory<P>(
   Component: React.ComponentType<P>,
   componentTheme?: {
     variants?: Object;
-    sizes?: Object;
     baseStyle?: Object;
     defaultProps?: Object;
   }
@@ -28,3 +39,4 @@ export default function Factory<P>(
     transformTheme(componentTheme, clonedConfig)
   );
 }
+
