1b28792781be006a77f506a78d1f717f4dc4c032
Akash Gautam <akashgautam@geekyants.com>
2023-10-26 20:21:32 +0530

    fix: usetoken hook

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -4,7 +4,8 @@ import { useToken as useTokenStyle } from '@gluestack-style/react';
 export const useToken = (tokenScale: string, token: string | string[]) => {
   return typeof token === 'string'
     ? (useTokenStyle(tokenScale, token) as string)
-    : (token.map((singleToken) =>
-        useTokenStyle(tokenScale, singleToken)
-      ) as string[]);
+    : token &&
+        (token.map((singleToken) =>
+          useTokenStyle(tokenScale, singleToken)
+        ) as string[]);
 };
