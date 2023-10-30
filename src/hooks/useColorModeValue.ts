2274d1044a3334177d0c52cf47a9db0ac63bfb8f
Akash Gautam <akashgautam@geekyants.com>
2023-10-30 12:13:56 +0530

    fix: useColorModeValue hook fix

--- src/hooks/useColorModeValue.ts
+++ src/hooks/useColorModeValue.ts
@@ -1,7 +1,8 @@
-import { useColorMode } from '@gluestack-style/react';
+import { useContext } from 'react';
+import { HooksContext } from '../components/Provider';
 
 export function useColorModeValue(props: any[]) {
-  const colorMode = useColorMode();
+  const { colorMode } = useContext(HooksContext) as { colorMode: string };
   if (colorMode === 'light') return props[0];
   return props[1];
 }
