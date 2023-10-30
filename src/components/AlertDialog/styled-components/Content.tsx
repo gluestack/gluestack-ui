3e6fb23dc5aa5fa7a1220932a5960c553d7ed61c
Suraj <suraj@Surajs-MacBook-Pro.local>
2023-10-30 14:59:47 +0530

    fix: alert content

--- src/components/AlertDialog/styled-components/Content.tsx
+++ src/components/AlertDialog/styled-components/Content.tsx
@@ -1,9 +1,12 @@
-import { AnimationResolver } from '@gluestack-style/animation-plugin';
+import {
+  AnimationResolver,
+  AnimatedView,
+} from '@gluestack-style/animation-resolver';
+import { MotionAnimationDriver } from '@gluestack-style/legend-motion-animation-driver';
 import { styled } from '@gluestack-style/react';
-import { Motion } from '@legendapp/motion';
 
 export default styled(
-  Motion.View,
+  AnimatedView,
   {
     // @ts-ignore
     'bg': '$muted.50',
@@ -45,6 +48,6 @@ export default styled(
     ancestorStyle: ['_content'],
   } as const,
   {
-    plugins: [new AnimationResolver({})],
+    plugins: [new AnimationResolver(MotionAnimationDriver)],
   }
 );
