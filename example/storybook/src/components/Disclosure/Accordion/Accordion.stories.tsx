29931f825740420cc2099f9fbb8597748fa5de92
Damini <daminipandey1310@gmail.com>
2023-11-10 03:35:11 +0530

    fix: remove argsType

--- example/storybook/src/components/Disclosure/Accordion/Accordion.stories.tsx
+++ example/storybook/src/components/Disclosure/Accordion/Accordion.stories.tsx
@@ -12,6 +12,24 @@ const AccordionMeta: ComponentMeta<typeof Accordion> = {
   metaInfo: {
     componentDescription: `The Actionsheet component presents a set of options to the user, overlaid on top of the app's content, allowing them to take quick actions without leaving the current page or view.`,
   },
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['sm', 'md', 'lg'],
+    },
+    variant: {
+      control: 'select',
+      options: ['filled', 'unfilled'],
+    },
+    type: {
+      control: 'select',
+      options: ['single', 'multiple'],
+    },
+    isDisabled: {
+      control: 'boolean',
+      options: [true, false],
+    },
+  },
 };
 
 export default AccordionMeta;
