4f1b056d6bff5b8be35bdcfd559a90333dfcd5ce
Damini <daminipandey1310@gmail.com>
2023-11-27 18:00:56 +0530

    feat: accordion hooks

---
+++ packages/accordion/src/useAccordion.ts
@@ -0,0 +1,191 @@
+import { useState, useEffect } from 'react';
+import type { Collection } from './types';
+
+type Props = {
+  type: 'single' | 'multiple';
+  isDisabledAccordion: boolean;
+  isCollapsible: boolean;
+  value?: string | string[];
+  defaultValue?: string | string[];
+  onValueChange?: (value: any) => void;
+};
+
+export const useAccordion = (props: Props) => {
+  const {
+    type,
+    isDisabledAccordion,
+    isCollapsible,
+    value,
+    defaultValue,
+    onValueChange,
+  } = props;
+
+  const [collection, setCollection] = useState<Collection>([]);
+
+  useEffect(() => {
+    if (value) {
+      if (type === 'single' && typeof value === 'string') {
+        setCollection((prevCollection) =>
+          prevCollection.map((item) => {
+            if (item.key === value) {
+              item.isExpanded = true;
+            } else {
+              item.isExpanded = false;
+            }
+            return item;
+          })
+        );
+      } else if (type === 'multiple' && Array.isArray(value)) {
+        setCollection((prevCollection) =>
+          prevCollection.map((item) => {
+            if ((value as string[]).includes(item.key)) {
+              item.isExpanded = true;
+            } else {
+              item.isExpanded = false;
+            }
+            return item;
+          })
+        );
+      }
+    } else {
+      setCollection((prevCollection) =>
+        prevCollection.map((item) => {
+          item.isExpanded = false;
+          return item;
+        })
+      );
+    }
+    if (!value) {
+      if (defaultValue) {
+        if (type === 'single' && typeof defaultValue === 'string') {
+          setCollection((prevCollection) =>
+            prevCollection.map((item) => {
+              if (item.key === defaultValue) {
+                item.isExpanded = true;
+              } else {
+                item.isExpanded = false;
+              }
+              return item;
+            })
+          );
+        } else if (type === 'multiple' && Array.isArray(defaultValue)) {
+          setCollection((prevCollection) =>
+            prevCollection.map((item) => {
+              if ((defaultValue as string[]).includes(item.key)) {
+                item.isExpanded = true;
+              } else {
+                item.isExpanded = false;
+              }
+              return item;
+            })
+          );
+        }
+      } else {
+        setCollection((prevCollection) =>
+          prevCollection.map((item) => {
+            item.isExpanded = false;
+            return item;
+          })
+        );
+      }
+    }
+  }, [type, value, defaultValue, setCollection]);
+
+  useEffect(() => {
+    if (onValueChange) {
+      if (type === 'single') {
+        const item = collection.find(
+          (accordionItem) => accordionItem.isExpanded
+        );
+        if (item) {
+          onValueChange(item.key);
+        }
+      } else if (type === 'multiple') {
+        const items = collection
+          .filter((item) => item.isExpanded)
+          .map((item) => item.key);
+        onValueChange(items);
+      }
+    }
+  }, [onValueChange, collection, type]);
+
+  const insertItem = ({
+    key,
+    isExpanded,
+    isDisabled,
+  }: {
+    key: string;
+    isExpanded: boolean;
+    isDisabled: boolean;
+  }) => {
+    if (key) {
+      setCollection((prevCollection) => {
+        const isItemAlreadyInCollection = prevCollection.some(
+          (item) => item.key === key
+        );
+
+        return isItemAlreadyInCollection
+          ? prevCollection
+          : [...prevCollection, { key, isExpanded, isDisabled }];
+      });
+    }
+  };
+
+  const toggleItem = (itemValue: string, isDisabled = false) => {
+    if (isDisabled || !itemValue) {
+      return;
+    }
+    if (isDisabledAccordion && isDisabled) {
+      return;
+    }
+    if (type === 'single') {
+      if (isCollapsible) {
+        const newCollection = collection.map((item) => {
+          if (item.key === itemValue) {
+            item.isExpanded = !item.isExpanded;
+          } else {
+            item.isExpanded = false;
+          }
+          return item;
+        });
+        setCollection(newCollection);
+      } else {
+        const newCollection = collection.map((item) => {
+          if (item.key === itemValue && !item.isExpanded) {
+            item.isExpanded = true;
+          } else if (item.key !== itemValue) {
+            item.isExpanded = false;
+          }
+          return item;
+        });
+        setCollection(newCollection);
+      }
+    } else {
+      if (isCollapsible) {
+        const newCollection = collection.map((item) => {
+          if (item.key === itemValue) {
+            item.isExpanded = !item.isExpanded;
+          }
+          return item;
+        });
+        setCollection(newCollection);
+      } else {
+        const newCollection = collection.map((item) => {
+          if (item.key === itemValue && !item.isExpanded) {
+            item.isExpanded = true;
+          }
+          return item;
+        });
+        setCollection(newCollection);
+      }
+    }
+  };
+
+  return {
+    state: {
+      collection,
+      insertItem,
+      toggleItem,
+    },
+  };
+};
---
+++ packages/accordion/src/useAccordionItem.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import type { State } from './types';
+
+type Props = {
+  value: string;
+  isDisabled?: boolean;
+};
+
+export const useAccordionItem = (state: State, props: Props) => {
+  const { insertItem, toggleItem } = state;
+  const { value, isDisabled = false } = props;
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const buttonId = `accordion-button-${value}`;
+  const regionId = `accordion-region-${value}`;
+
+  useEffect(() => {
+    insertItem({ key: value, isExpanded, isDisabled });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const accordionItem = state.collection.find((item) => item.key === value);
+
+    if (!accordionItem) return;
+
+    setIsExpanded(accordionItem.isExpanded);
+  }, [state.collection, value]);
+
+  const toggle = () => {
+    toggleItem(value, isDisabled);
+  };
+
+  return {
+    regionProps: {
+      'id': regionId,
+      'aria-labelledby': buttonId,
+      'role': 'region',
+    },
+    buttonProps: {
+      'aria-controls': regionId,
+      'aria-disabled': isDisabled,
+      'aria-expanded': isExpanded,
+      'onPress': toggle,
+      'role': 'button',
+      'isExpanded': isExpanded,
+    },
+  };
+};
