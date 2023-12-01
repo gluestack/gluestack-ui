9808c633060b84ca45f6d881ffb8c3e27a63a2a5
Damini <daminipandey1310@gmail.com>
2023-12-01 13:32:39 +0530

    fix: accordion hooks

--- packages/accordion/src/useAccordionItem.ts
+++ packages/accordion/src/useAccordionItem.ts
@@ -1,42 +1,21 @@
-import { useEffect, useState } from 'react';
 import { State } from './types';
 
 type Props = {
   value: string;
-  isDisabled: boolean;
   isExpanded: boolean;
-  handleToggle: (value: string) => void;
+  isDisabled: boolean;
 };
 
 export const useAccordionItem = (state: State, props: Props) => {
-  const { insertItem, toggleItem } = state;
-  const { value, isExpanded, isDisabled, handleToggle } = props;
-
-  const [expanded, setExpanded] = useState(false);
+  const { toggleItem } = state;
+  const { value, isExpanded, isDisabled } = props;
 
   // Generate unique IDs for each accordion trigger and region
   const buttonId = `accordion-button-${value}`;
   const regionId = `accordion-region-${value}`;
 
-  // Insert the item into the collection on mount
-  useEffect(() => {
-    insertItem({ key: value, isExpanded, isDisabled });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Update the expanded state when the collection changes
-  useEffect(() => {
-    const accordionItem = state.collection.get(value);
-
-    if (!accordionItem) return;
-
-    setExpanded(accordionItem.isExpanded);
-  }, [state.collection, value]);
-
-  // Toggle the item in the collection and call the toggle callback
   const toggle = () => {
     toggleItem(value, isDisabled);
-    handleToggle(value);
   };
 
   return {
@@ -46,11 +25,13 @@ export const useAccordionItem = (state: State, props: Props) => {
       'role': 'region',
     },
     buttonProps: {
+      'id': buttonId,
       'aria-controls': regionId,
       'aria-disabled': isDisabled,
-      'aria-expanded': expanded,
+      'aria-expanded': isExpanded,
       'onPress': toggle,
       'role': 'button',
     },
+    isExpanded,
   };
 };
