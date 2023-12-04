0aa966ac959667ee7a2774a0f7e226adeb338461
Rohit Singh <rohit99.ind@gmail.com>
2023-12-04 18:59:27 +0530

    fix: tooltip flickering issue

--- packages/themed/src/components/Tooltip/styled-components/Root.tsx
+++ packages/themed/src/components/Tooltip/styled-components/Root.tsx
@@ -1,6 +1,12 @@
 import { styled } from '@gluestack-style/react';
 import { View } from 'react-native';
 
-export default styled(View, {}, {
-  componentName: 'Tooltip',
-} as const);
+export default styled(
+  View,
+  {
+    pointerEvents: 'none',
+  },
+  {
+    componentName: 'Tooltip',
+  } as const
+);
