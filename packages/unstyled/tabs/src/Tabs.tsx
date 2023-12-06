7d2b4f0af00de5024e91727ab454a2988b745946
amars29 <amars@geekyants.com>
2023-12-06 13:32:39 +0530

    feat: updated tabs

--- packages/unstyled/tabs/src/Tabs.tsx
+++ packages/unstyled/tabs/src/Tabs.tsx
@@ -1,23 +1,39 @@
 import React, { memo } from 'react';
 import { forwardRef } from 'react';
 import { TabProvider } from './TabProvider';
+import { useControllableState } from '@gluestack-ui/hooks';
 
 export const Tabs = <StyledTabs,>(
   StyledTabs: React.ComponentType<StyledTabs>
 ) =>
   memo(
     forwardRef(
-      ({ value, ...props }: StyledTabs & { value?: string }, ref?: any) => {
-        const DEFAULT_TAB = 'tab-0';
-        const [currentActiveTab, setCurrentActiveTab] = React.useState(
-          value ?? DEFAULT_TAB
-        );
-
-        const onChange = (currentValue: string) =>
-          setCurrentActiveTab(currentValue);
+      (
+        {
+          value,
+          onChange,
+          defaultValue,
+          ...props
+        }: StyledTabs & {
+          value?: string;
+          defaultValue: string;
+          onChange: (value: string) => void;
+        },
+        ref?: any
+      ) => {
+        const [currentActiveTab, setCurrentActiveTab] = useControllableState({
+          value,
+          defaultValue,
+          onChange: (val) => {
+            onChange && onChange(val);
+          },
+        });
 
         return (
-          <TabProvider currentActiveTab={currentActiveTab} onChange={onChange}>
+          <TabProvider
+            currentActiveTab={currentActiveTab}
+            onChange={setCurrentActiveTab}
+          >
             <StyledTabs {...(props as StyledTabs)} ref={ref} />
           </TabProvider>
         );
