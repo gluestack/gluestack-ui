861af3e1b5d055efed8a8a2f1b143f573f0e9133
Vidhi Kataria <katariavidhi99@gmail.com>
2023-12-07 15:39:07 +0530

    fix: storybook

--- example/storybook/src/unstyled/ui-components/Button/index.tsx
+++ example/storybook/src/unstyled/ui-components/Button/index.tsx
@@ -1,41 +1,14 @@
-import React from 'react';
 import { createButton } from '@gluestack-ui/button';
 import { Root, Text, Group, Spinner, Icon } from './styled-components';
 
-export const AccessibleButton = createButton({
+export const Button = createButton({
   Root,
   Text,
   Group,
   Spinner,
   Icon,
 });
-
-//@ts-ignore
-export const Button = ({ children, ...props }) => {
-  // console.log(children, typeof children === 'string', 'Chlired button');
-  return (
-    <AccessibleButton {...props}>
-      {typeof children === 'string' ? (
-        <AccessibleButton.Text>{children}</AccessibleButton.Text>
-      ) : (
-        <>{children}</>
-      )}
-    </AccessibleButton>
-  );
-};
-
-//@ts-ignore
-export const ButtonText = ({ children, ...props }) => {
-  return <AccessibleButton.Text {...props}>{children}</AccessibleButton.Text>;
-};
-// export const ButtonGroup = Button.Group;
-// export const ButtonSpinner = Button.Spinner;
-// export const ButtonIcon = Button.Icon;
-
-// const ButtonTemp = ({ children, ...props }) => {
-//   return <AccessibleButton>{children}</AccessibleButton>;
-// };
-
-// const Button = ButtonTemp;
-// Button.Text = AccessibleButton.Text;
-// export const Button = ButtonTemp;
+export const ButtonText = Button.Text;
+export const ButtonGroup = Button.Group;
+export const ButtonSpinner = Button.Spinner;
+export const ButtonIcon = Button.Icon;
