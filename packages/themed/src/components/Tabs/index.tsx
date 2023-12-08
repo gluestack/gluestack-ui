9e203f41bd53b466fad1d06a3e9cf9f4d2f3905d
amars29 <amars@geekyants.com>
2023-12-08 13:40:13 +0530

    feat: updated theme of tabs

--- packages/themed/src/components/Tabs/index.tsx
+++ packages/themed/src/components/Tabs/index.tsx
@@ -1,26 +1,27 @@
 import { createTabs } from '@gluestack-ui/tabs';
 import {
   Root,
-  Tab,
-  TabPanels,
-  TabPanel,
-  TabList,
-  TabTitle,
-  TabIcon,
+  StyledTab,
+  StyledTabPanels,
+  StyledTabPanel,
+  StyledTabList,
+  StyledTabTitle,
+  StyledTabIcon,
 } from './styled-components';
 
 export const Tabs = createTabs({
   Root,
-  Tab,
-  TabPanels,
-  TabPanel,
-  TabList,
-  TabTitle,
-  TabIcon,
+  Tab: StyledTab,
+  TabPanels: StyledTabPanels,
+  TabPanel: StyledTabPanel,
+  TabList: StyledTabList,
+  TabTitle: StyledTabTitle,
+  TabIcon: StyledTabIcon,
 });
-export const TabsTab = Tabs.Tab;
-export const TabsTabPanels = Tabs.TabPanels;
-export const TabsTabPanel = Tabs.TabPanel;
-export const TabsTabList = Tabs.TabList;
-export const TabsTabTitle = Tabs.TabTitle;
-export const TabsTabIcon = Tabs.TabIcon;
+
+export const Tab = Tabs.Tab;
+export const TabPanels = Tabs.TabPanels;
+export const TabPanel = Tabs.TabPanel;
+export const TabList = Tabs.TabList;
+export const TabTitle = Tabs.TabTitle;
+export const TabIcon = Tabs.TabIcon;
