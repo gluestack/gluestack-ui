6851130a61e9d0b62d098101745fb1689c4c1187
ankit-tailor <tailorankit3345@gmail.com>
2024-01-10 12:41:11 +0530

    feat: theme support for useTheme

--- packages/styled/react/src/hooks/useToken.ts
+++ packages/styled/react/src/hooks/useToken.ts
@@ -1,15 +1,35 @@
 import { useStyled } from '../StyledProvider';
-import type { ICustomConfig } from '../types';
+import { useTheme } from '../Theme';
+import type { GSConfig, Tokens } from '../types';
+
+type GSTokens<T> = T extends keyof GSConfig['tokens']
+  ? keyof GSConfig['tokens'][T]
+  : string;
 
 /**
  *
- * @param tokenScale Type of the token ex: colors, spacing, fontSizes, etc
- * @param token Token name ex: red500, 1, sm, etc
+ * @param tokenScale Token scale ex: colors, space, fontSizes, etc
+ * @param token Token name ex: primary200, red500, 1, 2, sm, etc
  * @returns
  */
-export const useToken = (tokenScale: string, token: string) => {
-  const theme: ICustomConfig = useStyled();
-  // @ts-ignore
+export const useToken = <T extends keyof Tokens>(
+  tokenScale: T,
+  token: GSTokens<T>
+) => {
+  const theme = useStyled();
+  const { themes: activeThemes = [] } = useTheme();
+
+  for (const key in activeThemes?.reverse()) {
+    const currentTheme = activeThemes[key];
+    if (theme.config.themes.hasOwnProperty(currentTheme)) {
+      const currentThemeTokens = theme?.config?.themes[currentTheme];
+
+      if (currentThemeTokens?.[tokenScale]?.[token]) {
+        return currentThemeTokens?.[tokenScale]?.[token];
+      }
+    }
+  }
+
   const themeTokens = theme.config.tokens;
-  return themeTokens?.[`${tokenScale}`]?.[`${token}`] ?? token;
+  return themeTokens?.[tokenScale]?.[token] ?? token;
 };
