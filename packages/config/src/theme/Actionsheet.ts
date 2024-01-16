e5b99114841f06a7ec38c94791aa44d48ff59078
Rishabh Sahu <rishabhsahu2421@gmail.com>
2024-01-16 15:32:43 +0530

    fix: removed pointerEvents from actionsheet

--- packages/config/src/theme/Actionsheet.ts
+++ packages/config/src/theme/Actionsheet.ts
@@ -3,7 +3,4 @@ import { createStyle } from '@gluestack-style/react';
 export const Actionsheet = createStyle({
   width: '$full',
   height: '$full',
-  _web: {
-    pointerEvents: 'none',
-  },
 });
