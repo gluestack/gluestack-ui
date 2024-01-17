9b723f362903962c2fcf73e81f03891dc304251e
Rajat Chaudhary <rajatchaudhary.rc693@gmail.com>
2024-01-17 18:29:07 +0530

    fix: removed lineHeight from Text, Heading and InputField

--- packages/config/src/theme/Heading.ts
+++ packages/config/src/theme/Heading.ts
@@ -35,10 +35,63 @@ export const Heading = createStyle({
         textDecorationLine: 'line-through',
       },
     },
+    size: {
+      '5xl': {
+        //@ts-ignore
+        props: { as: H1 },
+        fontSize: '$6xl',
+      },
+      '4xl': {
+        //@ts-ignore
+        props: { as: H1 },
+        fontSize: '$5xl',
+      },
+
+      '3xl': {
+        //@ts-ignore
+        props: { as: H1 },
+        fontSize: '$4xl',
+      },
+
+      '2xl': {
+        //@ts-ignore
+        props: { as: H2 },
+        fontSize: '$3xl',
+      },
+
+      'xl': {
+        //@ts-ignore
+        props: { as: H3 },
+        fontSize: '$2xl',
+      },
+
+      'lg': {
+        //@ts-ignore
+        props: { as: H4 },
+        fontSize: '$xl',
+      },
+
+      'md': {
+        //@ts-ignore
+        props: { as: H5 },
+        fontSize: '$lg',
+      },
+
+      'sm': {
+        //@ts-ignore
+        props: { as: H6 },
+        fontSize: '$md',
+      },
+
+      'xs': {
+        //@ts-ignore
+        props: { as: H6 },
+        fontSize: '$sm',
+      },
+    },
     sub: {
       true: {
         fontSize: '$xs',
-        lineHeight: '$xs',
       },
     },
     italic: {
@@ -49,69 +102,6 @@ export const Heading = createStyle({
     highlight: {
       true: {
         bg: '$yellow500',
-      },
-    },
-    size: {
-      '5xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$6xl',
-        lineHeight: '$7xl',
-      },
-      '4xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$5xl',
-        lineHeight: '$6xl',
-      },
-
-      '3xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$4xl',
-        lineHeight: '$5xl',
-      },
-
-      '2xl': {
-        //@ts-ignore
-        props: { as: H2 },
-        fontSize: '$3xl',
-        lineHeight: '$3xl',
-      },
-
-      'xl': {
-        //@ts-ignore
-        props: { as: H3 },
-        fontSize: '$2xl',
-        lineHeight: '$3xl',
-      },
-
-      'lg': {
-        //@ts-ignore
-        props: { as: H4 },
-        fontSize: '$xl',
-        lineHeight: '$2xl',
-      },
-
-      'md': {
-        //@ts-ignore
-        props: { as: H5 },
-        fontSize: '$lg',
-        lineHeight: '$lg',
-      },
-
-      'sm': {
-        //@ts-ignore
-        props: { as: H6 },
-        fontSize: '$md',
-        lineHeight: '$lg',
-      },
-
-      'xs': {
-        //@ts-ignore
-        props: { as: H6 },
-        fontSize: '$sm',
-        lineHeight: '$xs',
       },
     },
   },
