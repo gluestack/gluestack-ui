174d1e972dfb5abc66270bcf47e8db1fb6cbeace
amanzrx4 <amansagar262@gmail.com>
2024-01-19 18:32:13 +0530

    fix: add default config if config is not provided

--- packages/themed/src/components/Provider/index.tsx
+++ packages/themed/src/components/Provider/index.tsx
@@ -3,6 +3,7 @@ import { createProvider } from '@gluestack-ui/provider';
 import { StyledProvider } from '@gluestack-style/react';
 import { OverlayProvider } from '@gluestack-ui/overlay';
 import { ToastProvider } from '@gluestack-ui/toast';
+import { config } from '@gluestack-ui/config';
 
 const GluestackUIStyledProvider = createProvider({ StyledProvider });
 
@@ -17,7 +18,7 @@ const GluestackUIProvider = ({
   return (
     <>
       {/** @ts-ignore */}
-      <GluestackUIStyledProvider colorMode="light" {...props}>
+      <GluestackUIStyledProvider colorMode="light" config={config} {...props}>
         <OverlayProvider>
           <ToastProvider>{children}</ToastProvider>
         </OverlayProvider>
