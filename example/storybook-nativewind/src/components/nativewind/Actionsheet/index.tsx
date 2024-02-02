5ef31528efd39477a74869f357986cf840fab7ef
ankit-tailor <tailorankit3345@gmail.com>
2024-02-02 13:42:27 +0530

    feat: actionsheet component

--- example/storybook-nativewind/src/components/nativewind/Actionsheet/index.tsx
+++ example/storybook-nativewind/src/components/nativewind/Actionsheet/index.tsx
@@ -1,10 +1,4 @@
-import { H1, H2, H3, H4, H5, H6 } from '@expo/html-elements';
-import {
-  AnimatePresence,
-  AnimatedPressable,
-  AnimatedView,
-} from '@gluestack-style/animation-resolver';
-import { styled, AsForwarder } from '@gluestack-style/react';
+import { H4 } from '@expo/html-elements';
 import { createActionsheet } from '@gluestack-ui/actionsheet';
 import {
   Pressable,
@@ -14,366 +8,361 @@ import {
   VirtualizedList,
   FlatList,
   SectionList,
+  Platform,
 } from 'react-native';
+import {
+  tva,
+  withStyleContext,
+  withStyleContextAndStates,
+} from '@gluestack-ui/nativewind-utils';
+import React from 'react';
 
-const StyledRoot = styled(View, {
-  width: '$full',
-  height: '$full',
-  _web: {
-    pointerEvents: 'none',
-  },
+export const AccessibleActionsheet = createActionsheet({
+  Root: View,
+  Content: withStyleContext(View),
+  Item:
+    Platform.OS === 'web'
+      ? withStyleContext(Pressable)
+      : withStyleContextAndStates(Pressable),
+  ItemText: Text,
+  DragIndicator: View,
+  IndicatorWrapper: View,
+  Backdrop: Pressable,
+  ScrollView: ScrollView,
+  VirtualizedList: VirtualizedList,
+  FlatList: FlatList,
+  SectionList: SectionList,
+  SectionHeaderText: H4,
+  Icon: View,
+  //@ts-ignore
+  AnimatePresence: null,
 });
 
-const StyledContent = styled(
-  AnimatedView,
-  {
-    alignItems: 'center',
-    borderTopLeftRadius: '$3xl',
-    borderTopRightRadius: '$3xl',
-    h: '$full',
-    p: '$2',
-    bg: '$background0',
+const actionsheetStyle = tva({ base: 'w-full h-full web:pointer-events-none' });
 
-    _sectionHeaderBackground: {
-      bg: '$background0',
-    },
-
-    defaultProps: {
-      hardShadow: '5',
-    },
-
-    _web: {
-      userSelect: 'none',
-      pointerEvents: 'auto',
-    },
-  },
-  {
-    descendantStyle: ['_sectionHeaderBackground'],
-  }
-);
-
-const StyledItem = styled(
-  Pressable,
-  {
-    'p': '$3',
-    'flexDirection': 'row',
-    'alignItems': 'center',
-    'rounded': '$sm',
-    'w': '$full',
-
-    ':disabled': {
-      opacity: 0.4,
-      _web: {
-        // @ts-ignore
-        pointerEvents: 'all !important',
-        cursor: 'not-allowed',
-      },
-    },
-
-    ':hover': {
-      bg: '$background50',
-    },
-
-    ':active': {
-      bg: '$background100',
-    },
-
-    ':focus': {
-      bg: '$background100',
-    },
-
-    '_web': {
-      ':focusVisible': {
-        bg: '$background100',
-      },
-    },
-  },
-  {
-    descendantStyle: ['_text', '_icon'],
-  }
-);
-
-const StyledItemText = styled(
-  Text,
-  {
-    mx: '$2',
-    props: {
-      size: 'md',
-    },
-    color: '$text800',
-  },
-  {
-    ancestorStyle: ['_text'],
-  }
-);
-
-const StyledDragIndicator = styled(View, {
-  w: '$16',
-  h: '$1',
-  bg: '$background400',
-  rounded: '$full',
+const actionsheetContentStyle = tva({
+  base: 'items-center rounded-tl-3xl rounded-tr-3xl p-2 bg-background0 web:pointer-events-auto web:select-none shadow-lg',
 });
 
-const StyledDragIndicatorWrapper = styled(View, {
-  py: '$1',
-  w: '$full',
-  alignItems: 'center',
+const actionsheetItemStyle = tva({
+  base: 'w-full flex-row items-center p-3 rounded-sm disabled:opacity-40 disabled:web:pointer-events-auto disabled:web:cursor-not-allowed hover:bg-background-50 active:bg-background-100 focus:bg-background-100 web:focus-visible:bg-background-100',
 });
 
-const StyledBackdrop = styled(AnimatedPressable, {
-  ':initial': {
-    opacity: 0,
-  },
-
-  ':animate': {
-    opacity: 0.5,
-  },
-
-  ':exit': {
-    opacity: 0,
-  },
-
-  'position': 'absolute',
-  'left': 0,
-  'top': 0,
-  'right': 0,
-  'bottom': 0,
-  'bg': '$background950',
-
-  '_web': {
-    cursor: 'default',
-    pointerEvents: 'auto',
-  },
-});
-
-const StyledScrollView = styled(ScrollView, {
-  w: '$full',
-  h: 'auto',
-});
-
-const StyledVirtualizedList = styled(VirtualizedList, {
-  w: '$full',
-  h: 'auto',
-});
-
-const StyledFlatList = styled(FlatList, {
-  w: '$full',
-  h: 'auto',
-});
-
-const StyledSectionList = styled(SectionList, {
-  w: '$full',
-  h: 'auto',
-});
-
-const StyledSectionHeaderText = styled(H4, {
-  letterSpacing: '$sm',
-  fontWeight: '$bold',
-  fontFamily: '$heading',
-
-  // Overrides expo-html default styling
-  marginVertical: 0,
-
+const actionsheetItemTextStyle = tva({
+  base: 'text-typography-700 font-normal font-body tracking-md text-left mx-2',
   variants: {
     isTruncated: {
-      true: {
-        props: {
-          // @ts-ignore
-          numberOfLines: 1,
-          ellipsizeMode: 'tail',
-        },
-      },
+      true: '',
     },
     bold: {
-      true: {
-        fontWeight: '$bold',
-      },
+      true: 'font-bold',
     },
     underline: {
-      true: {
-        textDecorationLine: 'underline',
-      },
+      true: 'underline',
     },
     strikeThrough: {
-      true: {
-        textDecorationLine: 'line-through',
-      },
+      true: 'line-through',
     },
     size: {
-      '5xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$6xl',
-      },
-      '4xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$5xl',
-      },
-
-      '3xl': {
-        //@ts-ignore
-        props: { as: H1 },
-        fontSize: '$4xl',
-      },
-
-      '2xl': {
-        //@ts-ignore
-        props: { as: H2 },
-        fontSize: '$3xl',
-      },
-
-      'xl': {
-        //@ts-ignore
-        props: { as: H3 },
-        fontSize: '$2xl',
-      },
-
-      'lg': {
-        //@ts-ignore
-        props: { as: H4 },
-        fontSize: '$xl',
-      },
-
-      'md': {
-        //@ts-ignore
-        props: { as: H5 },
-        fontSize: '$lg',
-      },
-
-      'sm': {
-        //@ts-ignore
-        props: { as: H6 },
-        fontSize: '$md',
-      },
-
-      'xs': {
-        //@ts-ignore
-        props: { as: H6 },
-        fontSize: '$sm',
-      },
-    },
-    sub: {
-      true: {
-        fontSize: '$xs',
-      },
-    },
-    italic: {
-      true: {
-        fontStyle: 'italic',
-      },
-    },
-    highlight: {
-      true: {
-        bg: '$yellow500',
-      },
+      '2xs': 'text-2xs',
+      'xs': 'text-xs',
+      'sm': 'text-sm',
+      'md': 'text-md',
+      'lg': 'text-lg',
+      'xl': 'text-xl',
+      '2xl': 'text-2xl',
+      '3xl': 'text-3xl',
+      '4xl': 'text-4xl',
+      '5xl': 'text-5xl',
+      '6xl': 'text-6xl',
     },
   },
-  color: '$text500',
-  props: { size: 'xs' },
-  textTransform: 'uppercase',
-  p: '$3',
+  defaultVariants: {
+    size: 'md',
+  },
 });
 
-const StyledIcon = styled(
-  AsForwarder,
-  {
-    variants: {
-      size: {
-        '2xs': {
-          h: '$3',
-          w: '$3',
-          props: {
-            // @ts-ignore
-            size: 12,
-          },
-        },
-        'xs': {
-          h: '$3.5',
-          w: '$3.5',
-          props: {
-            //@ts-ignore
-            size: 14,
-          },
-        },
-        'sm': {
-          h: '$4',
-          w: '$4',
-          props: {
-            //@ts-ignore
-            size: 16,
-          },
-        },
-        'md': {
-          h: '$4.5',
-          w: '$4.5',
-          props: {
-            //@ts-ignore
-            size: 18,
-          },
-        },
-        'lg': {
-          h: '$5',
-          w: '$5',
-          props: {
-            //@ts-ignore
-            size: 20,
-          },
-        },
-        'xl': {
-          h: '$6',
-          w: '$6',
-          props: {
-            //@ts-ignore
-            size: 24,
-          },
-        },
-      },
+const actionsheetDragIndicatorStyle = tva({
+  base: 'w-16 h-1 bg-background400 rounded-full',
+});
+
+const actionsheetDragIndicatorWrapperStyle = tva({
+  base: 'w-full py-1 items-center',
+});
+
+const actionsheetBackdropStyle = tva({
+  base: 'absolute left-0 top-0 right-0 bottom-0 bg-background950 web:cursor-default web:pointer-events-auto',
+});
+
+const actionsheetScrollViewStyle = tva({
+  base: 'w-full h-auto',
+});
+
+const actionsheetVirtualizedListStyle = tva({
+  base: 'w-full h-auto',
+});
+
+const actionsheetFlatListStyle = tva({
+  base: 'w-full h-auto',
+});
+
+const actionsheetSectionListStyle = tva({
+  base: 'w-full h-auto',
+});
+
+const actionsheetSectionHeaderTextStyle = tva({
+  base: 'leading-5 font-bold font-heading my-0 text-typography-500 p-3 uppercase',
+  variants: {
+    isTruncated: {
+      true: '',
     },
-    props: {
-      size: 'sm',
+    bold: {
+      true: 'font-bold',
+    },
+    underline: {
+      true: 'underline',
+    },
+    strikeThrough: {
+      true: 'line-through',
+    },
+    size: {
+      '5xl': 'text-5xl',
+      '4xl': 'text-4xl',
+      '3xl': 'text-3xl',
+      '2xl': 'text-2xl',
+      'xl': 'text-xl',
+      'lg': 'text-lg',
+      'md': 'text-md',
+      'sm': 'text-sm',
+      'xs': 'text-xs',
     },
 
-    color: '$background500',
-  },
-  {
-    componentName: 'BaseIcon',
-    resolveProps: ['stroke', 'fill'],
-  } as const,
-  {
-    propertyTokenMap: {
-      stroke: 'colors',
-      fill: 'colors',
+    sub: {
+      true: 'text-xs',
     },
+    italic: {
+      true: 'italic',
+    },
+    highlight: {
+      true: 'bg-yellow500',
+    },
+  },
+  defaultVariants: {
+    size: 'xs',
+  },
+});
+
+const actionsheetIconStyle = tva({
+  base: 'bg-background-500',
+  variants: {
+    size: {
+      '2xs': 'h-3 w-3',
+      'xs': 'h-3.5 w-3.5',
+      'sm': 'h-4 w-4',
+      'md': 'w-4 h-4',
+      'lg': 'h-5 w-5',
+      'xl': 'h-6 w-6',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+});
+
+const Actionsheet = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet
+        className={actionsheetStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 
-export const Actionsheet = createActionsheet({
-  Root: StyledRoot,
-  Content: StyledContent,
-  Item: StyledItem,
-  ItemText: StyledItemText,
-  DragIndicator: StyledDragIndicator,
-  IndicatorWrapper: StyledDragIndicatorWrapper,
-  Backdrop: StyledBackdrop,
-  ScrollView: StyledScrollView,
-  VirtualizedList: StyledVirtualizedList,
-  FlatList: StyledFlatList,
-  SectionList: StyledSectionList,
-  SectionHeaderText: StyledSectionHeaderText,
-  Icon: StyledIcon,
-  AnimatePresence: AnimatePresence,
-});
+const ActionsheetContent = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.Content
+        className={actionsheetContentStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
-export const ActionsheetContent = Actionsheet.Content;
-export const ActionsheetItem = Actionsheet.Item;
-export const ActionsheetItemText = Actionsheet.ItemText;
-export const ActionsheetDragIndicator = Actionsheet.DragIndicator;
-export const ActionsheetDragIndicatorWrapper = Actionsheet.DragIndicatorWrapper;
-export const ActionsheetBackdrop = Actionsheet.Backdrop;
-export const ActionsheetScrollView = Actionsheet.ScrollView;
-export const ActionsheetVirtualizedList = Actionsheet.VirtualizedList;
-export const ActionsheetFlatList = Actionsheet.FlatList;
-export const ActionsheetSectionList = Actionsheet.SectionList;
-export const ActionsheetSectionHeaderText = Actionsheet.SectionHeaderText;
-export const ActionsheetIcon = Actionsheet.Icon;
+const ActionsheetItem = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.Item
+        className={actionsheetItemStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetItemText = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.ItemText
+        className={actionsheetItemTextStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetDragIndicator = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.DragIndicator
+        className={actionsheetDragIndicatorStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetDragIndicatorWrapper = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.DragIndicatorWrapper
+        className={actionsheetDragIndicatorWrapperStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetBackdrop = React.forwardRef(
+  ({ className, ...props }: any, ref) => {
+    return (
+      <AccessibleActionsheet.Backdrop
+        {...props}
+        className={actionsheetBackdropStyle({
+          class: className,
+        })}
+        ref={ref}
+      />
+    );
+  }
+);
+
+const ActionsheetScrollView = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.ScrollView
+        className={actionsheetScrollViewStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetVirtualizedList = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.VirtualizedList
+        className={actionsheetVirtualizedListStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetFlatList = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.FlatList
+        className={actionsheetFlatListStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetSectionList = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.SectionList
+        className={actionsheetSectionListStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetSectionHeaderText = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.SectionHeaderText
+        className={actionsheetSectionHeaderTextStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+const ActionsheetIcon = React.forwardRef(
+  ({ className, ...props }: any, ref: any) => {
+    return (
+      <AccessibleActionsheet.Icon
+        className={actionsheetIconStyle({
+          class: className,
+        })}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+export {
+  Actionsheet,
+  ActionsheetContent,
+  ActionsheetItem,
+  ActionsheetItemText,
+  ActionsheetDragIndicator,
+  ActionsheetDragIndicatorWrapper,
+  ActionsheetBackdrop,
+  ActionsheetScrollView,
+  ActionsheetVirtualizedList,
+  ActionsheetFlatList,
+  ActionsheetSectionList,
+  ActionsheetSectionHeaderText,
+  ActionsheetIcon,
+};
