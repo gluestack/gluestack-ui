fa52c42a855d466cf2c086aab8009d7c3fe06397
Viraj-10 <virajjoshi132@gmail.com>
2024-02-06 23:49:26 +0530

    feat: added alert dialog box

--- example/storybook-nativewind/src/components/nativewind/AlertDialog/index.tsx
+++ example/storybook-nativewind/src/components/nativewind/AlertDialog/index.tsx
@@ -1,218 +1,174 @@
-import {
-  AnimatePresence,
-  AnimatedView,
-  AnimatedPressable,
-} from '@gluestack-style/animation-resolver';
+import React from 'react';
 import { createAlertDialog } from '@gluestack-ui/alert-dialog';
-import { View, Pressable, ScrollView } from 'react-native';
-import { styled } from '@gluestack-style/react';
+import { View, Pressable, ScrollView, Platform } from 'react-native';
 
-const StyledRoot = styled(
-  View,
-  {
-    w: '$full',
-    h: '$full',
-    justifyContent: 'center',
-    alignItems: 'center',
+import {
+  tva,
+  useStyleContext,
+  withStates,
+  withStyleContext,
+} from '@gluestack-ui/nativewind-utils';
 
-    variants: {
-      size: {
-        xs: { _content: { w: '60%', maxWidth: 360 } },
-        sm: { _content: { w: '70%', maxWidth: 420 } },
-        md: { _content: { w: '80%', maxWidth: 510 } },
-        lg: { _content: { w: '90%', maxWidth: 640 } },
-        full: { _content: { w: '$full' } },
-      },
-    },
-    defaultProps: { size: 'md' },
+import { cssInterop } from 'nativewind';
 
-    _web: {
-      pointerEvents: 'box-none',
+const alertDialogStyle = tva({
+  base: 'w-full h-full justify-center items-center web:pointer-events-none',
+
+  variants: {
+    size: {
+      xs: '',
+      sm: '',
+      md: '',
+      lg: '',
+      full: '',
     },
   },
-  {
-    descendantStyle: ['_content'],
-  }
-);
-
-const StyledContent = styled(
-  AnimatedView,
-  {
-    'bg': '$background50',
-    'rounded': '$lg',
-    'overflow': 'hidden',
-
-    //@ts-ignore
-    ':initial': {
-      scale: 0.9,
-      opacity: 0,
-    },
-
-    ':animate': {
-      scale: 1,
-      opacity: 1,
-    },
-
-    ':exit': {
-      scale: 0.9,
-      opacity: 0,
-    },
-
-    ':transition': {
-      type: 'spring',
-      damping: 18,
-      stiffness: 250,
-      opacity: {
-        type: 'timing',
-        duration: 250,
-      },
-    },
-
-    'defaultProps': {
-      softShadow: '3',
-    },
-  },
-  {
-    ancestorStyle: ['_content'],
-  }
-);
-
-const StyledCloseButton = styled(
-  Pressable,
-  {
-    'zIndex': 1,
-    'rounded': '$sm',
-    'p': '$2',
-
-    '_icon': {
-      color: '$background400',
-    },
-
-    '_text': {
-      color: '$background400',
-    },
-
-    ':hover': {
-      _icon: {
-        color: '$background700',
-      },
-      _text: {
-        color: '$background700',
-      },
-    },
-
-    ':active': {
-      _icon: {
-        color: '$background900',
-      },
-      _text: {
-        color: '$background900',
-      },
-    },
-
-    ':focusVisible': {
-      bg: '$background100',
-
-      _icon: {
-        color: '$background900',
-      },
-
-      _text: {
-        color: '$background900',
-      },
-    },
-
-    '_web': {
-      outlineWidth: 0,
-      cursor: 'pointer',
-    },
-  },
-  {
-    descendantStyle: ['_icon', '_text'],
-  }
-);
-const StyledHeader = styled(
-  View,
-  {
-    p: '$4',
-    borderColor: '$border300',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  {}
-);
-
-const StyledFooter = styled(
-  View,
-  {
-    p: '$4',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    borderColor: '$border300',
-  },
-  {}
-);
-const StyledBody = styled(ScrollView, { px: '$4', py: '$2' }, {});
-
-const StyledBackdrop = styled(
-  AnimatedPressable,
-  {
-    ':initial': {
-      opacity: 0,
-    },
-
-    ':animate': {
-      opacity: 0.5,
-    },
-
-    ':exit': {
-      opacity: 0,
-    },
-
-    ':transition': {
-      type: 'spring',
-      damping: 18,
-      stiffness: 250,
-      opacity: {
-        type: 'timing',
-        duration: 250,
-      },
-    },
-
-    'position': 'absolute',
-    'left': 0,
-    'top': 0,
-    'right': 0,
-    'bottom': 0,
-    'bg': '$background950',
-
-    // @ts-ignore
-    '_web': {
-      cursor: 'default',
-    },
-  },
-  {}
-);
-
-const AccessibleAlertDialog = createAlertDialog({
-  Root: StyledRoot,
-  Content: StyledContent,
-  CloseButton: StyledCloseButton,
-  Header: StyledHeader,
-  Footer: StyledFooter,
-  Body: StyledBody,
-  Backdrop: StyledBackdrop,
-  //@ts-ignore
-  AnimatePresence: AnimatePresence,
 });
 
-export const AlertDialog = AccessibleAlertDialog;
-export const AlertDialogContent = AccessibleAlertDialog.Content;
-export const AlertDialogCloseButton = AccessibleAlertDialog.CloseButton;
-export const AlertDialogHeader = AccessibleAlertDialog.Header;
-export const AlertDialogFooter = AccessibleAlertDialog.Footer;
-export const AlertDialogBody = AccessibleAlertDialog.Body;
-export const AlertDialogBackdrop = AccessibleAlertDialog.Backdrop;
+const alertDialogContentStyle = tva({
+  base: 'bg-background-50 rounded-lg overflow-hidden shadow',
+  parentVariants: {
+    size: {
+      xs: 'w-[60%] max-w-[360px]',
+      sm: 'w-[70%] max-w-[420px]',
+      md: 'w-[80%] max-w-[510px]',
+      lg: 'w-[90%] max-w-[640px]',
+      full: 'w-full',
+    },
+  },
+});
+
+const alertDialogCloseButtonStyle = tva({
+  base: 'p-2 z-1 rounded-sm data-[focus-visible=true]:bg-background-100 web:outline-none web:cursor-pointer',
+});
+
+const alertDialogHeaderStyle = tva({
+  base: 'p-4 border-b border-border-300 justify-between items-center flex-row',
+});
+
+const alertDialogFooterStyle = tva({
+  base: 'p-4 border-border-300 justify-end items-center flex-row flex-wrap',
+});
+const alertDialogBodyStyle = tva({ base: 'px-4 py-2' });
+
+const alertDialogBackdropStyle = tva({
+  base: 'absolute left-0 top-0 right-0 bottom-0 bg-background-950 web:cursor-default',
+});
+
+const UIAlertDialog = createAlertDialog({
+  Root: withStyleContext(View),
+  Content: View,
+  CloseButton: Platform.OS === 'web' ? Pressable : withStates(Pressable),
+  Header: View,
+  Footer: View,
+  Body: ScrollView,
+  Backdrop: Pressable,
+  //@ts-ignore
+  AnimatePresence: null,
+});
+
+// @ts-ignore
+cssInterop(UIAlertDialog, { className: 'style' });
+cssInterop(UIAlertDialog.Content, { className: 'style' });
+cssInterop(UIAlertDialog.CloseButton, { className: 'style' });
+cssInterop(UIAlertDialog.Header, { className: 'style' });
+cssInterop(UIAlertDialog.Footer, { className: 'style' });
+cssInterop(UIAlertDialog.Body, { className: 'style' });
+cssInterop(UIAlertDialog.Backdrop, { className: 'style' });
+
+const AlertDialog = ({ className, size = 'md', ...props }: any) => {
+  return (
+    <UIAlertDialog
+      {...props}
+      className={alertDialogStyle({
+        class: className,
+      })}
+      context={{
+        size,
+      }}
+    />
+  );
+};
+
+const AlertDialogContent = ({ className, ...props }: any) => {
+  const { size } = useStyleContext();
+  return (
+    <UIAlertDialog.Content
+      {...props}
+      className={alertDialogContentStyle({
+        parentVariants: {
+          size,
+        },
+        class: className,
+      })}
+    />
+  );
+};
+const AlertDialogCloseButton = ({ className, ...props }: any) => {
+  return (
+    <UIAlertDialog.CloseButton
+      {...props}
+      className={alertDialogCloseButtonStyle({
+        class: className,
+      })}
+    />
+  );
+};
+const AlertDialogHeader = ({ className, ...props }: any) => {
+  return (
+    <UIAlertDialog.Header
+      {...props}
+      className={alertDialogHeaderStyle({
+        class: className,
+      })}
+    />
+  );
+};
+const AlertDialogFooter = ({ className, ...props }: any) => {
+  return (
+    <UIAlertDialog.Footer
+      {...props}
+      className={alertDialogFooterStyle({
+        class: className,
+      })}
+    />
+  );
+};
+const AlertDialogBody = ({ className, ...props }: any) => {
+  return (
+    <UIAlertDialog.Body
+      {...props}
+      className={alertDialogBodyStyle({
+        class: className,
+      })}
+    />
+  );
+};
+const AlertDialogBackdrop = ({ className, ...props }: any) => {
+  return (
+    <UIAlertDialog.Backdrop
+      {...props}
+      className={alertDialogBackdropStyle({
+        class: className,
+      })}
+    />
+  );
+};
+
+AlertDialog.displayName = 'AlertDialog';
+AlertDialogContent.displayName = 'AlertDialogContent';
+AlertDialogCloseButton.displayName = 'AlertDialogCloseButton';
+AlertDialogHeader.displayName = 'AlertDialogHeader';
+AlertDialogFooter.displayName = 'AlertDialogFooter';
+AlertDialogBody.displayName = 'AlertDialogBody';
+AlertDialogBackdrop.displayName = 'AlertDialogBackdrop';
+
+export {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogCloseButton,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogBody,
+  AlertDialogBackdrop,
+};
