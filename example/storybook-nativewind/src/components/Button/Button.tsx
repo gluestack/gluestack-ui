7f5c05ca110b307c27e35b1d3cec487a47e9dcb8
Suraj <suraj@Surajs-MacBook-Pro.local>
2024-02-07 19:04:49 +0530

    fix: merge conflict resolved

--- example/storybook-nativewind/src/components/Button/Button.tsx
+++ example/storybook-nativewind/src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, ButtonText } from '@/components/Button';
-import Wrapper from '@/components/Wrapper';
+import Wrapper from '../../components-example/themed/Wrapper';
 
 export const ButtonBasic = (props: any) => {
   return (
