2ba14a2a6862c1ed199fe1270e8f17e22543e6ca
Suraj <suraj@Surajs-MacBook-Pro.local>
2024-02-08 11:35:02 +0530

    fix: docs changes

--- example/storybook-nativewind/src/components-example/nativewind/Wrapper.tsx
+++ example/storybook-nativewind/src/components-example/nativewind/Wrapper.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Box, Center } from '@gluestack-ui/themed';
-import { GluestackUIProvider } from './GluestackUIProvider';
+// import { GluestackUIProvider } from './GluestackUIProvider';
 
 const Wrapper = ({ children, ...props }: any) => {
   return (
-    <GluestackUIProvider style={{ height: '100%' }}>
-      <Box
-        sx={{
-          _ios: {
-            h: '100%',
-          },
-        }}
-        {...props}
-      >
-        <Center height="100%">{children}</Center>
-      </Box>
-    </GluestackUIProvider>
+    <Box
+      sx={{
+        _ios: {
+          h: '100%',
+        },
+      }}
+      {...props}
+    >
+      <Center height="100%">{children}</Center>
+    </Box>
   );
 };
 
