31d63424afdbc107637bd62e6618dbc7b0b4ed1b
Rishabh Sahu <rishabhsahu2421@gmail.com>
2024-02-14 15:09:08 +0530

    fix: statusbar backgroundColor not working (android-only)

--- packages/themed/src/components/StatusBar/styled-components/Root.tsx
+++ packages/themed/src/components/StatusBar/styled-components/Root.tsx
@@ -1,6 +1,18 @@
 import { StatusBar } from 'react-native';
 import { styled } from '@gluestack-style/react';
 
-export default styled(StatusBar, {}, {
-  componentName: 'StatusBar',
-} as const);
+export default styled(
+  StatusBar,
+  {},
+  {
+    componentName: 'StatusBar',
+    resolveProps: ['backgroundColor'],
+  } as const,
+  {
+    propertyResolver: {
+      backgroundColor: (rawValue: any, resolver: any) => {
+        return resolver(rawValue);
+      },
+    },
+  }
+);
