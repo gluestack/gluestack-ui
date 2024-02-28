e2a54c9ea69975b45ca216fe5948809658bffdc2
Rajat Chaudhary <rajatchaudhary.rc693@gmail.com>
2024-02-28 14:42:27 +0530

    feat: added nativewind spinner component examples

--- example/storybook-nativewind/src/components/Spinner/Spinner.tsx
+++ example/storybook-nativewind/src/components/Spinner/Spinner.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
-import { HStack, Text, VStack } from '@gluestack-ui/themed';
 import { Spinner } from '@/components/ui/Spinner';
+import { HStack } from '@/components/ui/HStack';
+import { Text } from '@/components/ui/Text';
+import { VStack } from '@/components/ui/VStack';
+import colors from 'tailwindcss/colors';
 
-const SpinnerBasic = ({ ...props }) => <Spinner {...props} />;
+const SpinnerBasic = ({ ...props }) => (
+  <Spinner {...props} color={colors.gray[500]} />
+);
 
 SpinnerBasic.description =
   'This is a basic Spinner component example. Spinners are used to show a loading state of a component or page.';
