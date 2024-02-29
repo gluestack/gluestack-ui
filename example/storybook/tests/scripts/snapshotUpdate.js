85a337fbb9c1c887074b2c4b7b26b6eb69dabcec
Akash Gautam <akashgautam@geekyants.com>
2024-02-29 12:40:19 +0530

    chore: convert bash to node scripts

---
+++ example/storybook/tests/scripts/snapshotUpdate.js
@@ -0,0 +1,20 @@
+// // Import required modules
+// const { execSync } = require('child_process');
+
+// // Get arguments
+// const args = process.argv.slice(2);
+
+// // Loop through each argument
+// args.forEach((arg) => {
+//   // Extract component name
+//   const key = arg.replace(/^--/, '').split('=')[0];
+
+//   // Extract testing type name
+//   const value = arg.includes('=') ? arg.split('=')[1] : 'default';
+
+//   // Set environment variables
+//   process.env[key] = value;
+// });
+
+// // Run playwright test
+// execSync('yarn playwright test', { stdio: 'inherit' });
