30eb31dbafc485c7e655d7c66ffc17f9637a9a81
Rajat Chaudhary <rajatchaudhary.rc693@gmail.com>
2024-03-04 12:39:18 +0530

    fix: badge component nativewind

--- example/storybook-nativewind/src/components/Badge/Badge.tsx
+++ example/storybook-nativewind/src/components/Badge/Badge.tsx
@@ -1,41 +1,40 @@
 import React from 'react';
 import {
-  Center,
-  Icon,
-  Box,
-  Text,
-  VStack,
-  HStack,
-  SettingsIcon,
-  Divider,
-  AddIcon,
-  Image,
-  CheckIcon,
-  Heading,
-  Avatar,
-  AvatarFallbackText,
-  AvatarImage,
-  Button,
-  ButtonText,
-  Menu,
-  MenuIcon,
-  MenuItem,
-  MenuItemLabel,
-  GlobeIcon,
-} from '@gluestack-ui/themed';
-import {
   PaintBucket,
   PuzzleIcon,
   BadgeCheckIcon,
   BadgePlusIcon,
 } from 'lucide-react-native';
+import {
+  Avatar,
+  AvatarFallbackText,
+  AvatarImage,
+} from '@/components/ui/Avatar';
+import { Center } from '@/components/ui/Center';
+import { Box } from '@/components/ui/Box';
+import { Text } from '@/components/ui/Text';
+import { VStack } from '@/components/ui/VStack';
+import { HStack } from '@/components/ui/HStack';
+import { Divider } from '@/components/ui/Divider';
+import { Image } from '@/components/ui/Image';
+import { Heading } from '@/components/ui/Heading';
+import { Button, ButtonText } from '@/components/ui/Button';
+import { Menu, MenuItem, MenuItemLabel } from '@/components/ui/Menu';
 import { Badge, BadgeText, BadgeIcon } from '@/components/ui/Badge';
+import {
+  GlobeIcon,
+  MenuIcon,
+  Icon,
+  SettingsIcon,
+  AddIcon,
+  CheckIcon,
+} from '@/components/ui/Icon';
 
 const BadgeBasic = ({ text = 'NEW FEATURE', _colorMode, ...props }: any) => {
   return (
-    <Badge {...props} gap="$1">
+    <Badge {...props}>
       <BadgeText>{text}</BadgeText>
-      <BadgeIcon as={GlobeIcon} />
+      <BadgeIcon as={GlobeIcon} className="ml-2" />
     </Badge>
   );
 };
