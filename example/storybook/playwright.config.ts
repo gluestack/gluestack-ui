db3d41314a8bdbda7b1daf9d9420296f3c6a6257
Akash Gautam <akashgautam@geekyants.com>
2024-03-06 11:33:06 +0530

    fix: ratio of all pixels diff pass

--- example/storybook/playwright.config.ts
+++ example/storybook/playwright.config.ts
@@ -67,6 +67,21 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    // timeout: 5000,
+
+    // toHaveScreenshot: {
+    //   // An acceptable amount of pixels that could be different, unset by default.
+    //   maxDiffPixels: 10,
+    // },
+
+    toMatchSnapshot: {
+      // An acceptable ratio of pixels that are different to the
+      // total amount of pixels, between 0 and 1.
+      maxDiffPixelRatio: 0.011,
+    },
+  },
 
   /* Run your local dev server before starting the tests */
   // webServer: {
