d7880867a452b9c740bd37b31958117b3dfbc5f6
Sankalp Pandey <sankalp@geekyants.com>
2024-03-12 13:35:48 +0530

    chore: snapshot test and snapshot update file completed

--- example/storybook/tests/example.spec.js
+++ example/storybook/tests/example.spec.js
@@ -6,14 +6,16 @@ const testData = require('./data.json');
 const envs = process.env;
 
 for (const [key, value] of Object.entries(testData)) {
-  if (envs.hasOwnProperty(key) || envs.emptyArgs) {
+  if (envs.hasOwnProperty(key) || envs?.emptyArgs || envs?.multipleArgs) {
     const typeOfTest = envs[key];
     // total arguments for story
-    const args = value.arguments;
+    const args = value?.arguments;
+
     // flag whether overlay or not
     const overlay = value.overlay;
-    if (typeOfTest === 'multiple') {
+    if ((typeOfTest === 'multiple' || envs?.multipleArgs) && args) {
       // compound combination of all variants
+
       const keys = Object.keys(args);
       const combination = {};
       keys.forEach((variant) => {
@@ -35,7 +37,7 @@ for (const [key, value] of Object.entries(testData)) {
         )}`, async ({ page }, testinfo) => {
           await page.goto(storybookUrl);
           if (overlay) await page.waitForTimeout(300);
-          testinfo.snapshotSuffix = 'darwin';
+          testinfo.snapshotSuffix = '';
           expect(await page.screenshot()).toMatchSnapshot({
             maxDiffPixelRatio: 0.012,
           });
@@ -59,7 +61,7 @@ for (const [key, value] of Object.entries(testData)) {
         }
       }
     }
-    if (typeOfTest === 'single' || envs.emptyArgs) {
+    if ((typeOfTest === 'single' || envs?.emptyArgs) && args) {
       // each variant individually
       const keys = Object.keys(args);
       const combination = {};
@@ -88,7 +90,7 @@ for (const [key, value] of Object.entries(testData)) {
             )}`, async ({ page }, testinfo) => {
               await page.goto(storybookUrl);
               if (overlay) await page.waitForTimeout(300);
-              testinfo.snapshotSuffix = 'darwin';
+              testinfo.snapshotSuffix = '';
               expect(await page.screenshot()).toMatchSnapshot({
                 maxDiffPixelRatio: 0.012,
               });
@@ -109,63 +111,11 @@ for (const [key, value] of Object.entries(testData)) {
         );
         await page.goto(storybookUrl);
         if (overlay) await page.waitForTimeout(300);
-        testinfo.snapshotSuffix = 'darwin';
+        testinfo.snapshotSuffix = '';
         expect(await page.screenshot()).toMatchSnapshot({
           maxDiffPixelRatio: 0.011,
         });
       });
     }
-    // if (!args || Object.keys(args).length === 0) {
-    // test(`${key} is displayed`, async ({ page }) => {
-    //   const storybookUrl = path.join(
-    //     'file://',
-    //     __dirname,
-    //     `../storybook-static/iframe.html?args=&id=stories-${value.storyParent}-${value.storyAddress}--${value.storyName}&viewMode=story`
-    //   );
-    //   await page.goto(storybookUrl);
-    //   if (overlay) await page.waitForTimeout(300);
-    //   expect(await page.screenshot()).toMatchSnapshot();
-    // });
-    // } else {
-    // const keys = Object.keys(args);
-    // const combination = {};
-    // keys.forEach((variant) => {
-    //   combination[variant] = args[variant].options[0];
-    // });
-    // let done = false;
-    // while (!done) {
-    //   const storybookUrl = path.join(
-    //     'file://',
-    //     __dirname,
-    //     `../storybook-static/iframe.html?args=${JSON.stringify(combination)
-    //       .replace(/[{}"]/g, '')
-    //       .replace(/[,]/g, ';')}&id=stories-${value.storyParent}-${
-    //       value.storyAddress
-    //     }--${value.storyName}`
-    //   );
-    //   test(`${key} is displayed with arguments ${JSON.stringify(
-    //     combination
-    //   )}`, async ({ page }) => {
-    //     await page.goto(storybookUrl);
-    //     if (overlay) await page.waitForTimeout(300);
-    //     expect(await page.screenshot()).toMatchSnapshot();
-    //   });
-    //   let i = keys.length - 1;
-    //   while (i >= 0) {
-    //     const variant = keys[i];
-    //     const currentIndex = args[variant].options.indexOf(combination[variant]);
-    //     if (currentIndex < args[variant].options.length - 1) {
-    //       combination[variant] = args[variant].options[currentIndex + 1];
-    //       break;
-    //     } else {
-    //       combination[variant] = args[variant].options[0];
-    //       i--;
-    //     }
-    //     if (i < 0) {
-    //       done = true;
-    //     }
-    //   }
-    // }
-    // }
   }
 }
--- example/storybook/tests/scripts/snapshotUpdate.js
+++ example/storybook/tests/scripts/snapshotUpdate.js
@@ -1,20 +1,27 @@
-// // Import required modules
-// const { execSync } = require('child_process');
+// Import required modules
+const { execSync } = require('child_process');
 
-// // Get arguments
-// const args = process.argv.slice(2);
+// Get arguments
+const args = process.argv.slice(2);
 
-// // Loop through each argument
-// args.forEach((arg) => {
-//   // Extract component name
-//   const key = arg.replace(/^--/, '').split('=')[0];
+// Loop through each argument
+args.forEach((arg) => {
+  if (arg === '--multiple') {
+    process.env.multipleArgs = true;
+  }
+  // Extract component name
+  const key = arg.replace(/^--/, '').split('=')[0];
 
-//   // Extract testing type name
-//   const value = arg.includes('=') ? arg.split('=')[1] : 'default';
+  // Extract testing type name
+  const value = arg.includes('=') ? arg.split('=')[1] : 'default';
 
-//   // Set environment variables
-//   process.env[key] = value;
-// });
+  // Set environment variables
+  process.env[key] = value;
+});
 
-// // Run playwright test
-// execSync('yarn playwright test', { stdio: 'inherit' });
+if (args.length === 0) {
+  process.env.emptyArgs = true;
+}
+
+// Run playwright test
+execSync('yarn playwright test -u', { stdio: 'inherit' });
