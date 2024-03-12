a3bb47372efbc3d244cd1cc66997b4cb802004af
Suraj <suraj@Surajs-MacBook-Pro.local>
2024-03-12 11:34:18 +0530

    fix: app provider issue

--- example/storybook-nativewind/src/core-components/themed/Wrapper.tsx
+++ example/storybook-nativewind/src/core-components/themed/Wrapper.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box, Center } from '@gluestack-ui/themed';
 import { StyledProvider, useColorMode } from '@gluestack-style/react';
 import { createProvider } from '@gluestack-ui/provider';
-import { config } from './gluestack-ui-provider/config';
 
 const Provider = createProvider({ StyledProvider }) as any;
 Provider.displayName = 'Provider';
@@ -12,22 +11,16 @@ const Wrapper = ({ children, ...props }: any) => {
   return (
     // @ts-ignore
     <Box dataSet={{ 'theme-id': `withGluestackStyle_${colorMode}` }}>
-      <Provider
-        colorMode="light"
-        config={config}
-        style={{ flex: 1, height: '100%' }}
+      <Box
+        sx={{
+          _ios: {
+            h: '100%',
+          },
+        }}
+        {...props}
       >
-        <Box
-          sx={{
-            _ios: {
-              h: '100%',
-            },
-          }}
-          {...props}
-        >
-          <Center height="100%">{children}</Center>
-        </Box>
-      </Provider>
+        <Center height="100%">{children}</Center>
+      </Box>
     </Box>
   );
 };
