538183b0f38ce5783664e4d8932cd636b84631bf
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-03-18 13:18:18 +0530

    fix: radioIcon size

--- example/storybook-nativewind/src/core-components/nativewind/radio/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/radio/index.tsx
@@ -18,14 +18,11 @@ const groupStyle = tva({ base: 'gap-2' });
 const iconStyle = tva({
   base: 'rounded-full group-data-[checked=true]/radioInd:text-primary-600 group-data-[checked=true]/radioInd:group-data-[hover=true]/radioInd:text-primary-700 data-[checked=true]:data-[hover=true]:data-[disabled=true]:text-primary-600',
 
-  variants: {
+  parentVariants: {
     size: {
-      '2xs': 'h-3 w-3',
-      'xs': 'h-3.5 w-3.5',
-      'sm': 'h-4 w-4',
-      'md': 'h-4.5 w-4.5',
-      'lg': 'h-5 w-5',
-      'xl': 'h-6 w-6',
+      sm: 'h-3 w-3',
+      md: 'h-4 w-4',
+      lg: 'h-[18px] w-[18px]',
     },
   },
 });
