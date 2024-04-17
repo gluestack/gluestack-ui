d21f27018a874d1c9769028b8e10c066a3b460a7
Rohit Singh <rohit99.ind@gmail.com>
2024-04-17 17:37:48 +0530

    feat: rsc component fix layout issue

--- example/storybook-nativewind/src/core-components/nativewind/box/styles.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/box/styles.tsx
@@ -1,4 +1,4 @@
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
 export const boxStyle = tva({
-  base: 'web:flex web:flex-col web:relative web:z-0',
+  base: 'web:flex web:flex-col web:relative web:z-0 web:box-border web:border-0 web:list-none web:min-w-0 web:min-h-0 web:bg-transparent web:items-stretch web:m-0 web:p-0 web:text-decoration-none',
 });
--- example/storybook-nativewind/src/core-components/nativewind/vstack/styles.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/vstack/styles.tsx
@@ -1,6 +1,6 @@
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
 export const vstackStyle = tva({
-  base: 'flex-col web:flex web:relative web:z-0',
+  base: 'flex-col web:flex web:relative web:z-0 web:box-border web:border-0 web:list-none web:min-w-0 web:min-h-0 web:bg-transparent web:items-stretch web:m-0 web:p-0 web:text-decoration-none',
   variants: {
     space: {
       'xs': 'gap-1',
