c4230dd9452fa144adc25778a20ae208ec17b556
Damini <daminipandey1310@gmail.com>
2024-04-25 16:56:28 +0530

    feat: add styles

---
+++ example/storybook-nativewind/src/core-components/nativewind/table/styles.tsx
@@ -0,0 +1,29 @@
+import { tva } from '@gluestack-ui/nativewind-utils/tva';
+
+export const tableStyle = tva({
+  base: '',
+});
+
+export const tableHeader = tva({
+  base: '',
+});
+
+export const tableBody = tva({
+  base: '',
+});
+
+export const tableFooter = tva({
+  base: '',
+});
+
+export const tableRowStyle = tva({
+  base: '',
+});
+
+export const tableHead = tva({
+  base: '',
+});
+
+export const tableData = tva({
+  base: '',
+});
