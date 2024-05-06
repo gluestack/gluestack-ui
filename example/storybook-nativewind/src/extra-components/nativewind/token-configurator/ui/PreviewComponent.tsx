e8e4c316da0e499e269cf2e851c7775e3f69f35b
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-05-06 19:25:19 +0530

    fix: styling changes

--- example/storybook-nativewind/src/extra-components/nativewind/token-configurator/ui/PreviewComponent.tsx
+++ example/storybook-nativewind/src/extra-components/nativewind/token-configurator/ui/PreviewComponent.tsx
@@ -65,7 +65,7 @@ const PreviewComponent = () => {
   const extraAvatars = avatars.slice(3);
   const remainingCount = extraAvatars.length;
   return (
-    <div className="rounded-lg p-6 w-full border-2">
+    <div className="bg-white rounded-lg p-6 border-2">
       <SolidButtonGrid />
       <OutlineButtonGrid />
       <LinkButtonGrid />
