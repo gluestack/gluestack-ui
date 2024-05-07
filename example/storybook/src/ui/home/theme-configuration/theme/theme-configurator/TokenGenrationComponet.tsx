a7d9e4c1a96592c44bf2af978adcbcae8658defc
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-05-07 14:38:15 +0530

    fix: theme configurator styled

--- example/storybook/src/ui/home/theme-configuration/theme/theme-configurator/TokenGenrationComponet.tsx
+++ example/storybook/src/ui/home/theme-configuration/theme/theme-configurator/TokenGenrationComponet.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import TokenConfiguratorBaseComponent from '../../../../../../../storybook-nativewind/src/extra-components/nativewind/token-configurator/TokenConfigComponent';
+import TokenConfiguratorBaseComponent from '../../../../../../../storybook-nativewind/src/extra-components/nativewind/token-configurator/TokenConfigComponent';
 export const TokenGenrationComponet = () => {
-  return <h1>Sample text</h1>;
-  // return <TokenConfiguratorBaseComponent />;
+  return <TokenConfiguratorBaseComponent />;
 };
