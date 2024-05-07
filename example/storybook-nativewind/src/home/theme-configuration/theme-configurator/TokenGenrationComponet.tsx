1c6542c660575467463f3c61156e3e2d32272f66
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-05-07 17:17:23 +0530

    fix: renamed components

--- example/storybook-nativewind/src/home/theme-configuration/theme-configurator/TokenGenrationComponet.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ThemeConfigurator from '../../../extra-components/nativewind/token-configurator/TokenConfigComponent';
-
-export const ThemeConfiguratorComponent = () => {
-  return <ThemeConfigurator />;
-};
--- example/storybook/src/ui/home/theme-configuration/theme/theme-configurator/TokenGenrationComponet.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ThemeConfigurator from '../../../../../../../storybook-nativewind/src/extra-components/nativewind/token-configurator/TokenConfigComponent';
-// import TokenConfiguratorBaseComponent from '../../../../../../docs-components/nativewind/extra-components/nativewind/token-configurator/TokenConfigComponent';
-
-export const ThemeConfiguratorComponent = () => {
-  return <ThemeConfigurator />;
-};
