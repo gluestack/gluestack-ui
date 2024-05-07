7668d96f99d1decd2adfb411fe336f63b77dd660
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-05-07 19:12:36 +0530

    fix: renamed components

--- example/storybook/src/ui/home/theme-configuration/theme/theme-configurator/ThemeConfig.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ThemeConfigurator from '../../../../../../../storybook-nativewind/src/extra-components/nativewind/token-configurator/TokenConfigComponent';
-
-export default function ThemeConfiguratorComponent() {
-  return <ThemeConfigurator />;
-}
