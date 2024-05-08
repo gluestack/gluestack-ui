93607e87bc8a3e472b6c41ef770fbaccd3cad4b2
Akash Gautam <akashgautam@geekyants.com>
2024-05-08 15:56:53 +0530

    fix: post install command

--- packages/nativewind/utils/scripts/post-install-script.js
+++ packages/nativewind/utils/scripts/post-install-script.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 var finder = require('./find-package-json');
-const { spawnSync } = require('child_process');
+const { exec } = require('child_process');
 const findWorkspaceRoot = require('find-yarn-workspace-root');
 
 const processPath = process.cwd();
 const workspaceRoot = findWorkspaceRoot(processPath);
 const f = finder(path.join(processPath, '..'));
-const userDirectory = f.next().filename.replace('package.json', '');
+const filename = f.next().filename;
+const userDirectory = filename.replace('package.json', '');
+
 function CopyDirectory(src, dest) {
   if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest);
   }
 
-  let entries = fs.readdirSync(src, { withFileTypes: true });
+  const entries = fs.readdirSync(src, { withFileTypes: true });
 
-  for (let entry of entries) {
-    let srcPath = path.join(src, entry.name);
-    let destPath = path.join(dest, entry.name);
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
 
     if (entry.isDirectory()) {
       CopyDirectory(srcPath, destPath);
@@ -27,41 +29,33 @@ function CopyDirectory(src, dest) {
   }
 }
 
-function main() {
-  CopyDirectory(
-    path.join(processPath, 'scripts', 'patches'),
-    path.join(userDirectory, 'patches')
-  );
+function installPatch(cwd) {
   // use npm if user is using npm or yarn if user is using yarn
-  try {
-    const packageManager = fs.existsSync(path.join(userDirectory, 'yarn.lock'))
-      ? 'yarn'
-      : 'npm';
+  const packageManager = fs.existsSync(path.join(cwd, 'yarn.lock'))
+    ? 'yarn'
+    : 'npx';
 
-    spawnSync(packageManager, ['patch-package'], {
-      cwd: userDirectory,
-      stdio: 'inherit',
-    });
-  } catch (error) {}
+  const command = packageManager + ' patch-package';
+  exec(command, {
+    cwd: cwd,
+    stdio: 'inherit',
+  });
+}
 
+function main() {
   if (workspaceRoot && workspaceRoot !== userDirectory) {
     CopyDirectory(
       path.join(processPath, 'scripts', 'patches'),
       path.join(workspaceRoot, 'patches')
     );
 
-    try {
-      const packageManager = fs.existsSync(
-        path.join(workspaceRoot, 'yarn.lock')
-      )
-        ? 'yarn'
-        : 'npm';
-
-      spawnSync(packageManager, ['patch-package'], {
-        cwd: workspaceRoot,
-        stdio: 'inherit',
-      });
-    } catch (error) {}
+    installPatch(workspaceRoot);
+  } else {
+    CopyDirectory(
+      path.join(processPath, 'scripts', 'patches'),
+      path.join(userDirectory, 'patches')
+    );
+    installPatch(userDirectory);
   }
 }
 
