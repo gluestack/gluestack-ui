420639ec1d0d7aec2c97aa6d486677684e9fb4ba
Damini <daminipandey1310@gmail.com>
2024-05-14 14:36:36 +0530

    feat: add qr code

--- example/storybook-nativewind/src/extra-components/nativewind/Responsive.tsx
+++ example/storybook-nativewind/src/extra-components/nativewind/Responsive.tsx
@@ -1,10 +1,20 @@
-import { Box, Text } from '@gluestack/design-system';
+import { Box, Text, Pressable, OpenInNewIcon } from '@gluestack/design-system';
 import React, { useRef, useState } from 'react';
 import NextImage from 'next/image';
 import { Expand } from '../../apps/dashboard-app/Expand';
 import { Resizable } from 're-resizable';
-// import { GluestackUIProvider } from "@gluestack-ui/themed"
-// import { OpenInNewIcon } from "@gluestack/design-system";
+import kitchensink from './kitchensink';
+
+//function to detect if the user is on a web browser
+function checkPlatform() {
+  if (/android/i.test(navigator.userAgent)) {
+    window.location.href = `exp://u.expo.dev/update/${kitchensink.updateIds.android}`;
+  } else if (/iPad|iPhone|iPod/i.test(navigator.userAgent)) {
+    window.location.href = `exp://u.expo.dev/update/${kitchensink.updateIds.ios}`;
+  } else if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+    window.open('https://ui-example-nativewind.vercel.app/');
+  }
+}
 
 function Responsiveness() {
   const resizableRef = useRef<any>(null);
@@ -138,7 +148,7 @@ function Responsiveness() {
 
         <Box h="100%" w="100%" overflow="hidden">
           <iframe
-            src="https://ui-kitchensink.gluestack.io/"
+            src="https://ui-example-nativewind.vercel.app/"
             title="NativeBase v3 Dashboard Example"
             style={{
               transformOrigin: '0px 0px',
@@ -152,6 +162,239 @@ function Responsiveness() {
           />
         </Box>
       </Resizable>
+
+      <Box
+        mt="-$12"
+        borderRadius="$lg"
+        borderWidth={1}
+        bg="$backgroundDark950"
+        borderColor="$borderDark800"
+        p="$4"
+        width="auto"
+        alignSelf="flex-start"
+        sx={{
+          '@md': {
+            flexDirection: 'row',
+          },
+        }}
+      >
+        <Box>
+          <Text
+            color="$textDark50"
+            fontSize="$xl"
+            fontWeight="$bold"
+            fontFamily="Inter"
+          >
+            Give it a shot!
+          </Text>
+
+          <Box
+            display="none"
+            sx={{
+              '@md': {
+                display: 'flex',
+              },
+            }}
+          >
+            <Text
+              color="$textDark300"
+              fontSize="$md"
+              fontWeight="$normal"
+              lineHeight="$lg"
+              fontFamily="Inter"
+              mt="$2"
+              sx={{
+                '@md': {
+                  maxWidth: 319,
+                },
+              }}
+            >
+              Try it in your browser or scan the QR code with the&nbsp;
+              <NextImage
+                src="/icon/expo-icon.svg"
+                alt="expo-icon"
+                width={18}
+                height={24}
+                style={{
+                  verticalAlign: 'middle',
+                }}
+              />
+              &nbsp;Expo app on your phone.
+            </Text>
+          </Box>
+
+          <Text
+            display="flex"
+            color="$textDark300"
+            fontSize="$md"
+            fontWeight="$normal"
+            lineHeight="$lg"
+            fontFamily="Inter"
+            mt="$2"
+            sx={{
+              '@md': {
+                display: 'none',
+                maxWidth: 319,
+              },
+            }}
+          >
+            Try on Expo app. It&apos;s the perfect way to dive right in and
+            explore.
+          </Text>
+
+          <Pressable
+            display="none"
+            py="$1"
+            px="$3"
+            mt="$5"
+            borderColor="$borderDark700"
+            borderRadius="$sm"
+            borderWidth={1}
+            bg="transparent"
+            flexDirection="row"
+            alignItems="center"
+            width="auto"
+            alignSelf="flex-start"
+            sx={{
+              '_web': {
+                ':focus': {
+                  boxShadow: '#004282 0px 0px 0px 2px',
+                  _dark: {
+                    boxShadow: '#004282 0px 0px 0px 2px',
+                  },
+                },
+              },
+              '@md': {
+                display: 'flex',
+              },
+            }}
+            onPress={checkPlatform}
+          >
+            <OpenInNewIcon />
+            <Text
+              color="$textLight50"
+              ml="$2.5"
+              fontSize="$sm"
+              fontWeight="$normal"
+              lineHeight="$md"
+            >
+              Open in new tab
+            </Text>
+          </Pressable>
+
+          <Pressable
+            display="flex"
+            py="$1"
+            px="$3"
+            mt="$5"
+            borderColor="$borderDark700"
+            borderRadius="$sm"
+            borderWidth={1}
+            bg="transparent"
+            flexDirection="row"
+            alignItems="center"
+            width="auto"
+            alignSelf="flex-start"
+            sx={{
+              '_web': {
+                ':focus': {
+                  boxShadow: '#004282 0px 0px 0px 2px',
+                  _dark: {
+                    boxShadow: '#004282 0px 0px 0px 2px',
+                  },
+                },
+              },
+              '@md': {
+                display: 'none',
+              },
+            }}
+            onPress={checkPlatform}
+          >
+            <NextImage
+              src="/icon/expo-icon.svg"
+              alt="expo-icon"
+              width={17}
+              height={16}
+            />
+            <Text
+              color="$textLight50"
+              ml="$2.5"
+              fontSize="$sm"
+              fontWeight="$normal"
+              lineHeight="$md"
+            >
+              Open in Expo
+            </Text>
+          </Pressable>
+        </Box>
+
+        <Box
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="row"
+          ml="$10"
+          borderLeftWidth={1}
+          borderColor="$borderDark700"
+          my="$6"
+          display="none"
+          sx={{
+            '@md': {
+              display: 'flex',
+            },
+          }}
+        >
+          <Box px="$5" justifyContent="center" alignItems="center">
+            <NextImage
+              alt="ios-qr-code"
+              width={84}
+              height={84}
+              src={`https://qr.expo.dev/eas-update?updateId=${kitchensink.updateIds.ios}&appScheme=exp&host=u.expo.dev`}
+            />
+            <Box flexDirection="row" alignItems="center" mt="$2">
+              <NextImage
+                alt="ios-icon"
+                src="/icon/ios-icon.svg"
+                width={16}
+                height={16}
+              />
+              <Text
+                ml="$1"
+                color="$textLight200"
+                fontSize="$sm"
+                fontWeight="$normal"
+                lineHeight="$sm"
+              >
+                iOS
+              </Text>
+            </Box>
+          </Box>
+          <Box justifyContent="center" alignItems="center">
+            <NextImage
+              alt="android-qr-code"
+              width={84}
+              height={84}
+              src={`https://qr.expo.dev/eas-update?updateId=${kitchensink.updateIds.android}&appScheme=exp&host=u.expo.dev`}
+            />
+            <Box flexDirection="row" alignItems="center" mt="$2">
+              <NextImage
+                alt="ios-icon"
+                src="/icon/android-icon.svg"
+                width={16}
+                height={16}
+              />
+              <Text
+                ml="$1"
+                color="$textLight200"
+                fontSize="$sm"
+                fontWeight="$normal"
+                lineHeight="$sm"
+              >
+                Android
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 }
---
+++ example/storybook-nativewind/src/extra-components/nativewind/kitchensink.tsx
@@ -0,0 +1,8 @@
+const kitchensink = {
+  updateIds: {
+    android: 'd315f92b-af4a-47dc-bbd1-e1b3db4ae7fe',
+    ios: '6147de3d-d492-4b4e-807e-1add3e1600be',
+  },
+};
+
+export default kitchensink;
