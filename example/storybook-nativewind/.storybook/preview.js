b6cae84ed15cb3d85e521644f3f901a86a40bbf0
Damini <daminipandey1310@gmail.com>
2024-05-15 11:19:09 +0530

    fix: change app icon

--- example/storybook-nativewind/.storybook/preview.js
+++ example/storybook-nativewind/.storybook/preview.js
@@ -92,7 +92,7 @@ export const parameters = {
         },
         {
           source: 'lucide-react-native',
-          name: 'LayoutList',
+          name: 'Layers',
           headerTitle: 'Apps',
         },
         {
