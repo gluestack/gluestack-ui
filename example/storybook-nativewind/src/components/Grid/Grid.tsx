dbf66e035f1711d90997e9b714eb5141919f3842
Damini <daminipandey1310@gmail.com>
2024-05-22 15:38:53 +0530

    fix: table and grid native stories

--- example/storybook-nativewind/src/components/Grid/Grid.tsx
+++ example/storybook-nativewind/src/components/Grid/Grid.tsx
@@ -1,34 +1,14 @@
 import { Grid, GridItem } from '@/components/ui/grid';
 import React from 'react';
-import { Text } from 'react-native';
 
 const GridBasic = () => {
   return (
-    <Grid numColumns={8} className="gap-x-5 gap-y-5 p-4 flex-col">
-      <GridItem colSpan={2} className="bg-pink-500 p-10">
-        <Text>2</Text>
-      </GridItem>
-      <GridItem colSpan={4} className="bg-pink-500">
-        <Text>4</Text>
-      </GridItem>
-      <GridItem colSpan={2} className="bg-pink-500">
-        <Text>2</Text>
-      </GridItem>
-
-      <GridItem colSpan={8} className="bg-pink-500">
-        <Text>8</Text>
-      </GridItem>
-
-      <GridItem colSpan={8} className="bg-pink-500">
-        <Text>12</Text>
-      </GridItem>
-
-      <GridItem colSpan={2} className="bg-pink-500">
-        <Text>2</Text>
-      </GridItem>
-      <GridItem colSpan={6} className="bg-pink-500">
-        <Text>6</Text>
-      </GridItem>
+    <Grid numColumns={8} className="gap-5">
+      <GridItem colSpan={3} className="bg-background-200 p-6 rounded-md" />
+      <GridItem colSpan={5} className="bg-background-200 p-6 rounded-md" />
+      <GridItem colSpan={6} className="bg-background-200 p-6 rounded-md" />
+      <GridItem colSpan={4} className="bg-background-200 p-6 rounded-md" />
+      <GridItem colSpan={4} className="bg-background-200 p-6 rounded-md" />
     </Grid>
   );
 };
