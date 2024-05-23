9bd1ecbf7f9a7936e36232364179ce243a1a2282
Viraj Ajay Joshi <virajajayjoshi@Virajs-MacBook-Pro.local>
2024-05-23 17:58:58 +0530

    fix: new

--- example/storybook-nativewind/src/components/Accordion/Accordion.stories.tsx
+++ example/storybook-nativewind/src/components/Accordion/Accordion.stories.tsx
@@ -1,9 +1,16 @@
 import type { ComponentMeta } from '@storybook/react-native';
 import Accordion from './Accordion';
+// import AccordionCustomisedBackground from './AccordionCustomisedBackground';
+// import AccordionRoundedCorners from './AccordionRoundedCorner';
+// import AccordionCustomisedComponent from './AccordionCustomisedComponent';
+// import AccordionNested from './AccordionNested';
 
 const AccordionMeta: ComponentMeta<typeof Accordion> = {
   title: 'stories/Accordion',
   component: Accordion,
+  metaInfo: {
+    componentDescription: `The Actionsheet component presents a set of options to the user, overlaid on top of the app's content, allowing them to take quick actions without leaving the current page or view.`,
+  },
   argTypes: {
     size: {
       control: 'select',
@@ -26,4 +33,10 @@ const AccordionMeta: ComponentMeta<typeof Accordion> = {
 
 export default AccordionMeta;
 
-export { Accordion };
+export {
+  Accordion,
+  // AccordionCustomisedBackground,
+  // AccordionRoundedCorners,
+  // AccordionCustomisedComponent,
+  // AccordionNested,
+};
