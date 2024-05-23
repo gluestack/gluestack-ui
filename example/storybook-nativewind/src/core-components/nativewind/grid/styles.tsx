ff8446fe6c9447a95a2a3310e6ae454b6d3cbe12
Damini <daminipandey1310@gmail.com>
2024-05-23 09:59:06 +0530

    fix: grid styles

--- example/storybook-nativewind/src/core-components/nativewind/grid/styles.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/grid/styles.tsx
@@ -3,13 +3,13 @@ import { isWeb } from '@gluestack-ui/nativewind-utils/IsWeb';
 
 const gridBaseStyle = isWeb
   ? 'grid grid-cols-12'
-  : 'w-full box-border flex-row flex-wrap justify-start';
-const gridItemBaseStyle = isWeb ? 'w-auto col-span-1' : 'w-full';
+  : 'box-border flex-row flex-wrap justify-start';
+const gridItemBaseStyle = isWeb ? 'w-auto col-span-1' : '';
 
 export const gridStyle = tva({
-  base: gridBaseStyle,
+  base: `w-full ${gridBaseStyle}`,
 });
 
 export const gridItemStyle = tva({
-  base: gridItemBaseStyle,
+  base: `w-full ${gridItemBaseStyle}`,
 });
