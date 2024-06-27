0300b0a0fe8786239f516630959bd831a580de61
viraj-10 <virajjoshi132@gmail.com>
2024-06-27 12:06:44 +0530

    chore: nativewind utils version bump 1.0.21

--- packages/nativewind/utils/useBreakpointValue/index.d.ts
+++ packages/nativewind/utils/useBreakpointValue/index.d.ts
@@ -1,4 +1,6 @@
-type BreakPointValue = Partial<{}>;
+declare const screenSize: Record<'sm' | 'md' | 'lg' | 'xl' | '2xl', string>;
+type breakpoints = keyof typeof screenSize | 'default';
+type BreakPointValue = Partial<Record<breakpoints, any>>;
 export declare const getBreakPointValue: (values: any, width: any) => any;
 export declare function useBreakpointValue(values: BreakPointValue): null;
 export declare function isValidBreakpoint(
