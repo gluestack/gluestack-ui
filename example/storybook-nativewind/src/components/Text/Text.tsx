52017e0b608553ce31c33d6afa7d753c04ec0baa
viraj-10 <virajjoshi132@gmail.com>
2024-06-28 11:50:59 +0530

    fix: text snippet

--- example/storybook-nativewind/src/components/Text/Text.tsx
+++ example/storybook-nativewind/src/components/Text/Text.tsx
@@ -1,33 +1,8 @@
 import React from 'react';
 import { Text } from '@/components/ui/text';
 
-const TextBasic = ({
-  size = 'md',
-  text = 'Hello world',
-  fontWeight = 'bold',
-  ...props
-}: any) => {
-  const fontWeights = {
-    hairline: 'font-hairline',
-    thin: 'font-thin',
-    light: 'font-light',
-    normal: 'font-normal',
-    medium: 'font-medium',
-    semibold: 'font-semibold',
-    bold: 'font-bold',
-    extrabold: 'font-extrabold',
-    black: 'font-black',
-  };
-
-  return (
-    <Text
-      size={size}
-      {...props}
-      className={`${fontWeights[fontWeight as keyof typeof fontWeights]}`}
-    >
-      {text}
-    </Text>
-  );
+const TextBasic = ({}: any) => {
+  return <Text>Hello</Text>;
 };
 
 TextBasic.description =
