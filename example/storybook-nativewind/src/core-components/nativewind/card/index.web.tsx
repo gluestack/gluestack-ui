ccb53e87f3e8e829f8b77dda426571aea6c510b2
viraj-10 <virajjoshi132@gmail.com>
2024-07-25 12:13:01 +0530

    fix: card typing

--- example/storybook-nativewind/src/core-components/nativewind/card/index.web.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/card/index.web.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { cardStyle } from './styles';
+import { VariantProps } from '@gluestack-ui/nativewind-utils';
+import { ViewProps } from 'react-native';
 
-const Card = ({
-  className,
-  size = 'md',
-  variant = 'elevated',
-  ...props
-}: any) => {
-  return (
-    <div
-      className={cardStyle({ size, variant, class: className })}
-      {...props}
-    />
-  );
+type Similar<T, U> = {
+  [K in keyof T & keyof U]: T[K] extends U[K]
+    ? U[K] extends T[K]
+      ? T[K]
+      : never
+    : never;
 };
+
+type ICardProps = Similar<ViewProps, React.ComponentPropsWithoutRef<'div'>> &
+  VariantProps<typeof cardStyle>;
+
+const Card = React.forwardRef<HTMLDivElement, ICardProps>(
+  ({ className, size = 'md', variant = 'elevated', ...props }, ref) => {
+    return (
+      <div
+        className={cardStyle({ size, variant, class: className })}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
 
 Card.displayName = 'Card';
 
