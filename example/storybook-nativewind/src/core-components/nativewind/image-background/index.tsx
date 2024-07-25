fa4a4827c683940f496d06a91ef66ce878c86056
ankit-tailor <tailorankit3345@gmail.com>
2024-07-25 17:14:36 +0530

    fix: imagebackground typings

--- example/storybook-nativewind/src/core-components/nativewind/image-background/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/image-background/index.tsx
@@ -2,14 +2,20 @@
 import { ImageBackground as RNImageBackground } from 'react-native';
 import React from 'react';
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
-export const ImageBackground = React.forwardRef(
-  ({ className, ...props }: any, ref?: any) => {
-    return (
-      <RNImageBackground
-        className={tva({ base: className })}
-        {...props}
-        ref={ref}
-      />
-    );
-  }
-);
+
+const imageBackgroundStyle = tva({});
+
+export const ImageBackground = React.forwardRef<
+  React.ElementRef<typeof RNImageBackground>,
+  React.ComponentProps<typeof RNImageBackground>
+>(({ className, ...props }, ref) => {
+  return (
+    <RNImageBackground
+      className={imageBackgroundStyle({
+        class: className,
+      })}
+      {...props}
+      ref={ref}
+    />
+  );
+});
