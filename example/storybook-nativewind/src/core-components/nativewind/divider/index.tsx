3408dfd5b28e79916ba48e4d741d5be4ebd9b22f
viraj-10 <virajjoshi132@gmail.com>
2024-07-25 14:20:52 +0530

    fix: divider typing

--- example/storybook-nativewind/src/core-components/nativewind/divider/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/divider/index.tsx
@@ -1,10 +1,11 @@
 'use client';
+import React from 'react';
 import { createDivider } from '@gluestack-ui/divider';
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
 import { View } from 'react-native';
 import { cssInterop } from 'nativewind';
+import type { VariantProps } from '@gluestack-ui/nativewind-utils';
 
-import React from 'react';
 const dividerStyle = tva({
   base: 'bg-background-200',
   variants: {
@@ -19,20 +20,24 @@ const UIDivider = createDivider({ Root: View });
 
 cssInterop(UIDivider, { className: 'style' });
 
-const Divider = React.forwardRef(
-  ({ className, orientation = 'horizontal', ...props }: any, ref?: any) => {
-    return (
-      <UIDivider
-        ref={ref}
-        {...props}
-        className={dividerStyle({
-          orientation,
-          class: className,
-        })}
-      />
-    );
-  }
-);
+type IUIDividerProps = React.ComponentPropsWithoutRef<typeof UIDivider> &
+  VariantProps<typeof dividerStyle>;
+
+const Divider = React.forwardRef<
+  React.ElementRef<typeof UIDivider>,
+  IUIDividerProps
+>(({ className, orientation = 'horizontal', ...props }, ref) => {
+  return (
+    <UIDivider
+      ref={ref}
+      {...props}
+      className={dividerStyle({
+        orientation,
+        class: className,
+      })}
+    />
+  );
+});
 
 Divider.displayName = 'Divider';
 
