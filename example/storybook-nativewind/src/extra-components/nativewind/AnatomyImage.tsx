06af03773393dcc99fee274388400a3f45ee5990
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-08-09 13:55:23 +0530

    fix: changed image links

--- example/storybook-nativewind/src/extra-components/nativewind/AnatomyImage.tsx
+++ example/storybook-nativewind/src/extra-components/nativewind/AnatomyImage.tsx
@@ -14,6 +14,7 @@ const AnatomyImage = ({
         source={{ uri: mobileUrl }}
         alt="anatomy-image"
         className="md:hidden w-full aspect-[513/375] mb-5"
+        //@ts-ignore
         size="none"
       />
 
@@ -21,6 +22,7 @@ const AnatomyImage = ({
         source={{ uri: webUrl }}
         alt="anatomy-image"
         className="hidden md:flex w-full aspect-[736/259] mb-6"
+        //@ts-ignore
         size="none"
       />
     </>
