14381870cce1a473db910814fdc09c7b6a8d5932
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-08-09 12:33:10 +0530

    fix: anatomyImage issue of aspectRatio

--- example/storybook-nativewind/src/extra-components/nativewind/AnatomyImage.tsx
+++ example/storybook-nativewind/src/extra-components/nativewind/AnatomyImage.tsx
@@ -1,36 +1,5 @@
-// import React from 'react';
-// import { Image } from '../../core-components/nativewind';
-
-// const AnatomyImage = ({
-//   mobileUrl,
-//   webUrl,
-// }: {
-//   mobileUrl: string;
-//   webUrl: string;
-// }) => {
-//   return (
-//     <>
-//       <Image
-//         source={{ uri: mobileUrl }}
-//         alt="anatomy-image"
-//         className="md:hidden aspect-[513/375]"
-//         size="full"
-//       />
-
-//       <Image
-//         source={{ uri: webUrl }}
-//         alt="anatomy-image"
-//         className="hidden md:flex aspect-[736/259]"
-//         size="full"
-//       />
-//     </>
-//   );
-// };
-// export default AnatomyImage;
-
 import React from 'react';
-import { Center } from '../../core-components/nativewind';
-import Image from 'next/image';
+import { Image } from '../../core-components/nativewind';
 
 const AnatomyImage = ({
   mobileUrl,
@@ -40,11 +9,21 @@ const AnatomyImage = ({
   webUrl: string;
 }) => {
   return (
-    <Center className={`w-full aspect-[513/375] md:aspect-[736/259] mb-6`}>
-      <Image src={mobileUrl} alt="anatomy-image" fill className="md:hidden" />
+    <>
+      <Image
+        source={{ uri: mobileUrl }}
+        alt="anatomy-image"
+        className="md:hidden w-full aspect-[513/375]"
+        size="none"
+      />
 
-      <Image src={webUrl} alt="anatomy-image" fill className="hidden md:flex" />
-    </Center>
+      <Image
+        source={{ uri: webUrl }}
+        alt="anatomy-image"
+        className="hidden md:flex w-full aspect-[736/259]"
+        size="none"
+      />
+    </>
   );
 };
 export default AnatomyImage;
