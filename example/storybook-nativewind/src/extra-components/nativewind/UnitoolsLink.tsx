447a9b63f2f02e897cea6b5cfe4a4d66b064ddaf
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-09-09 13:45:42 +0530

    feat: added unitools/link

---
+++ example/storybook-nativewind/src/extra-components/nativewind/UnitoolsLink.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link, Text, Center } from '../../core-components/nativewind';
+
+const UnitoolsLink = () => {
+  return (
+    <Center>
+      <Link href="https://gluestack.io/">
+        <Text>gluestack</Text>
+      </Link>
+    </Center>
+  );
+};
+export default UnitoolsLink;
