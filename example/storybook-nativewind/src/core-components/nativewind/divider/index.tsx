1c1f896a4bf0f2f1eba74c1f2e9a8a0da9a8c032
viraj-10 <virajjoshi132@gmail.com>
2024-10-17 16:46:22 +0530

    fix: switch and divider

--- example/storybook-nativewind/src/core-components/nativewind/divider/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/divider/index.tsx
@@ -1,9 +1,7 @@
 'use client';
 import React from 'react';
-import { createDivider } from '@gluestack-ui/divider';
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
-import { View } from 'react-native';
-import { cssInterop } from 'nativewind';
+import { Platform, View } from 'react-native';
 import type { VariantProps } from '@gluestack-ui/nativewind-utils';
 
 const dividerStyle = tva({
@@ -16,21 +14,19 @@ const dividerStyle = tva({
   },
 });
 
-const UIDivider = createDivider({ Root: View });
-
-cssInterop(UIDivider, { className: 'style' });
-
-type IUIDividerProps = React.ComponentPropsWithoutRef<typeof UIDivider> &
+type IUIDividerProps = React.ComponentPropsWithoutRef<typeof View> &
   VariantProps<typeof dividerStyle>;
 
 const Divider = React.forwardRef<
-  React.ElementRef<typeof UIDivider>,
+  React.ElementRef<typeof View>,
   IUIDividerProps
 >(({ className, orientation = 'horizontal', ...props }, ref) => {
   return (
-    <UIDivider
+    <View
       ref={ref}
       {...props}
+      aria-orientation={orientation}
+      role={Platform.OS === 'web' ? 'separator' : undefined}
       className={dividerStyle({
         orientation,
         class: className,
