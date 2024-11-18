ea896bc2ecc1357dc9b2f1cef937bc1f9f00394c
viraj-10 <virajjoshi132@gmail.com>
2024-11-18 18:37:52 +0530

    fix: jsx issue for with context

--- packages/nativewind/utils/context/index.jsx
+++ packages/nativewind/utils/context/index.jsx
@@ -2,5 +2,5 @@
 import { createContext, useContext } from 'react';
 export const ParentContext = createContext({});
 export const useParentContext = () => {
-    return useContext(ParentContext);
+  return useContext(ParentContext);
 };
