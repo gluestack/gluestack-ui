74d6c85e38fb95c174261d3302837141dfd60f79
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-12-03 16:56:14 +0530

    fix: docs examples images

--- example/storybook-nativewind/src/core-components/nativewind/accordion/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/accordion/index.tsx
@@ -76,7 +76,7 @@ const accordionHeaderStyle = tva({
   base: 'mx-0 my-0',
 });
 const accordionContentStyle = tva({
-  base: 'mt-4',
+  base: 'pt-1 pb-3 px-4',
 });
 const accordionTriggerStyle = tva({
   base: 'w-full flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50 py-3 px-4',
