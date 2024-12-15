d109930f9cded0ed857aa8e9649226fc0738f235
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-12-15 23:40:49 +0530

    feat: updated qr-code of kitchensink-app

--- example/storybook-nativewind/src/extra-components/nativewind/appConfig.tsx
+++ example/storybook-nativewind/src/extra-components/nativewind/appConfig.tsx
@@ -9,12 +9,12 @@ export const dashboard = {
 };
 export const kitchensink = {
   updateIds: {
-    android: '9b4fde60-22d0-472f-bb40-7616835fef62',
-    ios: 'df0d208f-4348-4989-a152-1217a1d05fed',
+    android: '173a81b5-77b8-4166-b126-71ccd26649eb',
+    ios: '1e6837aa-cfb9-4629-9bcf-7d433a175234',
   },
   iframeUri: 'https://kitchen-sink-v2.gluestack.io/',
   qrCodeUri:
-    'https://qr.expo.dev/eas-update?slug=exp&projectId=8d16a046-45ac-43e4-b730-33c7a0d6d1ea&groupId=b8a50696-388f-4287-ad95-43e83c011c91&host=u.expo.dev',
+    'https://qr.expo.dev/eas-update?slug=exp&projectId=8d16a046-45ac-43e4-b730-33c7a0d6d1ea&groupId=bcabb108-f0d4-4d8d-8b48-a288a2432364&host=u.expo.dev',
 };
 export const todoapp = {
   updateIds: {
