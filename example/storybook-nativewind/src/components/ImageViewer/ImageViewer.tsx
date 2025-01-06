b2cbb7c0f6ac03b3f5f0a1d5b0b7a5cc854d609d
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-01-06 19:41:05 +0530

    chore: removed 'use client'

--- example/storybook-nativewind/src/components/ImageViewer/ImageViewer.tsx
+++ example/storybook-nativewind/src/components/ImageViewer/ImageViewer.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useState } from 'react';
 import { Image, Pressable } from 'react-native';
 import {
--- example/storybook-nativewind/src/core-components/nativewind/image-viewer/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/image-viewer/index.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React from 'react';
 import { createImageViewer } from '@gluestack-ui/image-viewer';
 import { Image, Modal, Pressable } from 'react-native';
