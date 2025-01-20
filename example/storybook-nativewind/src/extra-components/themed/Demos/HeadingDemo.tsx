86da0820c239c9c23bfd31d32cf09b1d0e713279
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-01-20 19:10:51 +0530

    test: removed components for testing script

--- example/storybook-nativewind/src/extra-components/themed/Demos/HeadingDemo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Heading } from '../../../core-components/themed';
-const HeadingDemo = () => {
-  return <Heading>I am a Heading</Heading>;
-};
-
-export default HeadingDemo;
