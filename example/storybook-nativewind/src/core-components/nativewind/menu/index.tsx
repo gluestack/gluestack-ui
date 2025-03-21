94ce32f31c7eb93b4c144061ba52732b9f688e71
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-03-21 13:33:20 +0530

    fix: fixed animation for menu

--- example/storybook-nativewind/src/core-components/nativewind/menu/index.tsx
+++ example/storybook-nativewind/src/core-components/nativewind/menu/index.tsx
@@ -135,15 +135,12 @@ const Menu = React.forwardRef<React.ElementRef<typeof UIMenu>, IMenuProps>(
         ref={ref}
         initial={{
           opacity: 0,
-          scale: 0.8,
         }}
         animate={{
           opacity: 1,
-          scale: 1,
         }}
         exit={{
           opacity: 0,
-          scale: 0.8,
         }}
         transition={{
           type: 'timing',
