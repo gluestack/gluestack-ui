0f8e61c1523b9b4ea67390ddd1bba31b3f35a6fe
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-04-30 18:41:45 +0530

    feat: added forms controll component

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,15 +1,35 @@
 "use client";
 import { Text, View } from "react-native";
 import { Button, ButtonText, ButtonSpinner } from "../components/Button";
-import { CheckIcon } from "../components/Icon";
+import { CheckIcon, AlertCircleIcon } from "../components/Icon";
 import { Input, InputField, InputIcon, InputSlot } from "../components/Input";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  FormControlErrorIcon,
+  FormControlLabel,
+  FormControlLabelText,
+  FormControlHelper,
+  FormControlHelperText,
+} from "../components/FormControl";
 import {
   Checkbox,
   CheckboxIndicator,
   CheckboxLabel,
   CheckboxIcon,
 } from "../components/Checkbox";
+import { useState } from "react";
 export default function Home() {
+  const [isInvalid, setIsInvalid] = useState(false)
+  const [inputValue, setInputValue] = useState("12345")
+  const handleSubmit = () => {
+    if (inputValue.length < 6) {
+      setIsInvalid(true)
+    } else {
+      setIsInvalid(false)
+    }
+  }
   return (
     <View className="flex-1 items-center justify-center bg-white w-full h-full">
       <Text className="text-red-500 text-2xl font-bold">Hello World</Text>
@@ -35,6 +55,39 @@ export default function Home() {
           <InputIcon as={CheckIcon} />
         </InputSlot>
       </Input>
+      <FormControl
+        isInvalid={isInvalid}
+        size="md"
+        isDisabled={false}
+        isReadOnly={false}
+        isRequired={false}
+      >
+        <FormControlLabel>
+          <FormControlLabelText>Password</FormControlLabelText>
+        </FormControlLabel>
+        <Input className="my-1" size="lg">
+          <InputField
+            type="password"
+            placeholder="password"
+            value={inputValue}
+            onChangeText={(text: string) => setInputValue(text)}
+          />
+        </Input>
+        <FormControlHelper>
+          <FormControlHelperText>
+            Must be atleast 6 characters.
+          </FormControlHelperText>
+        </FormControlHelper>
+        <FormControlError>
+          <FormControlErrorIcon as={AlertCircleIcon} />
+          <FormControlErrorText>
+            Atleast 6 characters are required.
+          </FormControlErrorText>
+        </FormControlError>
+      </FormControl>
+      <Button className="w-fit self-end mt-4" size="sm" onPress={handleSubmit}>
+        <ButtonText>Submit</ButtonText>
+      </Button>
     </View>
   );
 }
