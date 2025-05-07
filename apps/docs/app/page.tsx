d728d9cc2b9cc1052d0569b6920b7f9adf3643c8
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-05-07 13:20:17 +0530

    feat: migrated modal component

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,42 +1,72 @@
 "use client";
 
-import { Actionsheet, ActionsheetContent, ActionsheetItem, ActionsheetItemText, ActionsheetDragIndicator, ActionsheetDragIndicatorWrapper, ActionsheetBackdrop } from "../components/ui/ActionSheet";
-import { Button, ButtonText } from "../components/ui/Button";
+import { Button, ButtonText } from "@/components/ui/button";
+import { View, Text } from "react-native";
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@/components/ui/modal";
 
+import { Icon, CloseIcon } from "@/components/ui/icon";
 import React from "react";
-	
 
-export default function App(){
-        const [showActionsheet, setShowActionsheet] = React.useState(false);
-        const handleClose = () => setShowActionsheet(false);
-          return (
-            <>
-              <Button onPress={() => setShowActionsheet(true)}>
-                <ButtonText>Open Actionsheet</ButtonText>
-              </Button>
-              <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
-                <ActionsheetBackdrop />
-                <ActionsheetContent>
-                  <ActionsheetDragIndicatorWrapper>
-                    <ActionsheetDragIndicator />
-                  </ActionsheetDragIndicatorWrapper>
-                  <ActionsheetItem onPress={handleClose}>
-                    <ActionsheetItemText>Edit Message</ActionsheetItemText>
-                  </ActionsheetItem>
-                  <ActionsheetItem onPress={handleClose}>
-                    <ActionsheetItemText>Mark Unread</ActionsheetItemText>
-                  </ActionsheetItem>
-                  <ActionsheetItem onPress={handleClose}>
-                    <ActionsheetItemText>Remind Me</ActionsheetItemText>
-                  </ActionsheetItem>
-                  <ActionsheetItem onPress={handleClose}>
-                    <ActionsheetItemText>Add to Saved Items</ActionsheetItemText>
-                  </ActionsheetItem>
-                  <ActionsheetItem isDisabled onPress={handleClose}>
-                    <ActionsheetItemText>Delete</ActionsheetItemText>
-                  </ActionsheetItem>
-                </ActionsheetContent>
-              </Actionsheet>
-              </>
-          );
-        }
+export default function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <View>
+      <Button onPress={() => setShowModal(true)}>
+        <ButtonText>Show Modal</ButtonText>
+      </Button>
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        size="md"
+      >
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <ModalCloseButton>
+              <Icon
+                as={CloseIcon}
+                size="md"
+                className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
+              />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text size="sm" className="text-typography-500">
+              Elevate user interactions with our versatile modals. Seamlessly
+              integrate notifications, forms, and media displays. Make an impact
+              effortlessly.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              action="secondary"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Explore</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </View>
+  );
+}
