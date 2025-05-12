ca257f3e50f2ab158bf0a7e891264589ebd728eb
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-05-12 11:46:48 +0530

    feat: copied the introduction page

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -15,7 +15,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: (props) => <h2 className="text-2xl font-bold" {...props} />,
     h3: (props) => <h3 className="text-xl font-bold" {...props} />,
     // Define paragraph styles
-    p: (props) => <p className="text-base" {...props} />,
+    p: (props) => <p className="text-base my-2" {...props} />,
     // Define container styles
     wrapper: (props) => (
       <div className="max-w-prose mx-auto py-8" {...props} />
