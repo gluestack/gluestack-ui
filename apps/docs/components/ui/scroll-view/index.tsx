39ef0f174f09da5434cd1efd0ef7d6d4d684bfbb
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-05-13 10:42:33 +0530

    feat: added all recipes

---
+++ apps/docs/components/ui/scroll-view/index.tsx
@@ -0,0 +1,2 @@
+'use client';
+export { ScrollView } from 'react-native';
---
+++ packages/src/components/ui/image-background/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { ImageBackground as RNImageBackground } from 'react-native';
+import React from 'react';
+import { tva } from '@gluestack-ui/nativewind-utils/tva';
+
+const imageBackgroundStyle = tva({});
+
+const ImageBackground = React.forwardRef<
+  React.ComponentRef<typeof RNImageBackground>,
+  React.ComponentProps<typeof RNImageBackground>
+>(function ImageBackground({ className, ...props }, ref) {
+  return (
+    <RNImageBackground
+      className={imageBackgroundStyle({
+        class: className,
+      })}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+
+ImageBackground.displayName = 'ImageBackground';
+
+export { ImageBackground };
