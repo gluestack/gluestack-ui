fd78e8b42a33b90c0346c6096e294f188143f558
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-05-16 12:33:06 +0530

    feat: added table styling

--- apps/docs/components/docs-components/table/index.tsx
+++ apps/docs/components/docs-components/table/index.tsx
@@ -1,45 +1,38 @@
 import React from "react";
 
-
 export const Table = (props: React.HTMLProps<HTMLTableElement>) => (
   <table
     {...props}
-    className={`w-full border-collapse text-sm my-4 rounded-lg overflow-hidden`}
+    className={`w-full text-sm my-4 rounded-xl border-collapse`}
   />
 );
 
-export const TableHeader = (props: React.HTMLProps<HTMLTableSectionElement>) => (
+export const TableHeader = (
+  props: React.HTMLProps<HTMLTableSectionElement>
+) => (
   <thead
     {...props}
-    className={`py-4 border border-border-200`}
+    className={`py-4 border-outline-50 bg-background-50 rounded-xl`}
   />
 );
 
 export const TableBody = (props: React.HTMLProps<HTMLTableSectionElement>) => (
-  <tbody
-    {...props}
-    className={`border-x border-border-200`}
-  />
+  <tbody {...props} className={`border border-outline-50 last:rounded-b-xl`} />
 );
 
 export const TableRow = (props: React.HTMLProps<HTMLTableRowElement>) => (
-  <tr
-    {...props}
-    className={`border-b border-border-200 last:border-b-0`}
-  />
-);  
+  <tr {...props} className={`border-b border-outline-50 last:rounded-b-xl`} />
+);
 
 export const TableCell = (props: React.HTMLProps<HTMLTableCellElement>) => (
-  <td
-    {...props}
-    className={`px-4 py-3`}
-  />
-);  
+  <td {...props} className={`px-4 py-3 font-medium`} />
+);
 
-export const TableHeaderCell = (props: React.HTMLProps<HTMLTableCellElement>) => (
+export const TableHeaderCell = (
+  props: React.HTMLProps<HTMLTableCellElement>
+) => (
   <th
     {...props}
-    className={`bg-background-50 py-4 px-4 text-left font-medium`}
+    className={`py-4 px-4 text-left font-medium first:rounded-tl-xl last:rounded-tr-xl`}
   />
-);      
-
+);
