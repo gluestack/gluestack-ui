dfe72af5e3c3f1e0400646270d185c3898c9c4e1
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-05-19 14:03:25 +0530

    feat: make the docs for drawer component

--- apps/docs/components/page-components/all-components/drawer/index.tsx
+++ apps/docs/components/page-components/all-components/drawer/index.tsx
@@ -5,85 +5,62 @@ import { DrawerContent } from '@/components/ui/drawer';
 import { DrawerHeader } from '@/components/ui/drawer';
 import { DrawerBody } from '@/components/ui/drawer';
 import { DrawerFooter } from '@/components/ui/drawer';
+import { DrawerCloseButton } from '@/components/ui/drawer';
 import { Button } from '@/components/ui/button';
 import { ButtonText } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { Icon } from '@/components/ui/icon';
+import { CloseIcon } from '@/components/ui/icon';
 
 export default function Example() {
   return (
     <ComponentPreviewer
-      code={`function App() {
-  const [showDrawer, setShowDrawer] = React.useState(false)
+      code={`function Example() {
+  const [showDrawer, setShowDrawer] = React.useState(false);
   return (
     <>
       <Button
         onPress={() => {
-          setShowDrawer(true)
+          setShowDrawer(true);
         }}
       >
-        <ButtonText>Show Drawer</ButtonText>
+        <ButtonText>Open Drawer</ButtonText>
       </Button>
       <Drawer
         isOpen={showDrawer}
         onClose={() => {
-          setShowDrawer(false)
+          setShowDrawer(false);
         }}
-        size="{{size}}"
-        anchor="{{anchor}}"
       >
         <DrawerBackdrop />
         <DrawerContent>
           <DrawerHeader>
-            <Heading size="{{size}}">Heading</Heading>
+            <Heading size="lg">Menu</Heading>
+            <DrawerCloseButton>
+              <Icon as={CloseIcon} />
+            </DrawerCloseButton>
           </DrawerHeader>
           <DrawerBody>
-            <Text size="{{size}}" className="text-typography-800">
-              This is a sentence.
-            </Text>
+            <Text>This is the basic drawer component.</Text>
           </DrawerBody>
           <DrawerFooter>
             <Button
+              variant="outline"
               onPress={() => {
-                setShowDrawer(false)
+                setShowDrawer(false);
               }}
-              className="flex-1"
             >
-              <ButtonText>Button</ButtonText>
+              <ButtonText>Cancel</ButtonText>
             </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
-  )
+  );
 }`}
-      argTypes={{
-  "size": {
-    "control": {
-      "type": "select"
-    },
-    "options": [
-      "sm",
-      "md",
-      "lg",
-      "full"
-    ],
-    "defaultValue": "sm"
-  },
-  "anchor": {
-    "control": {
-      "type": "select"
-    },
-    "options": [
-      "left",
-      "right",
-      "top",
-      "bottom"
-    ],
-    "defaultValue": "left"
-  }
-}}
-      reactLive={{ Drawer, DrawerBackdrop, DrawerContent, DrawerHeader, DrawerBody, DrawerFooter, Button, ButtonText, Heading, Text }}
+      argTypes={{}}
+      reactLive={{ Drawer, DrawerBackdrop, DrawerContent, DrawerHeader, DrawerBody, DrawerFooter, DrawerCloseButton, Button, ButtonText, Heading, Text, Icon, CloseIcon }}
     />
   );
 }
