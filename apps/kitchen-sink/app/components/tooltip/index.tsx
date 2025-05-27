586e069675a1f1a00d1e67a875a3e6f109222bb7
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-05-27 19:34:20 +0530

    feat: made the docs for tooltip

--- apps/kitchen-sink/app/components/tooltip/index.tsx
+++ apps/kitchen-sink/app/components/tooltip/index.tsx
@@ -1,6 +1,14 @@
 import { ComponentPreviewer } from '@/components/custom/component-previewer'
 import { Tooltip, TooltipContent, TooltipText } from '@/components/ui/tooltip'
 import { Button, ButtonText } from '@/components/ui/button'
+import { Avatar, AvatarGroup, AvatarFallbackText } from '@/components/ui/avatar'
+import { HStack, Box } from '@/components/ui/box'
+import { Text } from '@/components/ui/text'
+import { Heading } from '@/components/ui/heading'
+import { VStack } from '@/components/ui/vstack'
+import { Center } from '@/components/ui/center'
+import { EditIcon, Icon } from '@/components/ui/icon'
+import { Command } from 'lucide-react-native'
 
 
 import React from 'react';
@@ -47,6 +55,87 @@ export default function ComponentExamples() {
       </TooltipContent>
     </Tooltip>
   )}}
+</ComponentPreviewer>
+
+<ComponentPreviewer props={{}} title={"Tooltip with Heading"}>
+  {props => {
+          return (
+            <Box className="justify-center h-96">
+              <AvatarGroup className="flex-row">
+                <Tooltip
+                  placement="top"
+                  trigger={(triggerProps) => {
+                    return (
+                      <Avatar
+                        size="lg"
+                        {...triggerProps}
+                        className="border-outline-0 border-2 bg-primary-600"
+                      >
+                        <AvatarFallbackText>+ 3</AvatarFallbackText>
+                      </Avatar>
+                    )
+                  }}
+                >
+                  <TooltipContent
+                    className="p-4 rounded-md max-w-72 bg-background-50"
+                  >
+                    <VStack space='md' className="rounded-lg">
+                      <Heading size="sm">View all members of this channel</Heading>
+                      <Center>
+                        <Text className="font-sm">Includes John, Sarah, Mike, Emily</Text>
+                        <Text className="font-sm">and David</Text>
+                      </Center>
+                    </VStack>
+                  </TooltipContent>
+                </Tooltip>
+               <Avatar size="lg"
+                    className="border-outline-0 border-2 bg-emerald-600" >
+                      <AvatarFallbackText className="text-white">Sandeep Srivastva</AvatarFallbackText>
+                </Avatar>
+                   <Avatar size="lg" className="border-outline-0 border-2 bg-cyan-600" >
+                      <AvatarFallbackText className="text-white">Arjun Kapoor</AvatarFallbackText>
+                </Avatar>
+                      <Avatar size="lg"
+                    className="border-outline-0 border-2 bg-indigo-600" >
+                      <AvatarFallbackText className="text-white">Ritik Sharma </AvatarFallbackText>
+                </Avatar>
+              </AvatarGroup>
+            </Box>
+          );}}
+</ComponentPreviewer>
+
+<ComponentPreviewer props={{}} title={"Tooltip with Icon"}>
+  {props => {
+          return (
+            <Box className="h-96 justify-center">
+              <Tooltip
+                  placement="top"
+                  trigger={(triggerProps) => {
+                    return (
+                    <Avatar size="md" {...triggerProps} className="bg-primary-600">
+                      <Icon as={EditIcon} size="sm" className="text-white"/>
+                    </Avatar>
+                    )
+                  }}
+              >
+                <TooltipContent
+                className="bg-background-50 rounded-md"
+                >
+                  <Box className="p-2.5">
+                    <Text size="sm">New message</Text>
+                    <HStack space="xs" className="p-1 ml-3">
+                          <Avatar size="xs" className="bg-gray-500 rounded">
+                              <Icon as={Command} className="text-typography-0"/>
+                          </Avatar>
+                          <Avatar size="xs" className="bg-gray-500 rounded">
+                            <AvatarFallbackText>N</AvatarFallbackText>
+                          </Avatar>
+                    </HStack>
+                  </Box>
+                </TooltipContent>
+              </Tooltip>
+            </Box>
+        );}}
 </ComponentPreviewer>
         </ScrollView>
   );
