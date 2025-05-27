9c1ed0e3216bf1e0255b44938b661cc5520a7422
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-05-27 17:25:44 +0530

    feat: made the docs for the spinner

--- apps/kitchen-sink/app/components/spinner/index.tsx
+++ apps/kitchen-sink/app/components/spinner/index.tsx
@@ -1,5 +1,8 @@
 import { ComponentPreviewer } from '@/components/custom/component-previewer'
 import { Spinner } from '@/components/ui/spinner'
+import { HStack } from '@/components/ui/hstack'
+import { Text } from '@/components/ui/text'
+import { VStack } from '@/components/ui/vstack'
 
 
 import React from 'react';
@@ -33,9 +36,31 @@ export default function ComponentExamples() {
     ],
     "defaultValue": "red"
   }
-}} title={undefined}>
+}} title={"Basic"}>
   {props => {
   return <Spinner size={props.size} color={props.color} />}}
+</ComponentPreviewer>
+
+<ComponentPreviewer props={{}} title={"Spinner with Label"}>
+  {props => {
+  return (
+    <HStack space="sm" >
+          <Spinner/>
+          <Text size="md">Please Wait</Text>
+        </HStack>
+  );}}
+</ComponentPreviewer>
+
+<ComponentPreviewer props={{}} title={"Color Scheme"}>
+  {props => {
+  return (
+    <VStack space="md" >
+        <Spinner color="red" />
+        <Spinner color="blue" />
+        <Spinner color="green" />
+        <Spinner color="orange" />
+      </VStack>
+  );}}
 </ComponentPreviewer>
         </ScrollView>
   );
