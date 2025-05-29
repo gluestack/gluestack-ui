eea98dd43b563027b1d2b93c94ccc6fec94093e3
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-05-29 18:44:51 +0530

    feat: added GTMs and sentry

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,8 +1,8 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import { withGluestackUI } from "@gluestack/ui-next-adapter";
 import withPlugins from "next-compose-plugins";
 import createMDX from "@next/mdx";
 import rehypeSlug from "rehype-slug";
-
 const redirects = [
   {
     source: "/ui/docs/styling/state",
@@ -742,22 +742,11 @@ const redirects = [
   },
 
   {
-    source: "/ui/docs/components/text-area",
-    destination: "/ui/docs/components/textarea",
-    permanent: true,
-  },
-  {
-    source: "/ui/with-nativewind/docs/overview/introduction",
-    destination: "/ui/docs/home/overview/introduction",
-    permanent: true,
-  },
-  {
     source: "/ui/docs/guides/resources/style/docs/getting-started/styled",
     destination: "/ui/docs/home/overview/introduction",
     permanent: true,
   },
 ];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = withGluestackUI({
   reactStrictMode: true,
@@ -1032,4 +1021,37 @@ const config = withPlugins([withMDX], {
   ...nextConfig,
 });
 
-export default config;
+export default withSentryConfig(config, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: "gluestack",
+  project: "gluestack-website",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
