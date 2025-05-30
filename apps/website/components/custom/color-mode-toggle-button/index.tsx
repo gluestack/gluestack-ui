703134f886ebfceaa0ee5b94add9cc26272c56b9
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-05-30 16:59:07 +0530

    feat: created color-mode-toggle-button fab for toggling theme in website

---
+++ apps/website/components/custom/color-mode-toggle-button/index.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import {
+  Pressable,
+  SunIcon,
+  MoonIcon,
+  Icon,
+  Fab,
+  FabIcon,
+} from "@/components/ui";
+import { ThemeContext } from "@/utils/context/theme-context";
+
+export const ToggleColorModeButton = () => {
+  const { colorMode, setColorMode } = useContext(ThemeContext);
+  return (
+    <div className="fixed bottom-4 right-0">
+      <Fab
+        size="md"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+        onPress={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+      >
+        <FabIcon
+          as={colorMode === "dark" ? SunIcon : MoonIcon}
+          className="text-typography-300"
+        />
+      </Fab>
+    </div>
+  );
+};
