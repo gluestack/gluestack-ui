6125aa8232d50d6e5d2124c98ff850319118bf48
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-05-30 11:08:31 +0530

    fix: sidebar z-index fixed

--- apps/website/components/custom/layout/index.tsx
+++ apps/website/components/custom/layout/index.tsx
@@ -31,7 +31,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
         }`}
       >
         <div
-          className={`w-[250px] hidden lg:flex z-10 ${
+          className={`w-[250px] hidden lg:flex z-0 ${
             fluidLayout ? "border-r border-gray-200 dark:border-gray-800" : ""
           }`}
         >
