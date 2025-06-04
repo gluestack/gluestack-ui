be90dfc58f1def6f53c34ac71f8bc3da07991ec3
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-04 16:11:24 +0530

    feat: initialized starter-kit-expo with typescript and nativewind

---
+++ apps/starter-kit-expo/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
