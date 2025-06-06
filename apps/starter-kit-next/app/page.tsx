28188bc1571f13b02258155aa969840c796a68fd
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-06 12:06:45 +0530

    chore: replicated the template as in previous nextjs starter kit

--- apps/starter-kit-next/app/page.tsx
+++ apps/starter-kit-next/app/page.tsx
@@ -1,104 +1,72 @@
-'use client';
-import Image from 'next/image';
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <Container />
+    </main>
   );
 }
+
+const FeatureCard = ({ iconSvg, name, desc }: any) => {
+  return (
+    <Box className="flex-column border border-w-1 border-outline-700 flex-1 m-2 p-4 rounded">
+      <Box className="items-center flex flex-row">
+        <Image
+          src={`/${iconSvg}`}
+          alt="document"
+          priority
+          width={22}
+          height={22}
+        />
+        <Text className="text-typography-white font-medium ml-2 text-xl">
+          {name}
+        </Text>
+      </Box>
+      <Text className="text-typography-400 mt-2">{desc}</Text>
+    </Box>
+  );
+};
+
+const Container = () => {
+  return (
+    <Box className="flex-1 bg-black h-[100vh]">
+      <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
+        <Image src="/gradient.svg" alt="Gradient" fill priority />
+      </Box>
+      <Box className="flex flex-1 items-center my-16 mx-5 lg:my-24 lg:mx-32">
+        <Box className="py-2 px-6 rounded-full items-center flex-column sm:flex-row md:self-start">
+          <Text className="text-typography-white font-normal">
+            Get started by editing
+          </Text>
+          <Text className="text-typography-white font-medium ml-2">
+            <code>./app/page.tsx</code>
+          </Text>
+        </Box>
+        <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
+          <Image src="/logo.svg" fill alt="logo" priority />
+        </Box>
+
+        <Box className="flex-column md:flex-row">
+          <FeatureCard
+            iconSvg="document-data.svg"
+            name="Docs"
+            desc="Find in-depth information about gluestack features and API."
+          />
+          <FeatureCard
+            iconSvg="lightbulb-person.svg"
+            name="Learn"
+            desc="Learn about gluestack in an interactive course with quizzes!"
+          />
+          <FeatureCard
+            iconSvg="rocket.svg"
+            name="Deploy"
+            desc="Instantly drop your gluestack site to a shareable URL with vercel."
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
