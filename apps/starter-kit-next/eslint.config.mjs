b1fe2192f7eef47b68a218c2c84d34141b1522a7
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-06 12:04:34 +0530

    chore: updated eslint config

--- apps/starter-kit-next/eslint.config.mjs
+++ apps/starter-kit-next/eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
