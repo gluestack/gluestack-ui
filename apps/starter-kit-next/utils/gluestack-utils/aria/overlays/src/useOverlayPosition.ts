bc6b15f7541ec707f8bb410f4b50d6e2c7e66020
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-12 15:54:31 +0530

    fix: fixed broken utils

--- apps/starter-kit-next/utils/gluestack-utils/aria/overlays/src/useOverlayPosition.ts
+++ apps/starter-kit-next/utils/gluestack-utils/aria/overlays/src/useOverlayPosition.ts
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 import type { Placement, PositionProps } from '@react-types/overlays';
 //@ts-ignore
-import { isRTL } from '@react-native-aria/utils';
+import { isRTL } from '@/utils/gluestack-utils/aria/utils/src';
 
 import { APPROX_STATUSBAR_HEIGHT } from './utils';
 const measureOffset = (ref: RefObject<any>) =>
