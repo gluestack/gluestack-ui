024c763990b99283344488b9d17500f6e03991b2
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-17 19:06:36 +0530

    v0.1.16

--- packages/gluestack-ui/CHANGELOG.md
+++ packages/gluestack-ui/CHANGELOG.md
@@ -1,3 +1,9 @@
 ## 0.4.9-alpha.0 (2023-07-21)
 
+## 0.1.13
+
+### Patch Changes
+
+- fix: removed unneccessary gluestack-ui-config.ts init
+
 - Testing
--- packages/gluestack-ui/src/eject-components/index.ts
+++ packages/gluestack-ui/src/eject-components/index.ts
@@ -1,9 +1,7 @@
 import { updateConfig } from './add-aliases';
 import { componentAdder } from './add-components';
-import { addGluestackConfig } from './add-config';
 
 const ejectComponents = async () => {
-  await addGluestackConfig();
   await componentAdder('--all');
   await updateConfig();
 };
