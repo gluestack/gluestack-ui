7d71a0cc7ff3d92358cbca9b27fe286d870754f8
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-06-18 19:00:21 +0530

    chore: added next-sitemap.config.js and added og images

---
+++ apps/website/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://gluestack.io',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  };
