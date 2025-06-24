d4919bc5a173d3a16f1a1a3414659a23b898ba0d
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-06-24 12:23:52 +0530

    feat: change the basic example for skeleton

--- apps/website/components/page-components/all-components/skeleton/index.tsx
+++ apps/website/components/page-components/all-components/skeleton/index.tsx
@@ -9,11 +9,11 @@ export default function Example() {
     <ComponentPreviewer
       code={`function Example() {
   return (
-    <Box className="w-[325px] gap-4 p-3 rounded-md bg-background-100">
-      <Skeleton variant="sharp" className="h-[150px]" />
-      <SkeletonText _lines={3} className="h-3" />
-      <HStack className="gap-2 align-middle">
-        <Skeleton variant="circular" className="h-[24px] w-[24px] mr-2" />
+    <Box className="w-[300px] gap-4 p-3 rounded-md bg-background-100">
+      <Skeleton variant="sharp" className="h-[100px]" />
+      <SkeletonText _lines={3} className="h-2" />
+      <HStack className="gap-1 align-middle">
+        <Skeleton variant="circular" className="h-[24px] w-[28px] mr-2" />
         <SkeletonText _lines={2} gap={1} className="h-2 w-2/5" />
       </HStack>
     </Box>
