1140d55be0a51c4768ac073ca890d9b754136306
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-07-01 13:08:00 +0530

    fix: fixed links styling

--- src/docs/home/overview/introduction/githubLink.tsx
+++ src/docs/home/overview/introduction/githubLink.tsx
@@ -1,9 +1,9 @@
 import { Link, LinkText } from '@/components/ui/link';
 
-export const StarterKitLink = ({ children }: { children: React.ReactNode }) => {
+export const GithubLink = ({ children }: { children: React.ReactNode }) => {
   return (
     <Link
-      href={`https://github.com/gluestack/gluestack-ui/tree/${process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main'}/apps/${children}`}
+      href={`https://github.com/gluestack/gluestack-ui/tree/${process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main'}`}
       isExternal
       className="leading-6 font-body text-typography-950 underline underline-offset-4 decoration-typography-950 inline-block"
     >
