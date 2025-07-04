95b87934b929a010d34c1286af11bd52b358ec92
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-07-04 16:05:45 +0530

    fix: fixed safeareaview issue

--- scripts/mappers/kitchen-sink/templates.ts
+++ scripts/mappers/kitchen-sink/templates.ts
@@ -23,11 +23,9 @@ import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 export default function ComponentExamples() {
   return (
-    <SafeAreaView className="flex-1 bg-background-0">
         <ScrollView className="bg-background-0 flex-1" contentContainerClassName="px-3 pb-6 web:flex-col">
       ${processedContent.trim()}
         </ScrollView>
-    </SafeAreaView>
   );
 }`;
 };
