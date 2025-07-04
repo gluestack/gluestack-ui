2b26cd403ee5e182032af4340482f9f8b30aa004
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-07-04 19:08:09 +0530

    fix: set fulsh style

--- packages/gluestack-utils/src/nativewind-utils/index.ts
+++ packages/gluestack-utils/src/nativewind-utils/index.ts
@@ -2,3 +2,4 @@ export { tva } from './tva';
 export type { VariantProps } from './types';
 export { withStyleContext, useStyleContext } from './withStyleContext';
 export { isWeb } from './IsWeb';
+export { setFlushStyles } from './flush';
