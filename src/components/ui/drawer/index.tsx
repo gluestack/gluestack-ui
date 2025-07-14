aab0b16587e2726fad4ef990224360abb023a579
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-07-14 12:22:57 +0530

    feat: fix the drawer component

--- src/components/ui/drawer/index.tsx
+++ src/components/ui/drawer/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { createModal as createDrawer } from '@/components/ui/modal/creator';
+import { createModal as createDrawer } from '@gluestack-ui-nightly/core/modal/creator';
 import {
   Pressable,
   View,
