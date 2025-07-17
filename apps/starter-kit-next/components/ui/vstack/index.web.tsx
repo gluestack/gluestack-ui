8fe25d44fee0523c87a30ca122e3c46c548eded7
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-07-17 10:57:47 +0530

    chore: added all gluestack-ui components

---
+++ apps/starter-kit-next/components/ui/vstack/index.web.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { VariantProps } from '@gluestack-ui-nightly/utils/nativewind-utils';
+
+import { vstackStyle } from './styles';
+
+type IVStackProps = React.ComponentProps<'div'> &
+  VariantProps<typeof vstackStyle>;
+
+const VStack = React.forwardRef<React.ComponentRef<'div'>, IVStackProps>(
+  function VStack({ className, space, reversed, ...props }, ref) {
+    return (
+      <div
+        className={vstackStyle({ space, reversed, class: className })}
+        {...props}
+        ref={ref}
+      />
+    );
+  }
+);
+
+VStack.displayName = 'VStack';
+
+export { VStack };
