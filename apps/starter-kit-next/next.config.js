a99036be730e1e118a7e508ade8691879fed7d6f
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-07-17 10:56:58 +0530

    chore: transpiled core and utils package

--- apps/starter-kit-next/next.config.js
+++ apps/starter-kit-next/next.config.js
@@ -2,6 +2,7 @@ import { withUIAdapter } from '@gluestack-nightly/ui-next-adapter';
 
 const config = withUIAdapter({
   // Your config with full type safety
+  transpilePackages: ['@gluestack-ui-nightly/core', '@gluestack-ui-nightly/utils'],
 });
 
 export default config;
