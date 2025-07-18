eca3a147c15e7a9b14fd6e9c421f75efe29bd15f
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-07-18 15:15:24 +0530

    chore: removed starter-kits mappers (no longer needed)

--- scripts/mappers/starter-kit-expo/index.ts
+++
@@ -1,7 +0,0 @@
-import * as componentOperations from './componentsOperations';
-
-export default {
-  component: function (component: string, event = 'added') {
-    componentOperations.copyComponent(component, event);
-  },
-};
