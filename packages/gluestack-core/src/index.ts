c29c80d53ae154c68e0171ecee6b9060e4318005
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2025-07-22 16:08:32 +0530

    feat: change the gluestack core package build config

---
+++ packages/gluestack-core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './button/aria';
+export * from './button/creator';
+export * from './accordion/aria';
+export * from './accordion/creator';
+export * from './icon/creator';
+export * from './textarea/creator';
+export * from './toast/creator';  
