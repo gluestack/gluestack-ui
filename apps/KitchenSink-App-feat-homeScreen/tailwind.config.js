2661bd3b6d36f9e3b9de412642cf3a8f279bc2b5
Sanchitv3 <sanchitverma03032003@gmail.com>
2025-12-08 18:33:28 +0530

    chore: updated content path

--- apps/KitchenSink-App-feat-homeScreen/tailwind.config.js
+++ apps/KitchenSink-App-feat-homeScreen/tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     './app/**/*.{html,js,jsx,ts,tsx,mdx}',
     './components/**/*.{html,js,jsx,ts,tsx,mdx}',
     './utils/**/*.{html,js,jsx,ts,tsx,mdx}',
-    './*.{html,js,jsx,ts,tsx,mdx}',
-    './src/**/*.{html,js,jsx,ts,tsx,mdx}',
   ],
   presets: [require('nativewind/preset')],
   important: 'html',
