d03c66eb641ebe7bcae2b46bad7266b45b5e9c4b
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-01-05 11:10:38 +0530

    refactor: replace cssInterop with withUniwind in Portal component across multiple projects

--- src/components/ui/portal/index.tsx
+++ src/components/ui/portal/index.tsx
@@ -1,9 +1,6 @@
 'use client';
 import React from 'react';
 import { Overlay } from '@gluestack-ui/core/overlay/creator';
-import { cssInterop } from 'nativewind';
-
-cssInterop(Overlay, { className: 'style' });
 
 const Portal = React.forwardRef<
   React.ComponentRef<typeof Overlay>,
