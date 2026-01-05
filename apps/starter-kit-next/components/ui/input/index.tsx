1582c5fec2909cffb1a6096b46405e3baca9651f
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-01-05 11:11:54 +0530

    refactor: remove unused cssInterop calls and imports from UIInput component across multiple projects

--- apps/starter-kit-next/components/ui/input/index.tsx
+++ apps/starter-kit-next/components/ui/input/index.tsx
@@ -7,9 +7,8 @@ import {
   withStyleContext,
   useStyleContext,
 } from '@gluestack-ui/utils/nativewind-utils';
-import { cssInterop } from 'nativewind';
 import type { VariantProps } from '@gluestack-ui/utils/nativewind-utils';
-import { PrimitiveIcon, UIIcon } from '@gluestack-ui/core/icon/creator';
+import { UIIcon } from '@/components/ui/icon';
 
 const SCOPE = 'INPUT';
 
@@ -18,19 +17,6 @@ const UIInput = createInput({
   Icon: UIIcon,
   Slot: Pressable,
   Input: TextInput,
-});
-
-cssInterop(PrimitiveIcon, {
-  className: {
-    target: 'style',
-    nativeStyleToProp: {
-      height: true,
-      width: true,
-      fill: true,
-      color: 'classNameColor',
-      stroke: true,
-    },
-  },
 });
 
 const inputStyle = tva({
