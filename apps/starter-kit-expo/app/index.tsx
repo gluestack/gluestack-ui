ddb2d7fa4fb16c6abf2bb747d984e46a5362576a
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-01-06 13:39:55 +0530

    feat: enhance Home component with new UI elements including AlertDialog, Popover, Actionsheet, and Drawer for improved user interaction

--- apps/starter-kit-expo/app/index.tsx
+++ apps/starter-kit-expo/app/index.tsx
@@ -1,63 +1,471 @@
-import React from 'react';
-import Gradient from '@/assets/icons/Gradient';
-import Logo from '@/assets/icons/Logo';
-import { Box } from '@/components/ui/box';
-import { ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogBody,
+  AlertDialogBackdrop,
+} from '@/components/ui/alert-dialog';
+import { Button, ButtonIcon, ButtonText } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
+import { Heading } from '@/components/ui/heading';
+import { Icon, EditIcon } from '@/components/ui/icon';
+import {
+  Popover,
+  PopoverBackdrop,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+} from '@/components/ui/popover';
+import {
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  useToast,
+} from '@/components/ui/toast';
+import {
+  Actionsheet,
+  ActionsheetContent,
+  ActionsheetItem,
+  ActionsheetItemText,
+  ActionsheetDragIndicator,
+  ActionsheetDragIndicatorWrapper,
+  ActionsheetBackdrop,
+  ActionsheetIcon,
+} from '@/components/ui/actionsheet';
+import {
+  EyeOffIcon,
+  ClockIcon,
+  DownloadIcon,
+  TrashIcon,
+} from '@/components/ui/icon';
+import {
+  Checkbox,
+  CheckboxIndicator,
+  CheckboxLabel,
+  CheckboxIcon,
+} from '@/components/ui/checkbox';
+import { CheckIcon } from '@/components/ui/icon';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionTrigger,
+  AccordionTitleText,
+  AccordionContent,
+  AccordionContentText,
+  AccordionIcon,
+} from '@/components/ui/accordion';
+import { Divider } from '@/components/ui/divider';
+import { ChevronDownIcon, ChevronUpIcon } from '@/components/ui/icon';
+import {
+  Radio,
+  RadioGroup,
+  RadioIndicator,
+  RadioIcon,
+  RadioLabel,
+} from '@/components/ui/radio';
+import { CircleIcon } from '@/components/ui/icon';
+import { Tooltip, TooltipContent, TooltipText } from '@/components/ui/tooltip';
+import {
+  Select,
+  SelectTrigger,
+  SelectInput,
+  SelectIcon,
+  SelectPortal,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectItem,
+} from '@/components/ui/select';
 
-import { Button, ButtonText } from '@/components/ui/button';
-import { useRouter } from 'expo-router';
-import { Icon } from '@/components/ui/icon';
-
-const FeatureCard = ({ iconSvg: IconSvg, name, desc }: any) => {
-  return (
-    <Box
-      className="flex-column md:flex-1 m-2 p-4 rounded-lg bg-background-0/40"
-      key={name}
-    >
-      <Box className="items-center flex flex-row">
-        <Icon as={IconSvg}/>
-        <Text className="font-medium ml-2 text-xl">{name}</Text>
-      </Box>
-      <Text className="mt-2">{desc}</Text>
-    </Box>
-  );
-};
+import { Box } from '@/components/ui/box';
+import { Portal } from '@/components/ui/portal';
+import { HStack } from '@/components/ui/hstack';
+import { CloseIcon } from '@/components/ui/icon';
+import {
+  Drawer,
+  DrawerBackdrop,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  DrawerCloseButton,
+} from '@/components/ui/drawer';
+import { Progress, ProgressFilledTrack } from '@/components/ui/progress';
+import { Spinner } from '@/components/ui/spinner';
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from '@/components/ui/slider';
+import { Grid, GridItem } from '@/components/ui/grid';
 
 export default function Home() {
-  const router = useRouter();
+  const [showActionsheet, setShowActionsheet] = useState(false);
+  const handleCloseActionsheet = () => setShowActionsheet(false);
+  const [showAlertDialog, setShowAlertDialog] = useState(false);
+  const handleClose = () => setShowAlertDialog(false);
+  const [isOpenPopover, setIsOpenPopover] = useState(false);
+  const handleOpenPopover = () => {
+    setIsOpenPopover(true);
+  };
+  const handleClosePopover = () => {
+    setIsOpenPopover(false);
+  };
+  const toast = useToast();
+  const [toastId, setToastId] = React.useState(0);
+  const handleToast = () => {
+    if (!toast.isActive(toastId)) {
+      showNewToast();
+    }
+  };
+  const showNewToast = () => {
+    const newId = Math.random();
+    setToastId(newId);
+    toast.show({
+      id: newId,
+      placement: 'top',
+      duration: 3000,
+      render: ({ id }) => {
+        const uniqueToastId = 'toast-' + id;
+        return (
+          <Toast nativeID={uniqueToastId} action="muted" variant="solid">
+            <ToastTitle>Hello!</ToastTitle>
+            <ToastDescription>
+              This is a customized toast message.
+            </ToastDescription>
+          </Toast>
+        );
+      },
+    });
+  };
+  const [showDrawer, setShowDrawer] = React.useState(false);
+  const [visible, setVisible] = useState(false);
+  const handleClosePortal = () => setVisible(false);
   return (
-    <Box className="flex-1 bg-background-300 h-[100vh]">
-        <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
-          <Gradient />
-        </Box>
-      {/* <ScrollView
-        style={{ height: '100%' }}
-        contentContainerStyle={{ flexGrow: 1 }}
-      > */}
-        <Box className="flex flex-1 items-center mx-5 lg:my-24 lg:mx-32 py-safe">
-          <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
-            <Box className="bg-background-template py-2 px-6 rounded-full items-center flex-column md:flex-row md:self-start">
-              <Text className="text-white font-medium">
-                Get started by editing
-              </Text>
-              <Text className="text-white font-medium ml-2">./App.tsx or ./app/index.tsx (or whatever entry point you have)</Text>
-            </Box>
+    <Box className="flex-1 bg-background-0 gap-2 flex  items-center pt-safe">
+      <Icon as={EditIcon} size="lg" />
+      <Button onPress={() => setShowAlertDialog(true)}>
+        <ButtonText>Open Dialog</ButtonText>
+      </Button>
+      <AlertDialog isOpen={showAlertDialog} onClose={handleClose} size="md">
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading className="text-typography-950 font-semibold" size="md">
+              Are you sure you want to delete this post?
+            </Heading>
+          </AlertDialogHeader>
+          <AlertDialogBody className="mt-3 mb-4">
+            <Text size="sm">
+              Deleting the post will remove it permanently and cannot be undone.
+              Please confirm if you want to proceed.
+            </Text>
+          </AlertDialogBody>
+          <AlertDialogFooter className="">
             <Button
-              size="md"
-              className="bg-primary-500 px-6 py-2 rounded-full"
+              variant="outline"
+              action="secondary"
+              onPress={handleClose}
+              size="sm"
+            >
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button size="sm" onPress={handleClose}>
+              <ButtonText>Delete</ButtonText>
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Popover
+        isOpen={isOpenPopover}
+        onClose={handleClosePopover}
+        onOpen={handleOpenPopover}
+        placement="bottom"
+        size="md"
+        trigger={(triggerProps) => {
+          return (
+            <Button {...triggerProps}>
+              <ButtonText>Open Popover</ButtonText>
+            </Button>
+          );
+        }}
+      >
+        <PopoverBackdrop />
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverBody>
+            <Text className="text-typography-900">
+              Alex, Annie and many others are already enjoying the Pro features,
+              don't miss out on the fun!
+            </Text>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+      <Button onPress={() => setShowActionsheet(true)}>
+        <ButtonText>Open Actionsheet</ButtonText>
+      </Button>
+      <Button onPress={() => setShowActionsheet(true)}>
+        <ButtonText>Open</ButtonText>
+      </Button>
+      <Actionsheet isOpen={showActionsheet} onClose={handleCloseActionsheet}>
+        <ActionsheetBackdrop />
+        <ActionsheetContent>
+          <ActionsheetDragIndicatorWrapper>
+            <ActionsheetDragIndicator />
+          </ActionsheetDragIndicatorWrapper>
+          <ActionsheetItem onPress={handleCloseActionsheet}>
+            <ActionsheetIcon className="stroke-background-700" as={EditIcon} />
+            <ActionsheetItemText>Edit Message</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleCloseActionsheet}>
+            <ActionsheetIcon
+              className="stroke-background-700"
+              as={EyeOffIcon}
+            />
+            <ActionsheetItemText>Mark Unread</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleCloseActionsheet}>
+            <ActionsheetIcon className="stroke-background-700" as={ClockIcon} />
+            <ActionsheetItemText>Remind Me</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem onPress={handleCloseActionsheet}>
+            <ActionsheetIcon
+              className="stroke-background-700"
+              as={DownloadIcon}
+            />
+            <ActionsheetItemText>Add to Saved Items</ActionsheetItemText>
+          </ActionsheetItem>
+          <ActionsheetItem isDisabled onPress={handleCloseActionsheet}>
+            <ActionsheetIcon className="stroke-background-700" as={TrashIcon} />
+            <ActionsheetItemText>Delete</ActionsheetItemText>
+          </ActionsheetItem>
+        </ActionsheetContent>
+      </Actionsheet>
+      <Checkbox isDisabled={false} isInvalid={false} size="md">
+        <CheckboxIndicator>
+          <CheckboxIcon as={CheckIcon} />
+        </CheckboxIndicator>
+        <CheckboxLabel>Label</CheckboxLabel>
+      </Checkbox>
+      <Accordion
+        size="md"
+        variant="filled"
+        type="single"
+        isCollapsible={true}
+        isDisabled={false}
+        className="m-5 w-[90%] border border-outline-200"
+      >
+        <AccordionItem value="a">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      How do I place an order?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} className="ml-3" />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} className="ml-3" />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              To place an order, simply select the products you want, proceed to
+              checkout, provide shipping and payment information, and finalize
+              your purchase.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+        <Divider />
+        <AccordionItem value="b">
+          <AccordionHeader>
+            <AccordionTrigger>
+              {({ isExpanded }) => {
+                return (
+                  <>
+                    <AccordionTitleText>
+                      What payment methods do you accept?
+                    </AccordionTitleText>
+                    {isExpanded ? (
+                      <AccordionIcon as={ChevronUpIcon} className="ml-3" />
+                    ) : (
+                      <AccordionIcon as={ChevronDownIcon} className="ml-3" />
+                    )}
+                  </>
+                );
+              }}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>
+            <AccordionContentText>
+              We accept all major credit cards, including Visa, Mastercard, and
+              American Express. We also support payments through PayPal.
+            </AccordionContentText>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <RadioGroup>
+        <Radio value="" size="md" isInvalid={false} isDisabled={false}>
+          <RadioIndicator>
+            <RadioIcon as={CircleIcon} />
+          </RadioIndicator>
+          <RadioLabel>Label</RadioLabel>
+        </Radio>
+      </RadioGroup>
+      <Button onPress={handleToast}>
+        <ButtonText>Press Me</ButtonText>
+      </Button>
+      <Tooltip
+        placement="top"
+        trigger={(triggerProps) => {
+          return (
+            <Button {...triggerProps}>
+              <ButtonText>Hover on me!</ButtonText>
+            </Button>
+          );
+        }}
+      >
+        <TooltipContent>
+          <TooltipText>Tooltip</TooltipText>
+        </TooltipContent>
+      </Tooltip>
+      <Select>
+        <SelectTrigger variant="outline" size="md">
+          <SelectInput placeholder="Select option" />
+          <SelectIcon className="mr-3" as={ChevronDownIcon} />
+        </SelectTrigger>
+        <SelectPortal>
+          <SelectBackdrop />
+          <SelectContent>
+            <SelectDragIndicatorWrapper>
+              <SelectDragIndicator />
+            </SelectDragIndicatorWrapper>
+            <SelectItem label="UX Research" value="ux" />
+            <SelectItem label="Web Development" value="web" />
+            <SelectItem
+              label="Cross Platform Development Process"
+              value="Cross Platform Development Process"
+            />
+            <SelectItem label="UI Designing" value="ui" isDisabled={true} />
+            <SelectItem label="Backend Development" value="backend" />
+          </SelectContent>
+        </SelectPortal>
+      </Select>
+      <Portal isOpen={visible} className="justify-center items-center">
+        <HStack className="border-2 w-1/3 py-10 gap-4 rounded-lg flex-row justify-center items-center bg-background-0">
+          <Text className="text-typography-950">Portal Content</Text>
+          <Button
+            size="xs"
+            className="h-6 px-1 absolute top-2 right-2"
+            variant="outline"
+            onPress={handleClosePortal}
+          >
+            <ButtonIcon as={CloseIcon} />
+          </Button>
+        </HStack>
+      </Portal>
+      <Button onPress={() => setVisible(!visible)}>
+        <ButtonText>Toggle Portal</ButtonText>
+      </Button>
+      <Button
+        onPress={() => {
+          setShowDrawer(true);
+        }}
+      >
+        <ButtonText>Open Drawer</ButtonText>
+      </Button>
+      <Drawer
+        isOpen={showDrawer}
+        size="md"
+        anchor="left"
+        onClose={() => {
+          setShowDrawer(false);
+        }}
+      >
+        <DrawerBackdrop />
+        <DrawerContent>
+          <DrawerHeader>
+            <Heading size="lg">Menu</Heading>
+            <DrawerCloseButton>
+              <Icon as={CloseIcon} />
+            </DrawerCloseButton>
+          </DrawerHeader>
+          <DrawerBody>
+            <Text>This is the basic drawer component.</Text>
+          </DrawerBody>
+          <DrawerFooter>
+            <Button
+              variant="outline"
               onPress={() => {
-                router.push('/tabs/tab1');
+                setShowDrawer(false);
               }}
             >
-              <ButtonText>Explore Tab Navigation</ButtonText>
+              <ButtonText>Cancel</ButtonText>
             </Button>
-          </Box>
-          <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
-            <Logo />
-          </Box>
-        </Box>
-      {/* </ScrollView> */}
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <Progress value={40} size="md" orientation="horizontal">
+        <ProgressFilledTrack />
+      </Progress>
+      <Spinner size="large" color="grey" />
+      <Slider
+        defaultValue={30}
+        size="md"
+        orientation="horizontal"
+        isDisabled={false}
+        isReversed={false}
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <Grid className="gap-4" _extra={{ className: 'grid-cols-10' }}>
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-3' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-5' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-2' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-4' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-6' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-2' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-4' }}
+      />
+      <GridItem
+        className="bg-background-50 p-6 rounded-md"
+        _extra={{ className: 'col-span-4' }}
+      />
+    </Grid>
     </Box>
   );
 }
