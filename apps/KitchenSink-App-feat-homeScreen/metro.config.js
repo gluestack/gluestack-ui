6e357a277daf04a20b6dbe88da2c06524ad8d969
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-01-13 10:18:20 +0530

    feat: integrate uniwind and update tailwindcss to version 4.1.18, enhancing styling capabilities

--- apps/KitchenSink-App-feat-homeScreen/metro.config.js
+++ apps/KitchenSink-App-feat-homeScreen/metro.config.js
@@ -1,6 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+// const { withNativeWind } = require('nativewind/metro');
+const { withUniwindConfig } = require('uniwind/metro'); 
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' , inlineRem: 16});
+// module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withUniwindConfig(config, {  
+    // relative path to your global.css file (from previous step)
+    cssEntryFile: './global.css',
+    // (optional) path where we gonna auto-generate typings
+    // defaults to project's root
+    dtsFile: './uniwind-types.d.ts'
+  });
