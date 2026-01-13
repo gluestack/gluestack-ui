6d60ae51479f2cf561a6f8a50e13882abd5e9da8
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2026-01-13 10:56:07 +0530

    feat: tabs component

--- apps/website/components/docs-components/tabs/index.tsx
+++ apps/website/components/docs-components/tabs/index.tsx
@@ -7,14 +7,14 @@ export const Tabs = ({ children }) => {
   const [activeTab, setActiveTab] = useState(0);
   return (
     <div className="tabs-container my-4">
-      <div className="flex border-b border-typography-100">
+      <div className="flex border-b border-border">
         {tabNames.map((name, index) => (
           <button
             key={index}
             className={`py-2 px-4 font-bold text-lg ${
               activeTab === index
-                ? 'text-typography-950 border-b-2 border-typography-950'
-                : 'text-typography-400'
+                ? 'text-foreground border-b-2 border-primary'
+                : 'text-muted-foreground'
             }`}
             onClick={() => setActiveTab(index)}
           >
