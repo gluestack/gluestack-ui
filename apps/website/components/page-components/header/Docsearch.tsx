651aec7c31f520f2fb0e8f4bcca88239b360a19c
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2026-01-13 10:00:28 +0530

    feat: header setup

--- apps/website/components/page-components/header/Docsearch.tsx
+++ apps/website/components/page-components/header/Docsearch.tsx
@@ -8,6 +8,14 @@ export function UiDocSearch() {
       appId={process.env.NEXT_PUBLIC_DOCSEARCH_APP_ID ?? ''}
       indexName={process.env.NEXT_PUBLIC_DOCSEARCH_INDEX_NAME ?? ''}
       apiKey={process.env.NEXT_PUBLIC_DOCSEARCH_API_KEY ?? ''}
+      askAi={{
+        assistantId: 'askAIDemo',
+        searchParameters: {
+          facetFilters: ['language:en'],
+        },
+      }}
+      insights={true}
+      placeholder='Search docs'
     />
   );
 }
