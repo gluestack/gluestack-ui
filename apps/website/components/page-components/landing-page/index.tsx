3d353592b9792149a0df4ff7a95a1f67bbf2a8dd
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-01-22 11:12:12 +0530

    feat: add Kitchensink QR code section to landing page with responsive design for iOS and Android platforms

--- apps/website/components/page-components/landing-page/index.tsx
+++ apps/website/components/page-components/landing-page/index.tsx
@@ -23,6 +23,15 @@ import WebsiteLayout from './WebsiteLayout';
 
 import Inspiration from './Inspiration';
 import PowerOfTailwind from './PowerOfTailwind';
+import Image from 'next/image';
+import { kitchensink } from '@/components/docs-components/apps/appConfig';
+import { HStack } from '@/components/ui/hstack';
+import { Box } from '@/components/ui/box';
+import { VStack } from '@/components/ui/vstack';
+import { Text } from '@/components/ui/text';
+import { Divider } from '@/components/ui/divider';
+import IOS from './Ios';
+import Android from './Android';
 
 
 const App = ({ referrer }: { referrer: string }) => {
@@ -37,7 +46,7 @@ const App = ({ referrer }: { referrer: string }) => {
             </div>
             <div className="flex flex-col gap-3">
               <h1 className="text-4xl font-bold tracking-[0.2px] leading-[120%] md:text-right">
-                React & React Native <br/> UI Components & Patterns
+                React & React Native <br /> UI Components & Patterns
               </h1>
               <p className="font-sans text-sm text-muted-foreground md:text-right xl:max-w-[560px]">
                 Comprehensive React and React Native component library for
@@ -59,6 +68,43 @@ const App = ({ referrer }: { referrer: string }) => {
                 <CopySnippetButton />
               </div>
             </div>
+
+            {/* Kitchensink QR Code */}
+            <HStack className="flex-1 gap-2 items-end">
+              <Box className="flex-1 md:flex hidden">
+                <Image
+                  alt="kitchensink qrcode"
+                  src={kitchensink.qrCodeUri}
+                  height={256}
+                  width={256}
+                  unoptimized
+                />
+              </Box>
+              <VStack className="flex-1 items-center">
+                <Text className="text-lg font-normal leading-[30px]  text-center md:flex hidden">
+                  Scan the QR code
+                </Text>
+                <Text className="text-xs font-normal leading-[30px]  text-muted-foreground text-center md:flex hidden">
+                  Available in
+                </Text>
+                <VStack className="border border-border rounded-md items-center px-16 py-3 flex-1 gap-2 web:md:flex md:flex hidden web:hidden">
+                  <HStack className="flex-1 items-center justify-center gap-2">
+                    <IOS />
+                    <Text className="text-md font-normal leading-[30px] flex-1">
+                      ios
+                    </Text>
+                  </HStack>
+                  <Divider />
+                  <HStack className="flex-1 items-center justify-center gap-2">
+                    <Android />
+                    <Text className="text-md font-normal leading-[30px] flex-1">
+                      Andriod
+                    </Text>
+                  </HStack>
+                </VStack>
+              </VStack>
+            </HStack>
+            {/* Kitchensink QR Code end*/}
           </div>
           <div className="hidden md:block md:w-1/2 items-start">
             <video
