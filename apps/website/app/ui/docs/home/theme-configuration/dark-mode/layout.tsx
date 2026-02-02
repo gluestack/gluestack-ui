59276e28bd38df33842c63ca9967a4a246c09419
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-02-02 15:11:33 +0530

    docs: update dark mode documentation to reflect gluestack-ui v4, ensuring consistency in theme customization details

--- apps/website/app/ui/docs/home/theme-configuration/dark-mode/layout.tsx
+++ apps/website/app/ui/docs/home/theme-configuration/dark-mode/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: "Dark Mode | gluestack-ui | Tailwind Dark Mode Theme",
-  description: "Customize the theme in gluestack-ui v3 with Tailwind dark mode, UI theme dark mode colors, and React Native light & dark mode for different color schemes and color mode support."
+  description: "Customize the theme in gluestack-ui v4 with Tailwind dark mode, UI theme dark mode colors, and React Native light & dark mode for different color schemes and color mode support."
 }
 
 export default function Layout({
