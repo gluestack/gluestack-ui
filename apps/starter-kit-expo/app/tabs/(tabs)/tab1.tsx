21605fc553f4fee7e33b9a36cbde7cc9635e80ec
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2026-02-05 11:10:30 +0530

    feat: fix modal

--- apps/starter-kit-expo/app/tabs/(tabs)/tab1.tsx
+++ apps/starter-kit-expo/app/tabs/(tabs)/tab1.tsx
@@ -1,16 +1,139 @@
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Center } from '@/components/ui/center';
-import { Divider } from '@/components/ui/divider';
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from '@/components/ui/modal';
+import { Button, ButtonText } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { Icon, CloseIcon } from '@/components/ui/icon';
+import { Menu, MenuItem, MenuItemLabel } from '@/components/ui/menu';
+import {
+  AddIcon,
+  GlobeIcon,
+  PlayIcon,
+  SettingsIcon,
+} from '@/components/ui/icon';
 
-export default function Tab2() {
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogBody,
+  AlertDialogBackdrop,
+} from '@/components/ui/alert-dialog';
+
+import React from 'react';
+export default function Example() {
+  const [showModal, setShowModal] = React.useState(false);
+    const [showAlertDialog, setShowAlertDialog] = React.useState(false);
+    const handleClose = () => setShowAlertDialog(false);
+
   return (
-    <Center className="flex-1">
-      <Heading className="font-bold text-2xl">Expo - Tab 1</Heading>
-      <Divider className="my-[30px] w-[80%]" />
-      <Text className="p-4">Example below to use gluestack-ui components.</Text>
-      <EditScreenInfo path="app/(app)/(tabs)/tab1.tsx" />
-    </Center>
+    <>
+      <Button onPress={() => setShowModal(true)}>
+        <ButtonText>Open Modal</ButtonText>
+      </Button>
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        size="md"
+      >
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg">Modal Title</Heading>
+            <ModalCloseButton>
+              <Icon as={CloseIcon} />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text>This is the modal body. You can add any content here.</Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              action="secondary"
+              className="mr-3"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Save</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Button onPress={() => setShowAlertDialog(true)}>
+        <ButtonText>Open Dialog</ButtonText>
+      </Button>
+      <AlertDialog isOpen={showAlertDialog} onClose={handleClose}>
+        <AlertDialogBackdrop />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading className="text-foreground font-semibold text-lg">
+              Are you sure you want to delete this post?
+            </Heading>
+          </AlertDialogHeader>
+          <AlertDialogBody className="mt-3 mb-4">
+            <Text className="text-sm text-muted-foreground">
+              Deleting the post will remove it permanently and cannot be undone.
+              Please confirm if you want to proceed.
+            </Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button variant="outline" onPress={handleClose}>
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button onPress={handleClose}>
+              <ButtonText>Delete</ButtonText>
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Menu
+        placement="top"
+        offset={5}
+        disabledKeys={['Settings']}
+        trigger={({ ...triggerProps }) => {
+          return (
+            <Button {...triggerProps}>
+              <ButtonText>Menu</ButtonText>
+            </Button>
+          );
+        }}
+      >
+        <MenuItem key="Add account" textValue="Add account">
+          <Icon as={AddIcon} size="sm" className="mr-2 " />
+          <MenuItemLabel size="sm">Add account</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Community" textValue="Community">
+          <Icon as={GlobeIcon} size="sm" className="mr-2 " />
+          <MenuItemLabel size="sm">Community</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Plugins" textValue="Plugins">
+          <Icon as={PlayIcon} size="sm" className="mr-2 " />
+          <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Settings" textValue="Settings">
+          <Icon as={SettingsIcon} size="sm" className="mr-2 " />
+          <MenuItemLabel size="sm">Settings</MenuItemLabel>
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
