0d68e861942671d8e5e588261afec7594fd04a05
Sanchitv3 <sanchitverma03032003@gmail.com>
2026-02-26 12:00:42 +0530

    refactor: update InlineCode component to use styled() for className support, removing cssInterop and enhancing consistency across implementations

--- src/docs-components/inline-code/index.tsx
+++ src/docs-components/inline-code/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Code } from '@expo/html-elements';
-import { cssInterop } from 'nativewind';
+import { styled } from 'nativewind';
 import { tva } from '@gluestack-ui/utils/nativewind-utils';
-cssInterop(Code, { className: 'style' });
+const StyledCode = styled(Code, { className: 'style' });
 const inlineStyle = tva({
   base: 'text-foreground text-md font-medium leading-6 px-[6px] py-[2px] bg-muted font-space-mono rounded-sm',
 });
 export const InlineCode = React.forwardRef(
   ({ className, ...props }: any, ref?: any) => {
     return (
-      <Code
+      <StyledCode
         className={inlineStyle({ class: className })}
         {...props}
         ref={ref}
