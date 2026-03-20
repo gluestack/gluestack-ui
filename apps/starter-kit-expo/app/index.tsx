c3ec4275aef34649967fa06c3e670e9ed966635d
ujjjwal2608 <ujjwalaggarwal2608@gmail.com>
2026-03-20 12:08:18 +0530

    resolve dependencies

--- apps/starter-kit-expo/app/index.tsx
+++ apps/starter-kit-expo/app/index.tsx
@@ -3,8 +3,7 @@ import { Chat } from '@/components/ui/chat-ai';
 import { ChatMessages } from '@/components/ui/chat-ai';
 import { ChatInput } from '@/components/ui/chat-ai';
 import { useChat } from '@/components/ui/chat-ai';
-import { generateAPIUrl } from '../lib/utils';
-import { SafeAreaView } from 'react-native-safe-area-context';
+
 export default function Home() {
   const { messages, send, loading, error } = useChat({
     api: 'http://10.153.0.82:8081/api/chat',
